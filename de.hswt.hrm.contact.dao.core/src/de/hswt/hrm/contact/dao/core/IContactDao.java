79c33a6b1765c02d2b4605cf5485853ae6b366ed
Ben <benjamin.pabst85@gmail.com>
2013-03-30 17:31:28 +0100

    Add method to search for a specific contact in contact.dao.core.

--- de.hswt.hrm.contact.dao.core/src/de/hswt/hrm/contact/dao/core/IContactDao.java
+++ de.hswt.hrm.contact.dao.core/src/de/hswt/hrm/contact/dao/core/IContactDao.java
@@ -3,10 +3,19 @@ package de.hswt.hrm.contact.dao.core;
 import java.util.Collection;
 import de.hswt.hrm.contact.model.Contact;
 
+/**
+ * Defines all the public methods to interact with the storage system of the contact plugin.
+ */
 public interface IContactDao {
     
     /**
      * @return All contacts from the database.
      */
     public Collection<Contact> findAll();
+    
+    /**
+     * @param id of the target contact.
+     * @return Contact with the given id.
+     */
+    public Contact findById(long id);
 }
