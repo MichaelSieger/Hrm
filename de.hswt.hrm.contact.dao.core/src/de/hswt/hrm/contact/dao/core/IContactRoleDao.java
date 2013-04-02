bff5cc4ab216c71c2d87b375d37f165c609d5f13
Ben <benjamin.pabst85@gmail.com>
2013-04-02 15:39:08 +0200

    Add CRUD methods to contact role DAO in contact.dao.core.

--- de.hswt.hrm.contact.dao.core/src/de/hswt/hrm/contact/dao/core/IContactRoleDao.java
+++ de.hswt.hrm.contact.dao.core/src/de/hswt/hrm/contact/dao/core/IContactRoleDao.java
@@ -1,8 +1,37 @@
 package de.hswt.hrm.contact.dao.core;
 
+import java.util.Collection;
+
+import de.hswt.hrm.contact.model.ContactRole;
+
 /**
  * Defines all the public methods to interact with the storage system for contact roles.
  */
-public class IContactRoleDao {
-
+public interface IContactRoleDao {
+    /**
+     * @return All contact roles from storage.
+     */
+    public Collection<ContactRole> findAll();
+    
+    /**
+     * @param id of the target role.
+     * @return Contact role with the given id.
+     */
+    public ContactRole findById(long id);
+    
+    // TODO: specify exception that will be thrown when role already exists
+    /**
+     * Add a new role to storage.
+     * 
+     * @param role Contact role that should be stored.
+     */
+    public void insert(ContactRole role);
+    
+    // TODO: specify exception that will be thrown when role does not exist
+    /**
+     * Update an existing role in storage.
+     * 
+     * @param role Contact role that should be updated.
+     */
+    public void update(ContactRole role);
 }
