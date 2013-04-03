3c2764a82d31f019d90aed659bb4894832393e33
Michael Sieger <michael.sieger@student.hswt.de>
2013-04-03 08:50:28 +0200

    Decontexter::convertFile some clean up

--- de.hswt.hrm.plantimagemigration/src/de/hswt/hrm/plantimagemigration/Decontexter.java
+++ de.hswt.hrm.plantimagemigration/src/de/hswt/hrm/plantimagemigration/Decontexter.java
@@ -22,37 +22,32 @@ public class Decontexter {
     private static final String[] BLACKLIST = new String[] { "\\renewcommand" };
 
     public void convertFile(File fin, File fout) throws IOException {
-        InputStream sin = null;
-        OutputStream sout = null;
-        Scanner sc = null;
-        PrintWriter writer = null;
-        try {
-            fout.createNewFile();
-            sin = new FileInputStream(fin);
-            sout = new FileOutputStream(fout);
-            sc = new Scanner(sin);
-            writer = new PrintWriter(sout);
-            appendPreamble(writer);
-            nextLine: while (sc.hasNextLine()) {
-                String line = sc.nextLine();
-                for (String[] rep : REPLACE) {
-                    line = line.replaceAll(rep[0], rep[1]);
-                }
-                for (String black : BLACKLIST) {
-                    if (line.startsWith(black)) {
-                        continue nextLine;
+        fout.createNewFile();
+        try (Scanner sc = new Scanner(new FileInputStream(fin))) {
+            try (PrintWriter writer = new PrintWriter(new FileOutputStream(fout))) {
+                appendPreamble(writer);
+                while (sc.hasNextLine()) {
+                    String line = sc.nextLine();
+                    for (String[] rep : REPLACE) {
+                        line = line.replaceAll(rep[0], rep[1]);
                     }
+                    if (isBlacklisted(line)) {
+                        continue;
+                    }
+                    writer.println(line);
                 }
-                writer.println(line);
+                appendFooter(writer);
             }
-            appendFooter(writer);
         }
-        finally {
-            DisposeHelper.tryClose(sc);
-            DisposeHelper.tryClose(writer);
-            DisposeHelper.tryClose(sin);
-            DisposeHelper.tryClose(sout);
+    }
+
+    private boolean isBlacklisted(String s) {
+        for (String black : BLACKLIST) {
+            if (s.startsWith(black)) {
+                return true;
+            }
         }
+        return false;
     }
 
     private void appendPreamble(PrintWriter w) {
