c405e841df726ccae23ee2a5ca6b404b3eb02bf2
Anton Schreck <anton.schreck@student.hswt.de>
2013-04-04 08:09:06 +0200

    create representation class for plants

---
+++ de.hswt.hrm.plant/src/de/hswt/hrm/plant/model/PlantRep.java
@@ -0,0 +1,16 @@
+package de.hswt.hrm.plant.model;
+
+/**
+ * Representation of a specific plant.
+ * Contains an array of PlantParts and two arrays for the positions of the PlantParts.
+ * 
+ * @author Anton Schreck
+ *
+ */
+
+public class PlantRep {
+    
+    private PlantPart[] parts;
+    private int[] xPos, yPos;   //Position in the grid
+
+}
