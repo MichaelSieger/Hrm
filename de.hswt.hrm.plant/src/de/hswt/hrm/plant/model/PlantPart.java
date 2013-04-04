87906c7a14ceee378cb6a8dd4806669e40c0d257
Anton Schreck <anton.schreck@student.hswt.de>
2013-04-04 13:01:09 +0200

    new class PlantPart

---
+++ de.hswt.hrm.plant/src/de/hswt/hrm/plant/model/PlantPart.java
@@ -0,0 +1,41 @@
+package de.hswt.hrm.plant.model;
+
+/**
+ * Represents a part of a plant with its PlantImage,
+ * and the position in the grid.
+ * 
+ * @author Anton Schreck
+ *
+ */
+
+public class PlantPart {
+    
+    private int xPos, yPos;
+    private PlantImage imag;
+    
+    public int getxPos() {
+        return xPos;
+    }
+    
+    public void setxPos(int xPos) {
+        this.xPos = xPos;
+    }
+
+    public int getyPos() {
+        return yPos;
+    }
+
+    public void setyPos(int yPos) {
+        this.yPos = yPos;
+    }
+
+    public PlantImage getImag() {
+        return imag;
+    }
+
+    public void setImag(PlantImage imag) {
+        this.imag = imag;
+    }
+    
+
+}
