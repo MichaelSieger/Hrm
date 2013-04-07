8e4c4f569747afa125b4e34db840cfa1082fc823
Ben <benjamin.pabst85@gmail.com>
2013-04-07 21:04:17 +0200

    Add DAO methods to the service class in contact.service.

--- de.hswt.hrm.contact.service/src/de/hswt/hrm/contact/service/ContactService.java
+++ de.hswt.hrm.contact.service/src/de/hswt/hrm/contact/service/ContactService.java
@@ -1,5 +1,50 @@
 package de.hswt.hrm.contact.service;
 
-public class ContactService {
+import java.util.Collection;
 
+import de.hswt.hrm.common.database.exception.ElementNotFoundException;
+import de.hswt.hrm.common.database.exception.SaveException;
+import de.hswt.hrm.common.exception.NotImplementedException;
+import de.hswt.hrm.contact.model.Contact;
+
+/**
+ * Service class which should be used to interact with the storage system for contacts.
+ */
+public class ContactService {
+    /**
+     * @return All contacts from storage.
+     */
+    public Collection<Contact> findAll() {
+        throw new NotImplementedException();
+    }
+    
+    /**
+     * @param id of the target contact.
+     * @return Contact with the given id.
+     * @throws ElementNotFoundException If the given id is not present in the database.
+     */
+    public Contact findById(long id) throws ElementNotFoundException {
+        throw new NotImplementedException();
+    }
+    
+    /**
+     * Add a new contact to storage.
+     * 
+     * @param contact Contact that should be stored.
+     * @throws SaveException If the contact could not be inserted.
+     */
+    public void insert(Contact contact) throws SaveException {
+        throw new NotImplementedException();
+    }
+    
+    /**
+     * Update an existing contact in storage.
+     * 
+     * @param contact Contact that should be updated.
+     * @throws ElementNotFoundException If the given contact is not present in the database.
+     * @throws SaveException If the contact could not be updated.
+     */
+    public void update(Contact contact) throws ElementNotFoundException, SaveException {
+        throw new NotImplementedException();
+    }
 }
