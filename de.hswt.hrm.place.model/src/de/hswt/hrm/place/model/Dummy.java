dbba02f1168bbe91c3176cf721b1dafd5888710b
Tobias Placht <tobias.placht@student.hswt.de>
2013-04-07 20:27:08 +0200

    add place plugins/feature

---
+++ de.hswt.hrm.place.model/src/de/hswt/hrm/place/model/Dummy.java
@@ -0,0 +1,13 @@
+package de.hswt.hrm.place.model;
+
+public class Dummy {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
