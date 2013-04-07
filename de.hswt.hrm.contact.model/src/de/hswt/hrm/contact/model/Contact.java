b41b99552e0d79573f49ad35627c6835347ae7f5
Ben <benjamin.pabst85@gmail.com>
2013-04-07 18:16:04 +0200

    Add equals and hashcode for contact model in contact.model.

--- de.hswt.hrm.contact.model/src/de/hswt/hrm/contact/model/Contact.java
+++ de.hswt.hrm.contact.model/src/de/hswt/hrm/contact/model/Contact.java
@@ -128,4 +128,104 @@ public class Contact {
     public long getId() {
         return id;
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((city == null) ? 0 : city.hashCode());
+        result = prime * result + ((company == null) ? 0 : company.hashCode());
+        result = prime * result + ((email == null) ? 0 : email.hashCode());
+        result = prime * result + ((fax == null) ? 0 : fax.hashCode());
+        result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+        result = prime * result + (int) (id ^ (id >>> 32));
+        result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+        result = prime * result + ((mobile == null) ? 0 : mobile.hashCode());
+        result = prime * result + ((phone == null) ? 0 : phone.hashCode());
+        result = prime * result + ((postCode == null) ? 0 : postCode.hashCode());
+        result = prime * result + ((shortcut == null) ? 0 : shortcut.hashCode());
+        result = prime * result + ((street == null) ? 0 : street.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Contact other = (Contact) obj;
+        if (city == null) {
+            if (other.city != null)
+                return false;
+        }
+        else if (!city.equals(other.city))
+            return false;
+        if (company == null) {
+            if (other.company != null)
+                return false;
+        }
+        else if (!company.equals(other.company))
+            return false;
+        if (email == null) {
+            if (other.email != null)
+                return false;
+        }
+        else if (!email.equals(other.email))
+            return false;
+        if (fax == null) {
+            if (other.fax != null)
+                return false;
+        }
+        else if (!fax.equals(other.fax))
+            return false;
+        if (firstName == null) {
+            if (other.firstName != null)
+                return false;
+        }
+        else if (!firstName.equals(other.firstName))
+            return false;
+        if (id != other.id)
+            return false;
+        if (lastName == null) {
+            if (other.lastName != null)
+                return false;
+        }
+        else if (!lastName.equals(other.lastName))
+            return false;
+        if (mobile == null) {
+            if (other.mobile != null)
+                return false;
+        }
+        else if (!mobile.equals(other.mobile))
+            return false;
+        if (phone == null) {
+            if (other.phone != null)
+                return false;
+        }
+        else if (!phone.equals(other.phone))
+            return false;
+        if (postCode == null) {
+            if (other.postCode != null)
+                return false;
+        }
+        else if (!postCode.equals(other.postCode))
+            return false;
+        if (shortcut == null) {
+            if (other.shortcut != null)
+                return false;
+        }
+        else if (!shortcut.equals(other.shortcut))
+            return false;
+        if (street == null) {
+            if (other.street != null)
+                return false;
+        }
+        else if (!street.equals(other.street))
+            return false;
+        return true;
+    }
+    
 }
