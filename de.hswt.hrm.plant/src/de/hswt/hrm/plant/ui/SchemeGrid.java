313e4b6a3d6db97614e554448f10d1e06c54662c
Michael Sieger <michael.sieger@student.hswt.de>
2013-04-08 12:01:01 +0200

    add GridLines

--- de.hswt.hrm.plant/src/de/hswt/hrm/plant/ui/SchemeGrid.java
+++ de.hswt.hrm.plant/src/de/hswt/hrm/plant/ui/SchemeGrid.java
@@ -1,18 +1,12 @@
 package de.hswt.hrm.plant.ui;
 
-import java.awt.GridLayout;
-import java.awt.Image;
-
-import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.PaintEvent;
 import org.eclipse.swt.events.PaintListener;
 import org.eclipse.swt.graphics.GC;
+import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.Rectangle;
-import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Canvas;
 import org.eclipse.swt.widgets.Composite;
-
-import de.hswt.hrm.plant.model.PSGridImage;
 
 
 public class SchemeGrid extends Canvas{
@@ -33,28 +27,94 @@ public class SchemeGrid extends Canvas{
     }
     
     private void draw(GC gc){
+        drawImages(gc);
         drawHorizontalLines(gc);
         drawVerticalLines(gc);
+    }
+    
+    private void drawImages(GC gc){
+        final int w = getGridWidth();
+        final int h = getGridHeight();
+        boolean[][] drawn = new boolean[h][w];
+        for(int i = 0; i < h; i++){
+            for(int j = 0; j < w; j++){
+                if(!drawn[i][j] && quads[i][j] != null){
+                    final int imgW = getImageSpanWidth(j, i);
+                    final int imgH = getImageSpanHeight(j, i);
+                    drawImage(gc, quads[i][j], j, i, 
+                            imgW,
+                            imgH);
+                    fillTrue(drawn, j, i, imgW, imgH);
+                }
+            }
+        }
+    }
+    
+    private void drawImage(GC gc, RenderedGridImage gridImage, int x, int y, int w, int h){
+        Image image = gridImage.getRenderedImage();
+        final float quadW = getQuadWidth();
+        final float quadH = getQuadHeight();
+        Rectangle rec = image.getBounds();
+        gc.drawImage(image, 0, 0, 
+                rec.x, rec.y, 
+                Math.round(quadW*x), Math.round(quadH*y), 
+                Math.round(quadW*w), Math.round(quadH));
+    }
+    
+    private void fillTrue(boolean[][] arr, int x, int y, int w, int h){
+        for(int i = 0; i < h; i++){
+            for(int j = 0; j < w; j++){
+                arr[i+y][j+x] = true;
+            }
+        }
+    }
+    
+    private int getImageSpanWidth(int x, int y){
+        RenderedGridImage img = quads[y][x];
+        final int w = getGridWidth();
+        int i = x;
+        while(i < w && quads[y][i] == img){
+            i++;
+        }
+        return i-x;
+    }
+    
+    private int getImageSpanHeight(int x, int y){
+        RenderedGridImage img = quads[y][x];
+        final int h = getGridHeight();
+        int i = y;
+        while(i < h && quads[i][x] == img){
+            i++;
+        }
+        return i-y;
     }
     
     private void drawHorizontalLines(GC gc){
         Rectangle rec = this.getBounds();
         final int h = getGridHeight();
-        final float quadH = ((float)rec.y)/h;
+        final float quadH = getQuadHeight();
         for(int i = 0; i < h; i++){
             final int y = Math.round(i*quadH);
-            gc.drawLine(0, y, rec.x, y);
+            gc.drawLine(0, y, rec.width, y);
         }
     }
 
     private void drawVerticalLines(GC gc){
         Rectangle rec = this.getBounds();
         final int w = getGridWidth();
-        final float quadW = ((float)rec.x)/w;
+        final float quadW = getQuadWidth();
         for(int i = 0; i < w; i++){
             final int x = Math.round(i*quadW);
-            gc.drawLine(x, 0, x, rec.y);
+            gc.drawLine(x, 0, x, rec.height);
         }
+    }
+    
+    private float getQuadWidth(){
+        return ((float)getBounds().width)/getGridWidth();
+    }
+    
+    private float getQuadHeight(){
+        return ((float)getBounds().height)/getGridHeight();
     }
     
     private int getGridWidth(){
@@ -67,8 +127,8 @@ public class SchemeGrid extends Canvas{
     
     public void setPartAt(RenderedGridImage rendered,
                           int x, int y){
-        final int w = rendered.getPlantImage().getWidth();
-        final int h = rendered.getPlantImage().getHeight();
+        final int w = rendered.getSchemeGrid().getWidth();
+        final int h = rendered.getSchemeGrid().getHeight();
         for(int i = y; i < y+h; i++){
             for(int j = x; j < x+w; j++){
                 quads[i][j] = rendered;
