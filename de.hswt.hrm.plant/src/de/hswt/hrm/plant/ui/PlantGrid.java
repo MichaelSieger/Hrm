012b086122ce389341af38881d8e73f27140a92a
Michael Sieger <michael.sieger@student.hswt.de>
2013-04-08 08:22:35 +0200

    Change base class to Canvas, impementation of grid line draw

--- de.hswt.hrm.plant/src/de/hswt/hrm/plant/ui/PlantGrid.java
+++ de.hswt.hrm.plant/src/de/hswt/hrm/plant/ui/PlantGrid.java
@@ -4,31 +4,77 @@ import java.awt.GridLayout;
 import java.awt.Image;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.PaintEvent;
+import org.eclipse.swt.events.PaintListener;
+import org.eclipse.swt.graphics.GC;
+import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Canvas;
 import org.eclipse.swt.widgets.Composite;
 
 import de.hswt.hrm.plant.model.PlantImage;
 
 
-public class PlantGrid extends Composite{
+public class PlantGrid extends Canvas{
     
-    private static final int FIELD_WIDTH = 20;
+    private RenderedPlantImage[][] quads;
 
     public PlantGrid(Composite parent, int style, 
             int width, int height) {
         super(parent, style);
-        GridLayout layout = new GridLayout();
-        layout.setColumns(width);
-        layout.setRows(height);
+        quads = new RenderedPlantImage[height][width];
+        super.addPaintListener(new PaintListener() {
+            
+            @Override
+            public void paintControl(PaintEvent ev) {
+                draw(ev.gc);
+            }
+        });
+    }
+    
+    private void draw(GC gc){
+        drawHorizontalLines(gc);
+        drawVerticalLines(gc);
+    }
+    
+    private void drawHorizontalLines(GC gc){
+        Rectangle rec = this.getBounds();
+        final int h = getGridHeight();
+        final float quadH = ((float)rec.y)/h;
+        for(int i = 0; i < h; i++){
+            final int y = Math.round(i*quadH);
+            gc.drawLine(0, y, rec.x, y);
+        }
     }
 
-    public void setPartAt(PlantImage image){
-        ImageWidget widget = new ImageWidget(this, SWT.NONE);
-        GridData gridData = new GridData();
-        gridData.horizontalSpan = image.getWidth();
-        gridData.verticalSpan = image.getHeight();
-        widget.setLayoutData(gridData);
-        widget.setSize(image.getWidth()*FIELD_WIDTH, 
-                image.getHeight()*FIELD_WIDTH);
+    private void drawVerticalLines(GC gc){
+        Rectangle rec = this.getBounds();
+        final int w = quads[0].length;
+        final float quadW = ((float)rec.x)/w;
+        for(int i = 0; i < w; i++){
+            final int x = Math.round(i*quadW);
+            gc.drawLine(x, 0, x, rec.y);
+        }
     }
+    
+    private int getGridWidth(){
+        return quads[0].length;
+    }
+    
+    private int getGridHeight(){
+        return quads.length;
+    }
+    
+    public void setPartAt(RenderedPlantImage rendered,
+                          int x, int y){
+        final int w = rendered.getPlantImage().getWidth();
+        final int h = rendered.getPlantImage().getHeight();
+        for(int i = y; i < y+h; i++){
+            for(int j = x; j < x+w; j++){
+                quads[i][j] = rendered;
+            }
+        }
+    }
+    
+    
 }
