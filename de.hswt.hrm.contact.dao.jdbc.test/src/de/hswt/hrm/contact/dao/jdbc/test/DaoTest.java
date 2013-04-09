368c10e9f9d4fe603e9ce3a6f8d1f4a6cd5a424c
Ben <benjamin.pabst85@gmail.com>
2013-04-09 21:33:10 +0200

    Add test fragment for contact.dao.jdbc.

---
+++ de.hswt.hrm.contact.dao.jdbc.test/src/de/hswt/hrm/contact/dao/jdbc/test/DaoTest.java
@@ -0,0 +1,17 @@
+package de.hswt.hrm.contact.dao.jdbc.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import de.hswt.hrm.contact.dao.jdbc.Dummy;
+
+public class DaoTest {
+
+    @Test
+    public void test() {
+        Dummy test = new Dummy();
+        fail("Not yet implemented");
+    }
+
+}
