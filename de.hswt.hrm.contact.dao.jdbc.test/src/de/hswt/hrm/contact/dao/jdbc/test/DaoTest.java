339cb6e2e0681a3aeb534b51b9d2da753cecb5c7
Ben <benjamin.pabst85@gmail.com>
2013-04-09 21:45:06 +0200

    Add empty methods in ContactDao in contact.dao.jdbc.

--- de.hswt.hrm.contact.dao.jdbc.test/src/de/hswt/hrm/contact/dao/jdbc/test/DaoTest.java
+++ de.hswt.hrm.contact.dao.jdbc.test/src/de/hswt/hrm/contact/dao/jdbc/test/DaoTest.java
@@ -4,13 +4,11 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-import de.hswt.hrm.contact.dao.jdbc.Dummy;
-
 public class DaoTest {
 
     @Test
     public void test() {
-        Dummy test = new Dummy();
+        
         fail("Not yet implemented");
     }
 
