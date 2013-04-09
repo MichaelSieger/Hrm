72d21997abc3a7e82d373ff4dbbbf91fae534bcb
Ben <benjamin.pabst85@gmail.com>
2013-04-09 21:08:44 +0200

    Add factory class for database connections in common.database.

---
+++ de.hswt.hrm.common/src/de/hswt/hrm/common/database/DatabaseFactory.java
@@ -0,0 +1,41 @@
+package de.hswt.hrm.common.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import de.hswt.hrm.common.database.exception.DatabaseException;
+
+/**
+ * Class that is used to get a database connection. This may be changed to injection later on.
+ */
+public class DatabaseFactory {
+    private static Connection con;
+    
+    /**
+     * Returns a connection object for the database. This is a singleton (will only create the
+     * connection once and returns the same instance on all later calls).
+     * 
+     * @return Connection object for the database.
+     * @throws DatabaseException If connection could not be created.
+     */
+    public static Connection getConnection() throws DatabaseException {
+        // TODO load driver
+        // Class.forName("put driver name in here");
+        
+        // TODO load connection string from configuration
+        String config = "";
+        
+        if (con == null) {
+            try {
+                con = DriverManager.getConnection(config);
+            }
+            catch (SQLException e) {
+                // TODO maybe add specific information about the error
+                throw new DatabaseException(e);
+            }
+        }
+        
+        return con;
+    }
+}
