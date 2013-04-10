692cda09f30c200fda87042427a5aed056a224a7
Anton Schreck <anton.schreck@student.hswt.de>
2013-04-10 14:53:53 +0200

    added getters/setters

--- de.hswt.hrm.plant/src/de/hswt/hrm/plant/model/Plant.java
+++ de.hswt.hrm.plant/src/de/hswt/hrm/plant/model/Plant.java
@@ -23,5 +23,61 @@ public class Plant {
     // optional:
     private String producer;
     private Integer constYear;  // year of construction
-
+    
+    // Getters and setters:
+    public SchemePart[] getComposition() {
+        return composition;
+    }
+    public void setComposition(SchemePart[] composition) {
+        this.composition = composition;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getLocation() {
+        return location;
+    }
+    public void setLocation(String location) {
+        this.location = location;
+    }
+    public String getComments() {
+        return comments;
+    }
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
+    public Date getNextInspection() {
+        return nextInspection;
+    }
+    public void setNextInspection(Date nextInspection) {
+        this.nextInspection = nextInspection;
+    }
+    public Integer getNumElem() {
+        return numElem;
+    }
+    public void setNumElem(Integer numElem) {
+        this.numElem = numElem;
+    }
+    public TechnicalData getTechData() {
+        return techData;
+    }
+    public void setTechData(TechnicalData techData) {
+        this.techData = techData;
+    }
+    public String getProducer() {
+        return producer;
+    }
+    public void setProducer(String producer) {
+        this.producer = producer;
+    }
+    public Integer getConstYear() {
+        return constYear;
+    }
+    public void setConstYear(Integer constYear) {
+        this.constYear = constYear;
+    }
+    
 }
