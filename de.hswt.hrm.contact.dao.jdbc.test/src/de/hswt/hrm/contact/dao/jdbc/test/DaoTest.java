0da0d9fd4c91348b4136fb7d37beb57cb5b9a03e
Ben <benjamin.pabst85@gmail.com>
2013-04-14 01:10:06 +0200

    Implement insert test in contact.dao.jdbc.test.

--- de.hswt.hrm.contact.dao.jdbc.test/src/de/hswt/hrm/contact/dao/jdbc/test/DaoTest.java
+++ de.hswt.hrm.contact.dao.jdbc.test/src/de/hswt/hrm/contact/dao/jdbc/test/DaoTest.java
@@ -4,13 +4,28 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import de.hswt.hrm.common.database.exception.SaveException;
+import de.hswt.hrm.contact.dao.jdbc.ContactDao;
+import de.hswt.hrm.contact.model.Contact;
 import de.hswt.hrm.test.database.AbstractDatabaseTest;
 
 public class DaoTest extends AbstractDatabaseTest {
 
     @Test
-    public void test() {
-        fail("Not yet implemented");
+    public void testInsertContact() throws SaveException {
+        final String lastName = "lastname";
+        final String firstName = "firstname";
+        final String street = "street";
+        final String streetNo = "116a";
+        final String postCode = "81272";
+        final String city = "city";
+        
+        Contact c = new Contact(lastName, firstName, street, streetNo, postCode, city);
+        
+        ContactDao dao = new ContactDao();
+        dao.insert(c);
+        
+        // TODO retrieve and compare contact
     }
 
 }
