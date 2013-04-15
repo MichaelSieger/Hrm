b0201e04fcb919d2085d92e92dac80c47bc92266
Tobias Placht <tobias.placht@student.hswt.de>
2013-04-15 19:10:02 +0200

    add code to load xwt file

--- de.hswt.hrm.contact.ui/src/de/hswt/hrm/contact/ui/view/ContactView.java
+++ de.hswt.hrm.contact.ui/src/de/hswt/hrm/contact/ui/view/ContactView.java
@@ -1,5 +1,7 @@
 package de.hswt.hrm.contact.ui.view;
 
+import java.net.URL;
+
 import javax.inject.Inject;
 import javax.annotation.PostConstruct;
 
@@ -7,6 +9,8 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.e4.ui.di.Focus;
+import org.eclipse.e4.xwt.IConstants;
+import org.eclipse.e4.xwt.XWT;
 import org.eclipse.swt.layout.GridLayout;
 
 public class ContactView {
@@ -17,8 +21,22 @@ public class ContactView {
 
     @PostConstruct
     public void postConstruct(Composite parent) {
-        parent.setLayout(new GridLayout(2, false));
-        Button b = new Button(parent, SWT.BORDER);
+        // parent.setLayout(new GridLayout(2, false));
+        // Button b = new Button(parent, SWT.BORDER);
+
+        // String name = ContactView.class.getSimpleName() + IConstants.XWT_EXTENSION_SUFFIX;
+
+        URL u = ContactView.class.getClassLoader().getResource(
+                "de/hswt/hrm/contact/ui/xwt/Test.xwt");
+        try {
+            XWT.load(parent, u);
+            parent.pack();
+        }
+        catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
     }
 
     @Focus
