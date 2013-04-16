6e3ae7b79fea076b92d1ace5fa0e8b5ad0714ac2
Ben <benjamin.pabst85@gmail.com>
2013-04-16 19:09:50 +0200

    Add CRU methods in place.dao.core.

--- de.hswt.hrm.place.dao.core/src/de/hswt/hrm/place/dao/core/IPlaceDao.java
+++ de.hswt.hrm.place.dao.core/src/de/hswt/hrm/place/dao/core/IPlaceDao.java
@@ -1,5 +1,43 @@
 package de.hswt.hrm.place.dao.core;
 
-public interface IPlaceDao {
+import java.util.Collection;
 
+import de.hswt.hrm.common.database.exception.DatabaseException;
+import de.hswt.hrm.common.database.exception.ElementNotFoundException;
+import de.hswt.hrm.common.database.exception.SaveException;
+import de.hswt.hrm.place.model.Place;
+
+/**
+ * Defines all the public methods to interact with the storage system for places.
+ */
+public interface IPlaceDao {
+    /**
+     * @return All places from storage.
+     */
+    public Collection<Place> findAll() throws DatabaseException;
+    
+    /**
+     * @param id of the target place.
+     * @return Place with the given id.
+     * @throws ElementNotFoundException If the given id is not present in storage.
+     */
+    public Place findById(int id) throws DatabaseException, ElementNotFoundException;
+    
+    /**
+     * Add a new place to storage.
+     * 
+     * @param place Place that should be stored.
+     * @return Newly generated place (also holding the correct id).
+     * @throws SaveException If the place could not be inserted.
+     */
+    public Place insert(Place place) throws SaveException;
+    
+    /**
+     * Update an existing place in storage.
+     * 
+     * @param place Place that should be updated.
+     * @throws ElementNotFoundException If the given place is not present in the database.
+     * @throws SaveException If the place could not be updated.
+     */
+    public void update(Place place) throws ElementNotFoundException, SaveException;
 }
