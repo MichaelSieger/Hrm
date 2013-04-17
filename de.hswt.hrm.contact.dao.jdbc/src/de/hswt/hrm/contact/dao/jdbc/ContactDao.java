f0586b15985cb3da115c76a5f7b983277d57fd0c
haeringl <luke-haering@web.de>
2013-04-17 13:45:05 +0200

    add findAll method in ContactDao

--- de.hswt.hrm.contact.dao.jdbc/src/de/hswt/hrm/contact/dao/jdbc/ContactDao.java
+++ de.hswt.hrm.contact.dao.jdbc/src/de/hswt/hrm/contact/dao/jdbc/ContactDao.java
@@ -18,27 +18,42 @@ import de.hswt.hrm.contact.model.Contact;
 import de.hswt.hrm.contact.dao.core.IContactDao;
 
 public class ContactDao implements IContactDao {
-    
+
     @Override
     public Collection<Contact> findAll() throws DatabaseException {
-        throw new NotImplementedException();
+
+        final String query = "SELECT Contact_ID, Contact_Name, Contact_First_Name, "
+                + "Contact_Zip_Code, "
+                + "Contact_City, Contact_Street, Contact_Street_Number, Contact_Shortcut, "
+                + "Contact_Phone, Contact_Fax, Contact_Mobile, Contact_Email FROM Contact ;";
+
+        try (Connection con = DatabaseFactory.getConnection()) {
+            try (NamedParameterStatement stmt = NamedParameterStatement.fromConnection(con, query)) {
+                ResultSet result = stmt.executeQuery();
+
+                return fromResultSet(result);
+            }
+        }
+        catch (SQLException e) {
+            throw new DatabaseException(e);
+        }
     }
 
     @Override
     public Contact findById(int id) throws DatabaseException, ElementNotFoundException {
         checkArgument(id >= 0, "Id must not be negative.");
-        
+
         final String query = "SELECT Contact_ID, Contact_Name, Contact_First_Name, "
                 + "Contact_Zip_Code, "
                 + "Contact_City, Contact_Street, Contact_Street_Number, Contact_Shortcut, "
                 + "Contact_Phone, Contact_Fax, Contact_Mobile, Contact_Email FROM Contact "
                 + "WHERE Contact_ID = :id;";
-        
+
         try (Connection con = DatabaseFactory.getConnection()) {
             try (NamedParameterStatement stmt = NamedParameterStatement.fromConnection(con, query)) {
                 stmt.setParameter("id", id);
                 ResultSet result = stmt.executeQuery();
-                
+
                 Collection<Contact> contacts = fromResultSet(result);
                 if (contacts.size() < 1) {
                     throw new ElementNotFoundException();
@@ -46,7 +61,7 @@ public class ContactDao implements IContactDao {
                 else if (contacts.size() > 1) {
                     throw new DatabaseException("ID '" + id + "' is not unique.");
                 }
-                
+
                 return contacts.iterator().next();
             }
         }
@@ -60,82 +75,76 @@ public class ContactDao implements IContactDao {
      */
     @Override
     public Contact insert(Contact contact) throws SaveException {
-    	final String query = "INSERT INTO Contact (Contact_Name, Contact_First_Name, "
-    			+ "Contact_Zip_Code, Contact_City, Contact_Street, Contact_Street_Number, "
-    			+ "Contact_Shortcut, Contact_Phone, Contact_Fax, Contact_Mobile, Contact_Email) " 
-    			+ "VALUES (:name, :firstName, :zipCode, :city, :street, :streetNumber, "
-    			+ ":shortcut, :phone, :fax, :mobile, :email);";
-    	
-    	try (Connection con = DatabaseFactory.getConnection()) {
-    	    try (NamedParameterStatement stmt = NamedParameterStatement.fromConnection(con, query)) {
-    	        stmt.setParameter("name", contact.getLastName());
-    	        stmt.setParameter("firstName", contact.getFirstName());
-    	        stmt.setParameter("zipCode", contact.getPostCode());
-    	        stmt.setParameter("city", contact.getCity());
-    	        stmt.setParameter("street", contact.getStreet());
-    	        stmt.setParameter("streetNumber", contact.getStreetNo());
-    	        stmt.setParameter("shortcut", contact.getShortcut().orNull());
-    	        stmt.setParameter("phone", contact.getPhone().orNull());
-    	        stmt.setParameter("fax", contact.getFax().orNull());
-    	        stmt.setParameter("mobile", contact.getMobile().orNull());
-    	        stmt.setParameter("email", contact.getEmail().orNull());
-    	        
-    	        int affectedRows = stmt.executeUpdate();
-    	        if (affectedRows != 1) {
+        final String query = "INSERT INTO Contact (Contact_Name, Contact_First_Name, "
+                + "Contact_Zip_Code, Contact_City, Contact_Street, Contact_Street_Number, "
+                + "Contact_Shortcut, Contact_Phone, Contact_Fax, Contact_Mobile, Contact_Email) "
+                + "VALUES (:name, :firstName, :zipCode, :city, :street, :streetNumber, "
+                + ":shortcut, :phone, :fax, :mobile, :email);";
+
+        try (Connection con = DatabaseFactory.getConnection()) {
+            try (NamedParameterStatement stmt = NamedParameterStatement.fromConnection(con, query)) {
+                stmt.setParameter("name", contact.getLastName());
+                stmt.setParameter("firstName", contact.getFirstName());
+                stmt.setParameter("zipCode", contact.getPostCode());
+                stmt.setParameter("city", contact.getCity());
+                stmt.setParameter("street", contact.getStreet());
+                stmt.setParameter("streetNumber", contact.getStreetNo());
+                stmt.setParameter("shortcut", contact.getShortcut().orNull());
+                stmt.setParameter("phone", contact.getPhone().orNull());
+                stmt.setParameter("fax", contact.getFax().orNull());
+                stmt.setParameter("mobile", contact.getMobile().orNull());
+                stmt.setParameter("email", contact.getEmail().orNull());
+
+                int affectedRows = stmt.executeUpdate();
+                if (affectedRows != 1) {
                     throw new SaveException();
                 }
-    	        
-    	        try (ResultSet generatedKeys = stmt.getGeneratedKeys()) {
+
+                try (ResultSet generatedKeys = stmt.getGeneratedKeys()) {
                     if (generatedKeys.next()) {
                         int id = generatedKeys.getInt(1);
-                        
+
                         // Create new contact with id
-                        Contact inserted = new Contact(id, contact.getLastName(), 
-                                contact.getFirstName(), contact.getStreet(), 
-                                contact.getStreetNo(), contact.getPostCode(), contact.getCity());
-                        
+                        Contact inserted = new Contact(id, contact.getLastName(),
+                                contact.getFirstName(), contact.getStreet(), contact.getStreetNo(),
+                                contact.getPostCode(), contact.getCity());
+
                         inserted.setShortcut(contact.getShortcut().orNull());
                         inserted.setPhone(contact.getPhone().orNull());
                         inserted.setFax(contact.getFax().orNull());
                         inserted.setMobile(contact.getMobile().orNull());
                         inserted.setEmail(contact.getEmail().orNull());
-                        
+
                         return inserted;
                     }
                     else {
-                        throw new SaveException("Could not retrieve generated ID.");    
+                        throw new SaveException("Could not retrieve generated ID.");
                     }
                 }
-    	    }
-    	    
-    	}
-    	catch (SQLException|DatabaseException e) {
-    		throw new SaveException(e);
-    	}
+            }
+
+        }
+        catch (SQLException | DatabaseException e) {
+            throw new SaveException(e);
+        }
     }
 
     @Override
     public void update(Contact contact) throws ElementNotFoundException, SaveException {
         checkNotNull(contact, "Contact must not be null.");
-        
+
         if (contact.getId() < 0) {
             throw new ElementNotFoundException("Element has no valid ID.");
         }
-        
-        final String query = "UPDATE Contact SET "
-                + "Contact_Name = :lastName, "
-                + "Contact_First_Name = :firstName, "
-                + "Contact_Zip_Code = :zipCode, "
-                + "Contact_City = :city, "
-                + "Contact_Street = :street, "
-                + "Contact_Street_Number = :streetNumber, "
-                + "Contact_Shortcut = :shortcut, "
-                + "Contact_Phone = :phone, "
-                + "Contact_Fax = :fax, "
-                + "Contact_Mobile = :mobile, "
-                + "Contact_Email = :email "
+
+        final String query = "UPDATE Contact SET " + "Contact_Name = :lastName, "
+                + "Contact_First_Name = :firstName, " + "Contact_Zip_Code = :zipCode, "
+                + "Contact_City = :city, " + "Contact_Street = :street, "
+                + "Contact_Street_Number = :streetNumber, " + "Contact_Shortcut = :shortcut, "
+                + "Contact_Phone = :phone, " + "Contact_Fax = :fax, "
+                + "Contact_Mobile = :mobile, " + "Contact_Email = :email "
                 + "WHERE Contact_ID = :id;";
-        
+
         try (Connection con = DatabaseFactory.getConnection()) {
             try (NamedParameterStatement stmt = NamedParameterStatement.fromConnection(con, query)) {
                 stmt.setParameter("id", contact.getId());
@@ -150,14 +159,14 @@ public class ContactDao implements IContactDao {
                 stmt.setParameter("fax", contact.getFax().orNull());
                 stmt.setParameter("mobile", contact.getMobile().orNull());
                 stmt.setParameter("email", contact.getEmail().orNull());
-                
+
                 int affectedRows = stmt.executeUpdate();
                 if (affectedRows != 1) {
                     throw new SaveException();
                 }
             }
         }
-        catch (SQLException|DatabaseException e) {
+        catch (SQLException | DatabaseException e) {
             throw new SaveException(e);
         }
     }
@@ -165,29 +174,29 @@ public class ContactDao implements IContactDao {
     private Collection<Contact> fromResultSet(ResultSet rs) throws SQLException {
         checkNotNull(rs, "Result must not be null.");
         Collection<Contact> contactList = new ArrayList<>();
-        
+
         while (rs.next()) {
-            //Contact_Name, Contact_First_Name, Contact_Zip_Code, "
-            //        + "Contact_City, Contact_Street, Contact_Street_Number, Contact_Shortcut, "
-            //        + "Contact_Phone, Contact_Fax, Contact_Mobile, Contact_Email
+            // Contact_Name, Contact_First_Name, Contact_Zip_Code, "
+            // + "Contact_City, Contact_Street, Contact_Street_Number, Contact_Shortcut, "
+            // + "Contact_Phone, Contact_Fax, Contact_Mobile, Contact_Email
             int id = rs.getInt("Contact_ID");
             String lastName = rs.getString("Contact_Name");
-            String firstName = rs.getString("Contact_First_Name"); 
+            String firstName = rs.getString("Contact_First_Name");
             String zipCode = rs.getString("Contact_Zip_Code");
             String city = rs.getString("Contact_City");
             String street = rs.getString("Contact_Street");
             String streetNo = rs.getString("Contact_Street_Number");
-            
+
             Contact contact = new Contact(id, lastName, firstName, street, streetNo, zipCode, city);
             contact.setShortcut(rs.getString("Contact_Shortcut"));
             contact.setPhone(rs.getString("Contact_Phone"));
             contact.setFax(rs.getString("Contact_Fax"));
             contact.setMobile(rs.getString("Contact_Mobile"));
             contact.setEmail(rs.getString("Contact_Email"));
-            
+
             contactList.add(contact);
         }
-        
+
         return contactList;
     }
 }
