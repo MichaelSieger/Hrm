ce531214520eaa8ccc12ba39a83af02d888043ce
Tobias Placht <tobias.placht@student.hswt.de>
2013-04-18 21:13:41 +0200

    add: class ContactComperator

---
+++ de.hswt.hrm.contact.model/src/de/hswt/hrm/contact/model/ContactComperator.java
@@ -0,0 +1,14 @@
+package de.hswt.hrm.contact.model;
+
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.jface.viewers.ViewerComparator;
+
+public class ContactComperator extends ViewerComparator {
+
+    @Override
+    public int compare(Viewer viewer, Object e1, Object e2) {
+        // TODO Implement compareTo
+        return super.compare(viewer, e1, e2);
+    }
+
+}
