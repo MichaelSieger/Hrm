a00226c567ee077b17a64c8addc2591f8cb95588
Tobias Placht <tobias.placht@student.hswt.de>
2013-04-18 13:17:29 +0200

    add: missing manifest.entries

--- de.hswt.hrm.contact.ui/src/de/hswt/hrm/contact/ui/view/ContactView.java
+++ de.hswt.hrm.contact.ui/src/de/hswt/hrm/contact/ui/view/ContactView.java
@@ -1,18 +1,24 @@
 package de.hswt.hrm.contact.ui.view;
 
 import java.net.URL;
+import java.util.Collection;
 
 import javax.annotation.PostConstruct;
 
 import org.eclipse.e4.ui.di.Focus;
 import org.eclipse.e4.xwt.IConstants;
 import org.eclipse.e4.xwt.XWT;
+import org.eclipse.jface.viewers.ArrayContentProvider;
 import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.swt.widgets.Composite;
 
+import de.hswt.hrm.common.database.exception.DatabaseException;
+import de.hswt.hrm.contact.model.Contact;
+import de.hswt.hrm.contact.service.ContactService;
+
 public class ContactView {
 
-    private TableViewer table;
+    private TableViewer viewer;
 
     @PostConstruct
     public void postConstruct(Composite parent) {
@@ -21,18 +27,27 @@ public class ContactView {
                 "de/hswt/hrm/contact/ui/xwt/ContactView" + IConstants.XWT_EXTENSION_SUFFIX);
         try {
             Composite comp = (Composite) XWT.load(parent, url);
-            table = (TableViewer) XWT.findElementByName(comp, "contactTable");
-            System.out.println(table.toString());
+            viewer = (TableViewer) XWT.findElementByName(comp, "contactTable");
+            System.out.println(viewer.toString());
         }
         catch (Exception e) {
             e.printStackTrace();
         }
 
-        initalizeTable(parent, table);
+        initalizeTable(parent, viewer);
 
     }
 
-    private void initalizeTable(Composite parent, TableViewer table) {
+    private void initalizeTable(Composite parent, TableViewer viewer) {
+
+        try {
+            Collection<Contact> contacts = ContactService.findAll();
+            viewer.setContentProvider(ArrayContentProvider.getInstance());
+            viewer.setInput(contacts);
+        }
+        catch (DatabaseException e) {
+            e.printStackTrace();
+        }
 
     }
 
