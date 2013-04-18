21d83320b214cdb641180e6e2e878ce243855802
Ben <benjamin.pabst85@gmail.com>
2013-04-18 20:56:48 +0200

    Implement PlaceService in place.service.

--- de.hswt.hrm.place.service/src/de/hswt/hrm/place/service/PlaceService.java
+++ de.hswt.hrm.place.service/src/de/hswt/hrm/place/service/PlaceService.java
@@ -1,5 +1,33 @@
 package de.hswt.hrm.place.service;
 
-public interface PlaceService {
+import java.util.Collection;
 
+import de.hswt.hrm.common.database.exception.DatabaseException;
+import de.hswt.hrm.common.database.exception.ElementNotFoundException;
+import de.hswt.hrm.common.database.exception.SaveException;
+import de.hswt.hrm.place.dao.core.IPlaceDao;
+import de.hswt.hrm.place.dao.jdbc.PlaceDao;
+import de.hswt.hrm.place.model.Place;
+
+public class PlaceService {
+	
+	private PlaceService() { }
+	
+	private static IPlaceDao dao = new PlaceDao();
+	
+	public static Collection<Place> findAll() throws DatabaseException {
+		return dao.findAll();
+	}
+	
+	public static Place findById(int id) throws ElementNotFoundException, DatabaseException {
+		return dao.findById(id);
+	}
+	
+	public static Place insert(Place place) throws SaveException {
+		return dao.insert(place);
+	}
+	
+	public static void update(Place place) throws ElementNotFoundException, SaveException {
+		dao.update(place);
+	}
 }
