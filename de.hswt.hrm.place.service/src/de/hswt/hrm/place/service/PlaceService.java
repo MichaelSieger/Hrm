32e95ba1389d6922a806d2b2a75560b74dab02ff
Ben <benjamin.pabst85@gmail.com>
2013-04-21 16:24:56 +0200

    Add comments to place service.

--- de.hswt.hrm.place.service/src/de/hswt/hrm/place/service/PlaceService.java
+++ de.hswt.hrm.place.service/src/de/hswt/hrm/place/service/PlaceService.java
@@ -15,22 +15,53 @@ public class PlaceService {
 	
 	private static IPlaceDao dao = new PlaceDao();
 	
+	/**
+     * @return All places from storage.
+     * @throws DatabaseException 
+     */
 	public static Collection<Place> findAll() throws DatabaseException {
 		return dao.findAll();
 	}
 	
+	/**
+     * @param id of the target place.
+     * @return Place with the given id.
+     * @throws DatabaseException 
+     */
 	public static Place findById(int id) throws ElementNotFoundException, DatabaseException {
 		return dao.findById(id);
 	}
 	
+	/**
+     * Add a new place to storage.
+     * 
+     * @param place Place that should be stored.
+     * @return The created place.
+     * @throws SaveException If the place could not be inserted.
+     */
 	public static Place insert(Place place) throws SaveException {
 		return dao.insert(place);
 	}
 	
+	/**
+     * Update an existing place in storage.
+     * 
+     * @param place Place that should be updated.
+     * @throws ElementNotFoundException If the given place is not present in the database.
+     * @throws SaveException If the place could not be updated.
+     */
 	public static void update(Place place) throws ElementNotFoundException, SaveException {
 		dao.update(place);
 	}
 	
+	/**
+     * Refreshes a given place with values from the database.
+     * 
+     * @param contact
+     * @return
+     * @throws ElementNotFoundException
+     * @throws DatabaseException
+     */
 	public static Place refresh(Place place) throws ElementNotFoundException, DatabaseException {
 	    return dao.findById(place.getId());
 	}
