db25071b912e682c807e1c317e50e950a5f028c0
Ben <benjamin.pabst85@gmail.com>
2013-04-24 16:48:03 +0200

    Add logger and XWT file.

--- de.hswt.hrm.place.ui/src/de/hswt/hrm/place/ui/part/PlacePart.java
+++ de.hswt.hrm.place.ui/src/de/hswt/hrm/place/ui/part/PlacePart.java
@@ -2,7 +2,12 @@ package de.hswt.hrm.place.ui.part;
 
 import javax.annotation.PostConstruct;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class PlacePart {
+	
+	private final static Logger LOG = LoggerFactory.getLogger(PlacePart.class);
 
 	@PostConstruct
 	public void postConstruct() {
