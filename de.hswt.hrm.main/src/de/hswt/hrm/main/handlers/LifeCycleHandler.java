15d9fa3cc40b1d5fe7ad634b0ab35cc462b25a99
Tobias Placht <tobias.placht@student.hswt.de>
2013-04-27 16:07:55 +0200

    add comment

--- de.hswt.hrm.main/src/de/hswt/hrm/main/handlers/LifeCycleHandler.java
+++ de.hswt.hrm.main/src/de/hswt/hrm/main/handlers/LifeCycleHandler.java
@@ -7,15 +7,14 @@ import de.hswt.hrm.common.Hrm;
 public class LifeCycleHandler {
 
     /*
-     * This method is called before the Eclipse Context is created and therefore before the GUI is
-     * started. 
+     * This method is called before the Eclipse Context is created
      */
     @PostContextCreate
     public void init() {
 
-       /*
-        * Getting the Database Connection
-        */
+        /*
+         * Getting the Database Connection
+         */
         Hrm.init();
     }
 
