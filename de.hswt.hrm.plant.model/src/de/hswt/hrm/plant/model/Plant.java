2d430bd208e8f4cf574b8fc7c67f44bbabcaa608
Tobias Placht <tobias.placht@student.hswt.de>
2013-05-02 18:16:59 +0200

    add generic getter/setter

--- de.hswt.hrm.plant.model/src/de/hswt/hrm/plant/model/Plant.java
+++ de.hswt.hrm.plant.model/src/de/hswt/hrm/plant/model/Plant.java
@@ -32,6 +32,133 @@ public class Plant {
     private String ventilatorPerformance;
     private String current;
     private String voltage;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getInspectionInterval() {
+        return inspectionInterval;
+    }
+
+    public void setInspectionInterval(int inspectionInterval) {
+        this.inspectionInterval = inspectionInterval;
+    }
+
+    public int getNumberOfElements() {
+        return numberOfElements;
+    }
+
+    public void setNumberOfElements(int numberOfElements) {
+        this.numberOfElements = numberOfElements;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Place getPlace() {
+        return place;
+    }
+
+    public void setPlace(Place place) {
+        this.place = place;
+    }
+
+    public int getConstructionYear() {
+        return constructionYear;
+    }
+
+    public void setConstructionYear(int constructionYear) {
+        this.constructionYear = constructionYear;
+    }
+
+    public String getManufactor() {
+        return manufactor;
+    }
+
+    public void setManufactor(String manufactor) {
+        this.manufactor = manufactor;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getAirPerformance() {
+        return airPerformance;
+    }
+
+    public void setAirPerformance(String airPerformance) {
+        this.airPerformance = airPerformance;
+    }
+
+    public String getMotorPower() {
+        return motorPower;
+    }
+
+    public void setMotorPower(String motorPower) {
+        this.motorPower = motorPower;
+    }
+
+    public String getMotorRpm() {
+        return motorRpm;
+    }
+
+    public void setMotorRpm(String motorRpm) {
+        this.motorRpm = motorRpm;
+    }
+
+    public String getVentilatorPerformance() {
+        return ventilatorPerformance;
+    }
+
+    public void setVentilatorPerformance(String ventilatorPerformance) {
+        this.ventilatorPerformance = ventilatorPerformance;
+    }
+
+    public String getCurrent() {
+        return current;
+    }
+
+    public void setCurrent(String current) {
+        this.current = current;
+    }
+
+    public String getVoltage() {
+        return voltage;
+    }
+
+    public void setVoltage(String voltage) {
+        this.voltage = voltage;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public static String getIsMandatory() {
+        return IS_MANDATORY;
+    }
+
     private String note;
+
+    private static final String IS_MANDATORY = "Field is a mandatory.";
 
 }
