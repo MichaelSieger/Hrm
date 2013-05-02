54d26c7946bc4dec44cc28e5646bf15bd835027c
haeringl <luke-haering@web.de>
2013-05-02 18:03:41 +0200

    add basic methods in plant.dao.core

--- de.hswt.hrm.plant.dao.core/src/de/hswt/hrm/plant/dao/core/IPlantDao.java
+++ de.hswt.hrm.plant.dao.core/src/de/hswt/hrm/plant/dao/core/IPlantDao.java
@@ -1,5 +1,43 @@
 package de.hswt.hrm.plant.dao.core;
 
-public interface IPlantDao {
+import java.util.Collection;
 
+import de.hswt.hrm.common.database.exception.DatabaseException;
+import de.hswt.hrm.common.database.exception.ElementNotFoundException;
+import de.hswt.hrm.common.database.exception.SaveException;
+import de.hswt.hrm.plant.model.Plant;
+
+/**
+ * Defines all the public methods to interact with the storage system for plants.
+ */
+public interface IPlantDao {
+    /**
+     * @return All plants from storage.
+     */
+    Collection<Plant> findAll() throws DatabaseException;
+    
+    /**
+     * @param id of the target plant.
+     * @return Plant with the given id.
+     * @throws ElementNotFoundException If the given id is not present in storage.
+     */
+    Plant findById(int id) throws DatabaseException, ElementNotFoundException;
+    
+    /**
+     * Add a new plant to storage.
+     * 
+     * @param plant Plant that should be stored.
+     * @return Newly generated plant (also holding the correct id).
+     * @throws SaveException If the plant could not be inserted.
+     */
+    Plant insert(Plant plant) throws SaveException;
+    
+    /**
+     * Update an existing plant in storage.
+     * 
+     * @param plant Plant that should be updated.
+     * @throws ElementNotFoundException If the given plant is not present in the database.
+     * @throws SaveException If the plant could not be updated.
+     */
+    void update(Plant plant) throws ElementNotFoundException, SaveException;
 }
