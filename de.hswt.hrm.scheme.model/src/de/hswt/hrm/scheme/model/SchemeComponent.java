28ae8228c26a707728e971643c699ebee57c2d02
Michael Sieger <michael.sieger@student.hswt.de>
2013-05-02 10:14:44 +0200

    id is immutable

--- de.hswt.hrm.scheme.model/src/de/hswt/hrm/scheme/model/SchemeComponent.java
+++ de.hswt.hrm.scheme.model/src/de/hswt/hrm/scheme/model/SchemeComponent.java
@@ -29,10 +29,6 @@ public class SchemeComponent {
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
     public int getX() {
         checkArgument(x < 0);
         return x;
