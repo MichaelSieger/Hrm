b2b15c70aed8065090de9fcc78efc218f8b19ef0
Michi <michael.sieger@student.hswt.de>
2013-05-03 18:07:29 +0200

    hashcode, equals

--- de.hswt.hrm.scheme.model/src/de/hswt/hrm/scheme/model/Category.java
+++ de.hswt.hrm.scheme.model/src/de/hswt/hrm/scheme/model/Category.java
@@ -78,4 +78,46 @@ public class Category {
 		return id;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + height;
+		result = prime * result + id;
+		result = prime * result + (isRated ? 1231 : 1237);
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + qualifier;
+		result = prime * result + width;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Category other = (Category) obj;
+		if (height != other.height)
+			return false;
+		if (id != other.id)
+			return false;
+		if (isRated != other.isRated)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (qualifier != other.qualifier)
+			return false;
+		if (width != other.width)
+			return false;
+		return true;
+	}
+	
+	
+
 }
