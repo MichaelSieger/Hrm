53d78ca39cccb2e2a162a2f5e6cf0e1cc9aab430
Michi <michael.sieger@student.hswt.de>
2013-05-03 17:04:39 +0200

    Give the tree structure

--- de.hswt.hrm.scheme.ui/src/de/hswt/hrm/scheme/ui/TreeManager.java
+++ de.hswt.hrm.scheme.ui/src/de/hswt/hrm/scheme/ui/TreeManager.java
@@ -1,10 +1,14 @@
 package de.hswt.hrm.scheme.ui;
 
+import java.util.HashMap;
+
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Tree;
 import org.eclipse.swt.widgets.TreeItem;
 
-import de.hswt.hrm.scheme.model.RenderedGridImage;
+import de.hswt.hrm.scheme.model.Category;
+import de.hswt.hrm.scheme.model.RenderedComponent;
+import de.hswt.hrm.scheme.model.ThumbnailImage;
 
 /**
  * This class manages the TreeItems of a Tree that displays the GridImages.
@@ -27,10 +31,27 @@ public class TreeManager {
 
     private void generateTreeItems(){
 		tree.clearAll(true);
-		for(RenderedGridImage img : model.getImages()){
-			TreeItem item = new TreeItem(tree, SWT.NONE);
-			item.setImage(img.getThumbnail());
-			item.setData(img);
+		HashMap<Category, TreeItem> categoryItems = new HashMap<>();
+		
+		for(RenderedComponent comp : model.getImages()){
+			Category c = comp.getComponent().getCategory();
+			TreeItem categoryItem = categoryItems.get(c);
+			if(categoryItem == null){
+				categoryItem = new TreeItem(tree, SWT.NONE);
+				categoryItems.put(c, categoryItem);
+			}
+			TreeItem item = new TreeItem(categoryItem, SWT.NONE);
+			item.setText(c.getName());
+			addImage(comp.getDownUp(), comp, item);
+			addImage(comp.getLeftRight(), comp, item);
+			addImage(comp.getRightLeft(), comp, item);
+			addImage(comp.getTopDown(), comp, item);
 		}
 	}
+    
+    private void addImage(ThumbnailImage img, RenderedComponent comp, TreeItem parent){
+    	TreeItem item = new TreeItem(parent, SWT.NONE);
+    	item.setImage(img.getThumbnail());
+    	item.setData(parent);
+    }
 }
