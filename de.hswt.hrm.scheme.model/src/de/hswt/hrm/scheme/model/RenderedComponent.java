6b026531bef5ae90f0e2a81a566e45d79a5ba925
Michi <michael.sieger@student.hswt.de>
2013-05-03 16:26:55 +0200

    RenderedComponent

---
+++ de.hswt.hrm.scheme.model/src/de/hswt/hrm/scheme/model/RenderedComponent.java
@@ -0,0 +1,48 @@
+package de.hswt.hrm.scheme.model;
+
+/**
+ * Represents a Component with all images beeing rendered
+ * 
+ * @author Michael Sieger
+ *
+ */
+public class RenderedComponent {
+	
+	private final Component component;
+	private final ThumbnailImage leftRight;
+	private final ThumbnailImage rightLeft;
+	private final ThumbnailImage topDown;
+	private final ThumbnailImage downUp;
+	
+	public RenderedComponent(Component component, ThumbnailImage leftRight,
+			ThumbnailImage rightLeft, ThumbnailImage topDown,
+			ThumbnailImage downUp) {
+		this.component = component;
+		this.leftRight = leftRight;
+		this.rightLeft = rightLeft;
+		this.topDown = topDown;
+		this.downUp = downUp;
+	}
+
+	public Component getComponent() {
+		return component;
+	}
+
+	public ThumbnailImage getLeftRight() {
+		return leftRight;
+	}
+
+	public ThumbnailImage getRightLeft() {
+		return rightLeft;
+	}
+
+	public ThumbnailImage getTopDown() {
+		return topDown;
+	}
+
+	public ThumbnailImage getDownUp() {
+		return downUp;
+	}
+
+	
+}
