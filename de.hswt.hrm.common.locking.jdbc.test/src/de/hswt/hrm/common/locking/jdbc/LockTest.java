61a5f75689f2ad90dd63ef76853eac28df744745
Ben <benjamin.pabst85@gmail.com>
2013-05-03 18:31:31 +0200

    Add testcase to check if native locking is enabled in MySQL / MariaDB in common.locking.jdbc.test.

--- de.hswt.hrm.common.locking.jdbc.test/src/de/hswt/hrm/common/locking/jdbc/LockTest.java
+++ de.hswt.hrm.common.locking.jdbc.test/src/de/hswt/hrm/common/locking/jdbc/LockTest.java
@@ -1,5 +1,9 @@
 package de.hswt.hrm.common.locking.jdbc;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.UUID;
 
 import org.junit.Test;
@@ -7,6 +11,8 @@ import static org.junit.Assert.*;
 
 import com.google.common.base.Optional;
 
+import de.hswt.hrm.common.database.DatabaseFactory;
+import de.hswt.hrm.common.database.exception.DatabaseException;
 import de.hswt.hrm.test.database.AbstractDatabaseTest;
 
 public class LockTest extends AbstractDatabaseTest {
@@ -43,6 +49,61 @@ public class LockTest extends AbstractDatabaseTest {
         
         lock = service.getLock(LockService.TBL_PLANT, 1);
         assertTrue("Could not get lock for released id.", lock.isPresent());
+    }
+    
+//    @Test(expected=SQLException.class)
+    @Test
+    public void testDatabaseNativeLock() throws DatabaseException, SQLException {
+        // This test ensures that the database has enabled auto locking per row
+        // this is MySQL / MariaDB specific feature of the InnoDB driver
+        // because of this, the test should maybe be moved to an extra fragment
+        // TODO: check if test could be moved to extra fragment or similar
+        
+        Connection outerCon = DatabaseFactory.getConnection();
+        outerCon.setAutoCommit(false);
+        PreparedStatement stmt = outerCon.prepareStatement(
+                "INSERT INTO `Lock` (Lock_Uuid_Fk, Lock_Table, Lock_Row_ID) "
+                        + "VALUES (?, ?, ?);");
+        stmt.setString(1, SESSION);
+        stmt.setString(2, PLANT);
+        stmt.setInt(3, 1);
+        assertEquals("Could not insert new lock.", 1, stmt.executeUpdate());
+        stmt.close();
+        outerCon.commit();
+        
+        
+        // Lock lock
+        stmt = outerCon.prepareStatement(
+                "SELECT * FROM `Lock` WHERE Lock_Uuid_Fk = ? "
+                        + "AND Lock_Table = ? "
+                        + "AND Lock_Row_ID = ? "
+                        + "LOCK IN SHARE MODE;");
+        stmt.setString(1, SESSION);
+        stmt.setString(2, PLANT);
+        stmt.setInt(3, 1);
+        ResultSet r = stmt.executeQuery();
+        stmt.close();
+        assertTrue("Could not retrieve lock.", r.next());
+        
+        // Inner Connection -> outer connection didn't close the transaction
+        // so the line should be locked!
+        Connection innerCon = DatabaseFactory.getConnection();
+        stmt = innerCon.prepareStatement(
+                "DELETE FROM `Lock` "
+                        + "WHERE Lock_Uuid_Fk = ? "
+                        + "AND Lock_Table = ? "
+                        + "AND Lock_Row_ID = ?;");
+        stmt.setString(1, SESSION);
+        stmt.setString(2, PLANT);
+        stmt.setInt(3, 1);
+        assertEquals("Statement should not affect any rows.", 0 , stmt.executeUpdate());
+        
+        stmt.close();
+        r.close();
+        
+        innerCon.close();
+        outerCon.rollback();
+        outerCon.close();
     }
     
 }
