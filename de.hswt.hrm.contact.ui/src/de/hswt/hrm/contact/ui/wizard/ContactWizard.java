6242e38315aa32a2fa1e0d6589fbd97341c256a0
Tobias Placht <tobias.placht@student.hswt.de>
2013-05-05 17:46:38 +0200

    more refactoring

--- de.hswt.hrm.contact.ui/src/de/hswt/hrm/contact/ui/wizard/ContactWizard.java
+++ de.hswt.hrm.contact.ui/src/de/hswt/hrm/contact/ui/wizard/ContactWizard.java
@@ -10,7 +10,6 @@ import org.slf4j.LoggerFactory;
 import com.google.common.base.Optional;
 
 import de.hswt.hrm.common.database.exception.DatabaseException;
-import de.hswt.hrm.common.database.exception.ElementNotFoundException;
 import de.hswt.hrm.common.database.exception.SaveException;
 import de.hswt.hrm.contact.model.Contact;
 import de.hswt.hrm.contact.service.ContactService;
@@ -31,7 +30,6 @@ public class ContactWizard extends Wizard {
 
     @Override
     public void addPages() {
-
         addPage(first);
     }
 
@@ -42,64 +40,23 @@ public class ContactWizard extends Wizard {
 
     @Override
     public boolean performFinish() {
-
         if (contact.isPresent()) {
             return editExistingCustomer();
         }
-
         return insertNewContact();
     }
 
     private boolean editExistingCustomer() {
-
         Contact c = this.contact.get();
         try {
-
             c = ContactService.findById(c.getId());
+            setContent(contact);
+            ContactService.update(c);
+            contact = Optional.fromNullable(c);
 
         }
         catch (DatabaseException e1) {
-
-            LOG.error("Could not find Contact with ID: " + c.getId(), e1);
-        }
-
-        HashMap<String, Text> mandatoryWidgets = first.getMandatoryWidgets();
-        String firstName = mandatoryWidgets.get("firstName").getText();
-        String lastName = mandatoryWidgets.get("lastName").getText();
-        String street = mandatoryWidgets.get("street").getText();
-        String streetNumber = mandatoryWidgets.get("streetNumber").getText();
-        String city = mandatoryWidgets.get("city").getText();
-        String zipCode = mandatoryWidgets.get("zipCode").getText();
-
-        c.setFirstName(firstName);
-        c.setLastName(lastName);
-        c.setStreet(street);
-        c.setStreetNo(streetNumber);
-        c.setCity(city);
-        c.setPostCode(zipCode);
-
-        HashMap<String, Text> optionalWidgets = first.getOptionalWidgets();
-        String shortcut = optionalWidgets.get("shortcut").getText();
-        String phone = optionalWidgets.get("phone").getText();
-        String fax = optionalWidgets.get("fax").getText();
-        String mobilePhone = optionalWidgets.get("mobilePhone").getText();
-        String email = optionalWidgets.get("email").getText();
-
-        c.setShortcut(shortcut);
-        c.setPhone(phone);
-        c.setMobile(mobilePhone);
-        c.setFax(fax);
-        c.setEmail(email);
-
-        try {
-            ContactService.update(c);
-            contact = Optional.fromNullable(c);
-        }
-        catch (ElementNotFoundException e) {
-            LOG.error("Element " + contact + " not found in Database", e);
-        }
-        catch (SaveException e) {
-            LOG.error("Could not save Element: " + contact + "into Database", e);
+            LOG.error("An error occured", e1);
         }
 
         return true;
@@ -107,28 +64,8 @@ public class ContactWizard extends Wizard {
     }
 
     private boolean insertNewContact() {
-        HashMap<String, Text> mandatoryWidgets = first.getMandatoryWidgets();
-        String firstName = mandatoryWidgets.get("firstName").getText();
-        String lastName = mandatoryWidgets.get("lastName").getText();
-        String street = mandatoryWidgets.get("street").getText();
-        String streetNumber = mandatoryWidgets.get("streetNumber").getText();
-        String city = mandatoryWidgets.get("city").getText();
-        String zipCode = mandatoryWidgets.get("zipCode").getText();
-
-        Contact c = new Contact(lastName, firstName, street, streetNumber, zipCode, city);
-
-        HashMap<String, Text> optionalWidgets = first.getOptionalWidgets();
-        String shortcut = optionalWidgets.get("shortcut").getText();
-        String phone = optionalWidgets.get("phone").getText();
-        String fax = optionalWidgets.get("fax").getText();
-        String mobilePhone = optionalWidgets.get("mobilePhone").getText();
-        String email = optionalWidgets.get("email").getText();
-
-        c.setShortcut(shortcut);
-        c.setPhone(phone);
-        c.setMobile(mobilePhone);
-        c.setFax(fax);
-        c.setEmail(email);
+        Contact c = null;
+        c = setContent(Optional.fromNullable(c));
 
         try {
             c = ContactService.insert(c);
@@ -143,6 +80,50 @@ public class ContactWizard extends Wizard {
 
     public Optional<Contact> getContact() {
         return contact;
+    }
+
+    private Contact setContent(Optional<Contact> c) {
+
+        Contact contact;
+
+        HashMap<String, Text> mandatoryWidgets = first.getMandatoryWidgets();
+        String firstName = mandatoryWidgets.get("firstName").getText();
+        String lastName = mandatoryWidgets.get("lastName").getText();
+        String street = mandatoryWidgets.get("street").getText();
+        String streetNumber = mandatoryWidgets.get("streetNumber").getText();
+        String city = mandatoryWidgets.get("city").getText();
+        String zipCode = mandatoryWidgets.get("zipCode").getText();
+
+        HashMap<String, Text> optionalWidgets = first.getOptionalWidgets();
+        String shortcut = optionalWidgets.get("shortcut").getText();
+        String phone = optionalWidgets.get("phone").getText();
+        String fax = optionalWidgets.get("fax").getText();
+        String mobilePhone = optionalWidgets.get("mobilePhone").getText();
+        String email = optionalWidgets.get("email").getText();
+
+        if (c.isPresent()) {
+
+            contact = c.get();
+
+            contact.setFirstName(firstName);
+            contact.setLastName(lastName);
+            contact.setStreet(street);
+            contact.setStreetNo(streetNumber);
+            contact.setCity(city);
+            contact.setPostCode(zipCode);
+        }
+
+        else {
+            contact = new Contact(lastName, firstName, street, streetNumber, zipCode, city);
+        }
+        contact.setShortcut(shortcut);
+        contact.setPhone(phone);
+        contact.setMobile(mobilePhone);
+        contact.setFax(fax);
+        contact.setEmail(email);
+
+        return contact;
+
     }
 
 }
