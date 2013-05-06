97d750231ca25f772a20f769287a136a58fc108d
haeringl <luke-haering@web.de>
2013-05-06 18:00:25 +0200

    add fromResultSet in PlantDao.java

--- de.hswt.hrm.plant.dao.jdbc/src/de/hswt/hrm/plant/dao/jdbc/PlantDao.java
+++ de.hswt.hrm.plant.dao.jdbc/src/de/hswt/hrm/plant/dao/jdbc/PlantDao.java
@@ -3,9 +3,12 @@ package de.hswt.hrm.plant.dao.jdbc;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Collection;
 
 import org.apache.commons.dbutils.DbUtils;
+
+import static com.google.common.base.Preconditions.*;
 
 import de.hswt.hrm.common.database.DatabaseFactory;
 import de.hswt.hrm.common.database.NamedParameterStatement;
@@ -59,38 +62,37 @@ public class PlantDao implements IPlantDao {
     }
 
     private Collection<Plant> fromResultSet(ResultSet rs) throws SQLException {
-//        checkNotNull(rs, "Result must not be null.");
-//        Collection<Plant> plantList = new ArrayList<>();
-//
-//        while (rs.next()) {
-////            "SELECT Plant_ID, Plant_Place_FK, Plant_Inspection_Intervall, "
-////                    + "Plant_Manufacturer, Plant_Year_Of_Construction, Plant_Type"
-////                    + "Plant_Airperformance, Plant_Motorpower, Plant_Motor_Rpm, Plant_Ventilatorperformance, "
-////                    + "Plant_Current, Plant_Voltage, Plant_Note, Plant_Description FROM Plant ;";
-//
-//            int id = rs.getInt("plant_ID");
-//            Place plantPlace = 
-//            int inspectionIntervall = rs.getInt("Plant_Inspection_Intervall");
-//            String description = rs.getString("Plant_Description");
-//
-//
-//            Plant plant = new Plant(id, inspectionIntervall, description, plantPlace);
-//            plant.setConstructionYear(rs.getInt("Plant_Year_Of_Construction"));
-//            plant.setManufactor(rs.getString("Plant_Manufacturer"));
-//            plant.setType(rs.getString("Plant_Type"));
-//            plant.setAirPerformance(rs.getString("Plant_Airperformance"));
-//            plant.setMotorPower(rs.getString("Plant_Motorpower"));
-//            plant.setMotorRpm(rs.getString("Plant_Motor_Rpm"));
-//            plant.setVentilatorPerformance(rs.getString("Plant_Ventilatorperformance"));
-//            plant.setCurrent(rs.getString("Plant_Current"));
-//            plant.setVoltage(rs.getString("Plant_Voltage"));
-//            plant.setNote(rs.getString("Plant_Note"));
-//            
-//            plantList.add(plant);
-//        }
-//
-//        return plantList;
+        checkNotNull(rs, "Result must not be null.");
+        Collection<Plant> plantList = new ArrayList<>();
 
-        throw new NotImplementedException();
+        while (rs.next()) {
+//            "SELECT Plant_ID, Plant_Place_FK, Plant_Inspection_Intervall, "
+//                    + "Plant_Manufacturer, Plant_Year_Of_Construction, Plant_Type"
+//                    + "Plant_Airperformance, Plant_Motorpower, Plant_Motor_Rpm, Plant_Ventilatorperformance, "
+//                    + "Plant_Current, Plant_Voltage, Plant_Note, Plant_Description FROM Plant ;";
+
+            int id = rs.getInt("plant_ID");
+            int inspectionIntervall = rs.getInt("Plant_Inspection_Intervall");
+            String description = rs.getString("Plant_Description");
+
+
+            Plant plant = new Plant(id, inspectionIntervall, description);
+            plant.setConstructionYear(rs.getInt("Plant_Year_Of_Construction"));
+            plant.setManufactor(rs.getString("Plant_Manufacturer"));
+            plant.setType(rs.getString("Plant_Type"));
+            plant.setAirPerformance(rs.getString("Plant_Airperformance"));
+            plant.setMotorPower(rs.getString("Plant_Motorpower"));
+            plant.setMotorRpm(rs.getString("Plant_Motor_Rpm"));
+            plant.setVentilatorPerformance(rs.getString("Plant_Ventilatorperformance"));
+            plant.setCurrent(rs.getString("Plant_Current"));
+            plant.setVoltage(rs.getString("Plant_Voltage"));
+            plant.setNote(rs.getString("Plant_Note"));
+            
+            plantList.add(plant);
+        }
+
+        return plantList;
+
+
     }
 }
