fdbcd1cdf56aeb940728e5dc91512fca7fc4e2b3
Ben <benjamin.pabst85@gmail.com>
2013-05-07 17:22:10 +0200

    Refactor place part to use simplified version of the table viewer in place.ui.

--- de.hswt.hrm.place.ui/src/de/hswt/hrm/place/ui/part/PlacePart.java
+++ de.hswt.hrm.place.ui/src/de/hswt/hrm/place/ui/part/PlacePart.java
@@ -1,11 +1,8 @@
 package de.hswt.hrm.place.ui.part;
 
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Comparator;
 import java.util.List;
-import java.util.Map;
 
 import javax.annotation.PostConstruct;
 
@@ -13,10 +10,8 @@ import org.eclipse.e4.xwt.IConstants;
 import org.eclipse.e4.xwt.XWT;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ArrayContentProvider;
-import org.eclipse.jface.viewers.ColumnLabelProvider;
 import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.TableColumn;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -67,134 +62,23 @@ public class PlacePart {
 	}
 	
 	private void initializeTable(Composite parent, TableViewer viewer) {
-	    
-	    
-	    // Create columns
-	    List<ColumnDescription<Place>> columns = new ArrayList<>();
-	    columns.add(new ColumnDescription<Place>(
-	    		"Place Name",
-	    		new ColumnLabelProvider() {
-	    			@Override
-	    			public String getText(Object element) {
-	    				Place p = (Place) element;
-	    				return p.getPlaceName();
-	    			}
-	    		}, 
-	    		new Comparator<Place>() {
+	    List<ColumnDescription<Place>> columns = PlacePartUtil.getColumns();
 
-	    			@Override
-	    			public int compare(Place o1, Place o2) {
-	    				return o1.getPlaceName().compareTo(o2.getPlaceName());
-	    			}
-
-	    		}));
-	    
-	    columns.add(new ColumnDescription<>(
-	    		"Postcode",
-	    		new ColumnLabelProvider() {
-	    			@Override
-	    			public String getText(Object element) {
-	    				Place p = (Place) element;
-	    				return p.getPostCode();
-	    			}
-	    		},
-	    		new Comparator<Place>() {
-	    			@Override
-	    			public int compare(Place o1, Place o2) {
-	    				return o1.getPostCode().compareTo(o2.getPostCode());
-	    			}
-				}));
-	    
-	    columns.add(new ColumnDescription<>(
-	    		"City",
-	    		new ColumnLabelProvider() {
-	    			@Override
-	    			public String getText(Object element) {
-	    				Place p = (Place) element;
-	    				return p.getCity();
-	    			}
-	    		},
-	    		new Comparator<Place>() {
-	    			@Override
-	    			public int compare(Place o1, Place o2) {
-	    				return o1.getCity().compareTo(o2.getCity());
-	    			}
-				}));
-	    
-	    columns.add(new ColumnDescription<>(
-	    		"Street",
-	    		new ColumnLabelProvider() {
-	    			@Override
-	    			public String getText(Object element) {
-	    				Place p = (Place) element;
-	    				return p.getStreet();
-	    			}
-	    		},
-	    		new Comparator<Place>() {
-	    			@Override
-	    			public int compare(Place o1, Place o2) {
-	    				return o1.getStreet().compareTo(o2.getStreet());
-	    			}
-				}));
-	    
-	    columns.add(new ColumnDescription<>(
-	    		"Street Number",
-	    		new ColumnLabelProvider() {
-	    			@Override
-	    			public String getText(Object element) {
-	    				Place p = (Place) element;
-	    				return p.getStreetNo();
-	    			}
-	    		},
-	    		new Comparator<Place>() {
-	    			@Override
-	    			public int compare(Place o1, Place o2) {
-	    				return o1.getStreetNo().compareTo(o2.getStreetNo());
-	    			}
-				}));
-	    
-	    columns.add(new ColumnDescription<>(
-	    		"Location",
-	    		new ColumnLabelProvider() {
-	    			@Override
-	    			public String getText(Object element) {
-	    				Place p = (Place) element;
-	    				return p.getLocation();
-	    			}
-	    		},
-	    		new Comparator<Place>() {
-	    			@Override
-	    			public int compare(Place o1, Place o2) {
-	    				return o1.getLocation().compareTo(o2.getLocation());
-	    			}
-				}));
-	    
-	    columns.add(new ColumnDescription<>(
-	    		"Area",
-	    		new ColumnLabelProvider() {
-	    			@Override
-	    			public String getText(Object element) {
-	    				Place p = (Place) element;
-	    				return p.getArea();
-	    			}
-	    		},
-	    		new Comparator<Place>() {
-	    			@Override
-	    			public int compare(Place o1, Place o2) {
-	    				return o1.getArea().compareTo(o2.getArea());
-	    			}
-				}));
-	    
-	    Map<TableColumn, Comparator<Place>> comparators;
-	    
+	    // Create columns in tableviewer
 	    TableViewerController<Place> filler = new TableViewerController<>(viewer);
-	    comparators = filler.createColumns(columns);
+	    filler.createColumns(columns);
+	    
+	    // Enable column selection
 	    filler.createColumnSelectionMenu();
 	    
-	    ColumnComparator<Place> comparator = new ColumnComparator<>(comparators);
+	    // Enable sorting
+	    ColumnComparator<Place> comparator = new ColumnComparator<>(columns);
 	    filler.enableSorting(comparator);
 	    
+	    // Add dataprovider that handles our collection
         viewer.setContentProvider(ArrayContentProvider.getInstance());
+        
+        //Enable filtering
         viewer.addFilter(new PlaceFilter());
 	}
 
