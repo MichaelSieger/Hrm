7566d40a5dbc3d493953770e7086aa8330033561
haeringl <luke-haering@web.de>
2013-05-08 16:30:23 +0200

    add basic methods ITargetDao.java

--- de.hswt.hrm.catalog.dao.core/src/de/hswt/hrm/catalog/dao/core/ITargetDao.java
+++ de.hswt.hrm.catalog.dao.core/src/de/hswt/hrm/catalog/dao/core/ITargetDao.java
@@ -1,6 +1,44 @@
 package de.hswt.hrm.catalog.dao.core;
 
-//TODO Rechtsklick auf die Klasse -> Refactor -> Rename -> Richtigen Namen vergeben
-public interface ITargetDao {
+import java.util.Collection;
 
+import de.hswt.hrm.common.database.exception.DatabaseException;
+import de.hswt.hrm.common.database.exception.ElementNotFoundException;
+import de.hswt.hrm.common.database.exception.SaveException;
+import de.hswt.hrm.catalog.model.Target;
+
+/**
+ * Defines all the public methods to interact with the storage system for targets.
+ */
+public interface ITargetDao {
+    
+    /**
+     * @return All targets from storage.
+     */
+    Collection<Target> findAll() throws DatabaseException;
+    
+    /**
+     * @param id of the target target.
+     * @return Target with the given id.
+     * @throws ElementNotFoundException If the given id is not present in the database.
+     */
+    Target findById(int id) throws DatabaseException, ElementNotFoundException;
+    
+    /**
+     * Add a new target to storage.
+     * 
+     * @param target Target that should be stored.
+     * @return Newly generated target (also holding the correct id).
+     * @throws SaveException If the target could not be inserted.
+     */
+    Target insert(Target target) throws SaveException;
+    
+    /**
+     * Update an existing target in storage.
+     * 
+     * @param target Target that should be updated.
+     * @throws ElementNotFoundException If the given target is not present in the database.
+     * @throws SaveException If the target could not be updated.
+     */
+    void update(Target target) throws ElementNotFoundException, SaveException;
 }
