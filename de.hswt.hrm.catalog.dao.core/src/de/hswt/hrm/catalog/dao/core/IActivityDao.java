b195d4986fbc89c92ecbbedd07151ace0e6fa7fb
haeringl <luke-haering@web.de>
2013-05-08 16:29:49 +0200

    add basic methods IActivityDao.java

--- de.hswt.hrm.catalog.dao.core/src/de/hswt/hrm/catalog/dao/core/IActivityDao.java
+++ de.hswt.hrm.catalog.dao.core/src/de/hswt/hrm/catalog/dao/core/IActivityDao.java
@@ -1,6 +1,44 @@
 package de.hswt.hrm.catalog.dao.core;
 
-//TODO Rechtsklick auf die Klasse -> Refactor -> Rename -> Richtigen Namen vergeben
-public interface IActivityDao {
+import java.util.Collection;
 
+import de.hswt.hrm.common.database.exception.DatabaseException;
+import de.hswt.hrm.common.database.exception.ElementNotFoundException;
+import de.hswt.hrm.common.database.exception.SaveException;
+import de.hswt.hrm.catalog.model.Activity;
+
+/**
+ * Defines all the public methods to interact with the storage system for activitys.
+ */
+public interface IActivityDao {
+    
+    /**
+     * @return All activitys from storage.
+     */
+    Collection<Activity> findAll() throws DatabaseException;
+    
+    /**
+     * @param id of the target activity.
+     * @return Activity with the given id.
+     * @throws ElementNotFoundException If the given id is not present in the database.
+     */
+    Activity findById(int id) throws DatabaseException, ElementNotFoundException;
+    
+    /**
+     * Add a new activity to storage.
+     * 
+     * @param activity Activity that should be stored.
+     * @return Newly generated activity (also holding the correct id).
+     * @throws SaveException If the activity could not be inserted.
+     */
+    Activity insert(Activity activity) throws SaveException;
+    
+    /**
+     * Update an existing activity in storage.
+     * 
+     * @param activity Activity that should be updated.
+     * @throws ElementNotFoundException If the given activity is not present in the database.
+     * @throws SaveException If the activity could not be updated.
+     */
+    void update(Activity activity) throws ElementNotFoundException, SaveException;
 }
