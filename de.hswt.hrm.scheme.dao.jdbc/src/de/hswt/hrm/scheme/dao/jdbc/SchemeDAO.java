513912adcc3982d3d7dbd0d0a8a367dfc80d8010
Michael Sieger <michael.sieger@student.hswt.de>
2013-05-08 12:38:04 +0200

    add plugins: scheme.service.test, scheme.dao.core, scheme.dao.jdbc

---
+++ de.hswt.hrm.scheme.dao.jdbc/src/de/hswt/hrm/scheme/dao/jdbc/SchemeDAO.java
@@ -0,0 +1,7 @@
+package de.hswt.hrm.scheme.dao.jdbc;
+
+import de.hswt.hrm.scheme.dao.core.ISchemeDAO;
+
+public class SchemeDAO implements ISchemeDAO{
+
+}
---
+++ de.hswt.hrm.scheme.service.test/src/de/hswt/hrm/scheme/service/test/SchemeServiceTest.java
@@ -0,0 +1,12 @@
+package de.hswt.hrm.scheme.service.test;
+
+import org.junit.Test;
+
+public class SchemeServiceTest {
+    
+    @Test
+    public void blatest(){
+        
+    }
+
+}
