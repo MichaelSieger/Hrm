3ae3abaf30e891245604762e87991e801a8fee57
haeringl <luke-haering@web.de>
2013-05-09 17:18:44 +0200

    add basic methods in TargetDao.java

--- de.hswt.hrm.catalog.dao.jdbc/src/de/hswt/hrm/catalog/dao/jdbc/TargetDao.java
+++ de.hswt.hrm.catalog.dao.jdbc/src/de/hswt/hrm/catalog/dao/jdbc/TargetDao.java
@@ -1,6 +1,177 @@
 package de.hswt.hrm.catalog.dao.jdbc;
 
-//TODO Rechtsklick auf die Klasse -> Refactor -> Rename -> Richtigen Namen vergeben
-public class TargetDao {
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collection;
 
+import org.apache.commons.dbutils.DbUtils;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import de.hswt.hrm.common.database.DatabaseFactory;
+import de.hswt.hrm.common.database.NamedParameterStatement;
+import de.hswt.hrm.common.database.SqlQueryBuilder;
+import de.hswt.hrm.common.database.exception.DatabaseException;
+import de.hswt.hrm.common.database.exception.ElementNotFoundException;
+import de.hswt.hrm.common.database.exception.SaveException;
+import de.hswt.hrm.catalog.model.Target;
+import de.hswt.hrm.catalog.dao.core.ITargetDao;
+
+public class TargetDao implements ITargetDao {
+
+    @Override
+    public Collection<Target> findAll() throws DatabaseException {
+        SqlQueryBuilder builder = new SqlQueryBuilder();
+        builder.select(TABLE_NAME, Fields.ID, Fields.NAME, Fields.TEXT);
+
+        final String query = builder.toString();
+
+        try (Connection con = DatabaseFactory.getConnection()) {
+            try (NamedParameterStatement stmt = NamedParameterStatement.fromConnection(con, query)) {
+                ResultSet result = stmt.executeQuery();
+
+                Collection<Target> places = fromResultSet(result);
+                DbUtils.closeQuietly(result);
+
+                return places;
+            }
+        }
+        catch (SQLException e) {
+            throw new DatabaseException(e);
+        }
+    }
+
+    @Override
+    public Target findById(int id) throws DatabaseException, ElementNotFoundException {
+        checkArgument(id >= 0, "Id must not be negative.");
+
+        SqlQueryBuilder builder = new SqlQueryBuilder();
+        builder.select(TABLE_NAME, Fields.ID, Fields.NAME, Fields.TEXT);
+        builder.where(Fields.ID);
+
+        final String query = builder.toString();
+
+        try (Connection con = DatabaseFactory.getConnection()) {
+            try (NamedParameterStatement stmt = NamedParameterStatement.fromConnection(con, query)) {
+                stmt.setParameter(Fields.ID, id);
+                ResultSet result = stmt.executeQuery();
+
+                Collection<Target> activities = fromResultSet(result);
+                DbUtils.closeQuietly(result);
+
+                if (activities.size() < 1) {
+                    throw new ElementNotFoundException();
+                }
+                else if (activities.size() > 1) {
+                    throw new DatabaseException("ID '" + id + "' is not unique.");
+                }
+
+                return activities.iterator().next();
+            }
+        }
+        catch (SQLException e) {
+            throw new DatabaseException(e);
+        }
+    }
+
+    /**
+     * @see {@link IPlaceDao#insert(Place)}
+     */
+    @Override
+    public Target insert(Target target) throws SaveException {
+        SqlQueryBuilder builder = new SqlQueryBuilder();
+        builder.insert(TABLE_NAME, Fields.NAME, Fields.TEXT);
+
+        final String query = builder.toString();
+
+        try (Connection con = DatabaseFactory.getConnection()) {
+            try (NamedParameterStatement stmt = NamedParameterStatement.fromConnection(con, query)) {
+                stmt.setParameter(Fields.NAME, target.getName());
+                stmt.setParameter(Fields.TEXT, target.getText());
+
+                int affectedRows = stmt.executeUpdate();
+                if (affectedRows != 1) {
+                    throw new SaveException();
+                }
+
+                try (ResultSet generatedKeys = stmt.getGeneratedKeys()) {
+                    if (generatedKeys.next()) {
+                        int id = generatedKeys.getInt(1);
+
+                        // Create new Target with id
+                        Target inserted = new Target(id, target.getName(), target.getText());
+
+                        return inserted;
+                    }
+                    else {
+                        throw new SaveException("Could not retrieve generated ID.");
+                    }
+                }
+            }
+
+        }
+        catch (SQLException | DatabaseException e) {
+            throw new SaveException(e);
+        }
+    }
+
+    @Override
+    public void update(Target target) throws ElementNotFoundException, SaveException {
+        checkNotNull(target, "Target must not be null.");
+
+        if (target.getId() < 0) {
+            throw new ElementNotFoundException("Element has no valid ID.");
+        }
+
+        SqlQueryBuilder builder = new SqlQueryBuilder();
+        builder.update(TABLE_NAME, Fields.NAME, Fields.TEXT);
+        builder.where(Fields.ID);
+
+        final String query = builder.toString();
+
+        try (Connection con = DatabaseFactory.getConnection()) {
+            try (NamedParameterStatement stmt = NamedParameterStatement.fromConnection(con, query)) {
+                stmt.setParameter(Fields.ID, target.getId());
+                stmt.setParameter(Fields.NAME, target.getName());
+                stmt.setParameter(Fields.TEXT, target.getText());
+
+                int affectedRows = stmt.executeUpdate();
+                if (affectedRows != 1) {
+                    throw new SaveException();
+                }
+            }
+        }
+        catch (SQLException | DatabaseException e) {
+            throw new SaveException(e);
+        }
+    }
+
+    private Collection<Target> fromResultSet(ResultSet rs) throws SQLException {
+        checkNotNull(rs, "Result must not be null.");
+        Collection<Target> placeList = new ArrayList<>();
+
+        while (rs.next()) {
+            int id = rs.getInt(Fields.ID);
+            String name = rs.getString(Fields.NAME);
+            String text = rs.getString(Fields.TEXT);
+
+            Target place = new Target(id, name, text);
+
+            placeList.add(place);
+        }
+
+        return placeList;
+    }
+
+    private static final String TABLE_NAME = "State_Target";
+
+    private static class Fields {
+        public static final String ID = "State_Target_ID";
+        public static final String NAME = "State_Target_Name";
+        public static final String TEXT = "State_Target_Text";
+
+    }
 }
