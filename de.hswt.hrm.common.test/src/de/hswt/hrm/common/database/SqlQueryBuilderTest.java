bcef7b25feb71987026a7e9781eeb2d2002764bd
Ben <benjamin.pabst85@gmail.com>
2013-05-09 14:43:42 +0200

    Implement SqlQueryBuilder as fluid interface in common.

--- de.hswt.hrm.common.test/src/de/hswt/hrm/common/database/SqlQueryBuilderTest.java
+++ de.hswt.hrm.common.test/src/de/hswt/hrm/common/database/SqlQueryBuilderTest.java
@@ -6,10 +6,63 @@ import static org.junit.Assert.*;
 public class SqlQueryBuilderTest {
     @Test
     public void testSimpleSelectStatement() {
+        final String expected = "SELECT col1, col2, col3 FROM table;";
+        
+        SqlQueryBuilder builder = new SqlQueryBuilder();
+        final String actual = builder.select("table", "col1", "col2", "col3").toString();
+        assertEquals("Wrong select statement build.", expected, actual);
+    }
+    
+    @Test
+    public void testSimpleSelectStatementWithEscapedTable() {
         final String expected = "SELECT col1, col2, col3 FROM `table`;";
         
         SqlQueryBuilder builder = new SqlQueryBuilder();
-        final String actual = builder.buildSelectStatement("table", "col1", "col2", "col3");
+        builder.setEscapeTableName(true);
+        final String actual = builder.select("table", "col1", "col2", "col3").toString();
         assertEquals("Wrong select statement build.", expected, actual);
+    }
+    
+    @Test
+    public void testSelectStatmentWithWhere() {
+        final String expected = "SELECT col1, col2 FROM table WHERE col2 = :col2;";
+        
+        SqlQueryBuilder builder = new SqlQueryBuilder();
+        builder.select("table", "col1", "col2");
+        builder.where("col2");
+        assertEquals("Wrong select statement build.", expected, builder.toString());
+    }
+    
+    @Test
+    public void testSelectStatmentMultipleWhere() {
+        final String expected = "SELECT col1, col2 FROM table WHERE col2 = :col2 AND col3 = :col3;";
+        
+        SqlQueryBuilder builder = new SqlQueryBuilder();
+        builder.select("table", "col1", "col2");
+        builder.where("col2").and("col3");
+        assertEquals("Wrong select statement build.", expected, builder.toString());
+    }
+    
+    @Test
+    public void testUpdateStatementWithWhere() {
+        final String expected = "UPDATE `table` SET col1 = :col1, col2 = :col2 WHERE id = :id;";
+        
+        SqlQueryBuilder builder = new SqlQueryBuilder();
+        builder.setEscapeTableName(true);
+        builder.update("table", "col1", "col2");
+        builder.where("id");
+        
+        assertEquals("Wrong update statement build.", expected, builder.toString());
+    }
+    
+    @Test
+    public void testInsertStatement() {
+        final String expected = "INSERT INTO `table`(col1, col2) VALUES(:col1, :col2);";
+        
+        SqlQueryBuilder builder = new SqlQueryBuilder();
+        builder.setEscapeTableName(true);
+        builder.insert("table", "col1", "col2");
+        
+        assertEquals("Wrong insert statement build.", expected, builder.toString());
     }
 }
--- de.hswt.hrm.common/src/de/hswt/hrm/common/database/SqlQueryBuilder.java
+++ de.hswt.hrm.common/src/de/hswt/hrm/common/database/SqlQueryBuilder.java
@@ -5,43 +5,153 @@ import static com.google.common.base.Preconditions.checkArgument;
 import com.google.common.base.Joiner;
 
 public class SqlQueryBuilder {
-    
+    private StringBuilder query = new StringBuilder();
+    private Type type = Type.UNSET;
     private boolean escapeTableName = false;
     
     public SqlQueryBuilder() { }
     
-    public SqlQueryBuilder(final boolean escapeTableName) {
-        this.escapeTableName = escapeTableName;
-    }
-    
-    public String buildSelectStatement(final String table, final String... columns) {
+    public SqlQueryBuilder select(final String table, final String... columns) {
         checkArgument(columns.length > 0, "You must at least provide one column.");
+        
+        if (type != Type.UNSET) {
+            throw new IllegalStateException("You can only start one type of query.");
+        }
+        
+        type = Type.SELECT;
         
         Joiner joiner = Joiner.on(", ").skipNulls();
         
-        StringBuilder query = new StringBuilder();
         query.append("SELECT ");
-        query.append(joiner.join(columns));
-        query.append(" FROM ");
+        joiner.appendTo(query, columns);
+        query.append(" FROM ").append(escapeTableName(table));
         
+        return this;
+    }
+    
+    private String escapeTableName(final String table) {
         if (escapeTableName) {
-            query.append("`").append(table).append("`");
+            return "`" + table + "`";
         }
         else {
-            query.append(table);
+            return table;
+        }
+    }
+    
+    public SqlQueryBuilder update(final String table, final String... columns) {
+        checkArgument(columns.length > 0, "You must at least provide one column.");
+        
+        if (type != Type.UNSET) {
+            throw new IllegalStateException("You can only start one type of query.");
         }
         
+        type = Type.UPDATE;
+        
+        query.append("UPDATE ").append(escapeTableName(table));
+        query.append(" SET ");
+        
+        for (String col : columns) {
+            query.append(col).append(" = :").append(col).append(", ");
+        }
+        query.delete(query.length() -2, query.length());
+        
+        return this;
+    }
+    
+    public SqlQueryBuilder insert(final String table, final String... columns) {
+        checkArgument(columns.length > 0, "You must at least provide one column.");
+        
+        if (type != Type.UNSET) {
+            throw new IllegalStateException("You can only start one type of query.");
+        }
+        
+        type = Type.INSERT;
+        
+        Joiner joiner = Joiner.on(", ").skipNulls();
+        
+        query.append("INSERT INTO ").append(escapeTableName(table));
+        query.append("(");
+        joiner.appendTo(query, columns);
+        query.append(")");
+        query.append(" VALUES(");
+        
+        for (String col : columns) {
+            query.append(":").append(col).append(", ");
+        }
+        query.delete(query.length() -2, query.length());
+        query.append(")");
+        
+        return this;
+    }
+    
+    public SqlQueryBuilder where(final String column) {
+        if (type == Type.UNSET) {
+            throw new IllegalStateException("You have to start with a call to 'select' or"
+                    + " 'update'");
+        }
+        else if (type == Type.INSERT) {
+            throw new IllegalStateException("Where cannot be used on a insert statement.");
+        }
+        
+        query.append(" WHERE ");
+        query.append(column).append(" = :").append(column);
+        
+        return this;
+    }
+    
+    public SqlQueryBuilder and(final String column) {
+        if (type == Type.UNSET) {
+            throw new IllegalArgumentException("You have to start with a call to 'select' or"
+                    + " 'update'");
+        }
+        else if (type == Type.INSERT) {
+            throw new IllegalStateException("And cannot be used on a insert statement.");
+        }
+        
+        query.append(" AND ");
+        query.append(column).append(" = :").append(column);
+        
+        return this;
+    }
+    
+    public SqlQueryBuilder or(final String column) {
+        if (type == Type.UNSET) {
+            throw new IllegalArgumentException("You have to start with a call to 'select' or"
+                    + " 'update'");
+        }
+        else if (type == Type.INSERT) {
+            throw new IllegalStateException("Or cannot be used on a insert statement.");
+        }
+        
+        query.append(" OR ");
+        query.append(column).append(" = :").append(column);
+        
+        return this;
+    }
+    
+    @Override
+    public String toString() {
         query.append(";");
         return query.toString();
     }
-
+    
     public boolean isEscapeTableName() {
         return escapeTableName;
     }
 
     public void setEscapeTableName(boolean escapeTableName) {
+        if (type != Type.UNSET) {
+            throw new IllegalStateException("You can only change escape mode if statement"
+                    + " generation has not startet.");
+        }
+        
         this.escapeTableName = escapeTableName;
     }
     
-    
+    private static enum Type {
+        SELECT,
+        INSERT,
+        UPDATE,
+        UNSET
+    }
 }
