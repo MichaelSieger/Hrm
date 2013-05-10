4213825dafb91ba3711194f5a3583e95b09edb48
Michi <michael.sieger@student.hswt.de>
2013-05-10 15:47:16 +0200

    SchemeTreeViewerFilter

---
+++ de.hswt.hrm.scheme.ui/src/de/hswt/hrm/scheme/ui/tree/SchemeTreeViewerFilter.java
@@ -0,0 +1,31 @@
+package de.hswt.hrm.scheme.ui.tree;
+
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.jface.viewers.ViewerFilter;
+
+import de.hswt.hrm.scheme.model.Component;
+
+/**
+ * Filters the tree by Component names
+ * 
+ * @author Michael Sieger
+ *
+ */
+public class SchemeTreeViewerFilter extends ViewerFilter{
+	
+	private final String name;
+	
+	public SchemeTreeViewerFilter(String name){
+		this.name = name;
+	}
+
+	@Override
+	public boolean select(Viewer viewer, Object parentElement, Object element) {
+		if(element.getClass() == ComponentTreeItem.class){
+			ComponentTreeItem c = (ComponentTreeItem) element;
+			return c.getRenderedComponent().getComponent().getName().startsWith(name);
+		}
+		return true;
+	}
+
+}
