1ddc2e1ce8af6f2a8b071f373137eca6212b9f40
Michi <michael.sieger@student.hswt.de>
2013-05-10 12:53:58 +0200

    CategoryTreeItem

---
+++ de.hswt.hrm.scheme.ui/src/de/hswt/hrm/scheme/ui/tree/CategoryTreeItem.java
@@ -0,0 +1,52 @@
+package de.hswt.hrm.scheme.ui.tree;
+
+import java.util.Collection;
+import java.util.Iterator;
+
+import org.eclipse.swt.graphics.Image;
+
+import de.hswt.hrm.scheme.model.Category;
+import de.hswt.hrm.scheme.model.RenderedComponent;
+
+public class CategoryTreeItem implements SchemeTreeItem{
+	
+	private final Category c;
+	private final Collection<RenderedComponent> renComps;
+	
+	public CategoryTreeItem(Category c, Collection<RenderedComponent> renComps){
+		this.c = c;
+		this.renComps = renComps;
+	}
+
+	@Override
+	public SchemeTreeItem[] getChildren() {
+		ComponentTreeItem[] cTreeItems = new ComponentTreeItem[renComps.size()];
+		Iterator<RenderedComponent> it = renComps.iterator();
+		for(int i = 0; i < cTreeItems.length; i++){
+			RenderedComponent c = it.next();
+			cTreeItems[i] = new ComponentTreeItem(c);
+		}
+		return cTreeItems;
+	}
+
+	@Override
+	public boolean hasChildren() {
+		return true;
+	}
+
+	@Override
+	public SchemeTreeItem getParent() {
+		return null;
+	}
+
+	@Override
+	public String getText() {
+		return c.getName();
+	}
+
+	@Override
+	public Image getImage() {
+		return null;
+	}
+	
+}
