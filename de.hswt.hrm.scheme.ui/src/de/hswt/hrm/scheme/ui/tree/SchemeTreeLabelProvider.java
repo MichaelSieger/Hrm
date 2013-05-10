4d95ed9035ff4d43cf95d765a1f18e0a96f4154a
Michi <michael.sieger@student.hswt.de>
2013-05-10 13:38:31 +0200

    missing comment

--- de.hswt.hrm.scheme.ui/src/de/hswt/hrm/scheme/ui/tree/SchemeTreeLabelProvider.java
+++ de.hswt.hrm.scheme.ui/src/de/hswt/hrm/scheme/ui/tree/SchemeTreeLabelProvider.java
@@ -3,8 +3,16 @@ package de.hswt.hrm.scheme.ui.tree;
 import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.swt.graphics.Image;
 
+/**
+ * Implements the ILabelProvider interface and just redirects to the SchemeTreeItem interface.
+ * 
+ * @author Michael Sieger
+ *
+ */
 public class SchemeTreeLabelProvider extends LabelProvider{
 
+	
+	
 	@Override
 	public Image getImage(Object element) {
 		return ((SchemeTreeItem)element).getImage();
