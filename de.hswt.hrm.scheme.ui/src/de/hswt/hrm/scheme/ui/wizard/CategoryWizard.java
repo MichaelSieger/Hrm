95b7e3035a494cb7fb8a4f181a0c66488443b32c
Anton Schreck <anton.schreck@student.hswt.de>
2013-05-11 18:27:10 +0200

    Created dummy files for CategoryWizard

---
+++ de.hswt.hrm.scheme.ui/src/de/hswt/hrm/scheme/ui/wizard/CategoryWizard.java
@@ -0,0 +1,9 @@
+package de.hswt.hrm.scheme.ui.wizard;
+
+public class CategoryWizard {
+
+    public CategoryWizard() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
---
+++ de.hswt.hrm.scheme.ui/src/de/hswt/hrm/scheme/ui/wizard/CategoryWizardPageOne.java
@@ -0,0 +1,9 @@
+package de.hswt.hrm.scheme.ui.wizard;
+
+public class CategoryWizardPageOne {
+
+    public CategoryWizardPageOne() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
