a502588667cb2ab383df2858f6f90e2851d9f610
Ben <benjamin.pabst85@gmail.com>
2013-05-12 19:51:06 +0200

    Add tests for locking in place.service.test.

--- de.hswt.hrm.place.service.test/src/de/hswt/hrm/place/service/PlaceServiceTest.java
+++ de.hswt.hrm.place.service.test/src/de/hswt/hrm/place/service/PlaceServiceTest.java
@@ -3,15 +3,25 @@ package de.hswt.hrm.place.service;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.sql.SQLException;
 import java.util.Collection;
 
 import org.eclipse.e4.core.contexts.ContextInjectionFactory;
 import org.eclipse.e4.core.contexts.EclipseContextFactory;
 import org.eclipse.e4.core.contexts.IEclipseContext;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
+import com.google.common.base.Optional;
+
+import de.hswt.hrm.common.Config;
 import de.hswt.hrm.common.database.exception.DatabaseException;
 import de.hswt.hrm.common.database.exception.ElementNotFoundException;
+import de.hswt.hrm.common.database.exception.SaveException;
+import de.hswt.hrm.common.locking.jdbc.ILockService;
+import de.hswt.hrm.common.locking.jdbc.Lock;
+import de.hswt.hrm.common.locking.jdbc.LockService;
 import de.hswt.hrm.place.dao.core.IPlaceDao;
 import de.hswt.hrm.place.dao.jdbc.PlaceDao;
 import de.hswt.hrm.place.model.Place;
@@ -29,18 +39,39 @@ public class PlaceServiceTest extends AbstractDatabaseTest {
         assertEquals("StreetNo not set correctly.", expected.getStreetNo(), actual.getStreetNo());
     }
     
-    private PlaceService createInjectedPlaceService() {
+    private ILockService createLockService() throws DatabaseException, SQLException {
+        ILockService lockService = new LockService();
+        return lockService;
+    }
+    
+    private PlaceService createInjectedPlaceService() throws DatabaseException, SQLException {
         IPlaceDao dao = new PlaceDao();
         
         IEclipseContext context = EclipseContextFactory.create();
         context.set(IPlaceDao.class, dao);
+        context.set(ILockService.class, createLockService());
         PlaceService service = ContextInjectionFactory.make(PlaceService.class, context);
         
         return service;
     }
+    
+    @BeforeClass
+    public static void disableLockingInConfig() {
+        Config cfg = Config.getInstance();
+        cfg.setProperty(Config.Keys.DB_LOCKING, "0");
+    }
+    
+    @Before
+    public void forceNewSession() {
+        // As we delete the database everytime, but don't rebuild the config
+        // between tests, we have to ensure, that a new session is started
+        // for every test
+        Config cfg = Config.getInstance();
+        cfg.setProperty(Config.Keys.SESSION_UUID, "");
+    }
 
     @Test
-    public void testFindAll() throws DatabaseException {
+    public void testFindAll() throws DatabaseException, SQLException {
         Place p1 = new Place("place Name", "55464", "SomeCity", "Street", "113a", "Location",
                 "Some Area");
         Place p2 = new Place("Another place", "65461", "Gotham City", "Bat-Avenue", "558",
@@ -55,7 +86,7 @@ public class PlaceServiceTest extends AbstractDatabaseTest {
     }
 
     @Test
-    public void testFindById() throws ElementNotFoundException, DatabaseException {
+    public void testFindById() throws ElementNotFoundException, DatabaseException, SQLException {
         Place expected = new Place("place Name", "55464", "SomeCity", "Street", "113a", "Location",
                 "Some Area");
         
@@ -67,7 +98,7 @@ public class PlaceServiceTest extends AbstractDatabaseTest {
     }
 
     @Test
-    public void testInsert() throws ElementNotFoundException, DatabaseException {
+    public void testInsert() throws ElementNotFoundException, DatabaseException, SQLException {
         Place expected = new Place("place Name", "55464", "SomeCity", "Street", "113a", "Location",
                 "Some Area");
 
@@ -82,7 +113,7 @@ public class PlaceServiceTest extends AbstractDatabaseTest {
     }
 
     @Test
-    public void testUpdate() throws ElementNotFoundException, DatabaseException {
+    public void testUpdate() throws ElementNotFoundException, DatabaseException, SQLException {
         Place expected = new Place("place Name", "55464", "SomeCity", "Street", "113a", "Location",
                 "Some Area");
         
@@ -100,5 +131,54 @@ public class PlaceServiceTest extends AbstractDatabaseTest {
         Place retrieved = service.findById(parsed.getId());
         comparePlaceFields(parsed, retrieved);
         assertEquals("Requested object does not equal updated one.", parsed, retrieved);
+    }
+    
+    @Test(expected=IllegalStateException.class)
+    public void testFailWithoutLock() throws DatabaseException, SQLException {
+        Config cfg = Config.getInstance();
+        // First we have to enalbe locking
+        cfg.setProperty(Config.Keys.DB_LOCKING, "1");
+        
+        Place expected = new Place("place Name", "55464", "SomeCity", "Street", "113a", "Location",
+                "Some Area");
+        
+        PlaceService service = createInjectedPlaceService();
+        Place parsed = service.insert(expected);
+        
+        // Update without lock
+        parsed.setLocation("A new location.");
+        service.update(parsed);
+        
+        // Don't forget to disable locking afterwards to not crash other tests
+        cfg.setProperty(Config.Keys.DB_LOCKING, "0");
+    }
+    
+    @Test()
+    public void testUpdateWithLocking() throws DatabaseException, SQLException {
+        Config cfg = Config.getInstance();
+        // First we have to enalbe locking
+        cfg.setProperty(Config.Keys.DB_LOCKING, "1");
+        
+        Place expected = new Place("place Name", "55464", "SomeCity", "Street", "113a", "Location",
+                "Some Area");
+        
+        PlaceService service = createInjectedPlaceService();
+        Place parsed = service.insert(expected);
+        
+        // Retrieve lock
+        ILockService lockService = createLockService();
+        Optional<Lock> lock = lockService.getLock(ILockService.TBL_PLACE, parsed.getId());
+        assertTrue("Could not retrieve lock.", lock.isPresent());
+        
+        // Update lock
+        parsed.setLocation("A new location.");
+        service.update(parsed);
+        
+        // Release lock
+        lockService.release(lock);
+        
+        
+        // Don't forget to disable locking afterwards to not crash other tests
+        cfg.setProperty(Config.Keys.DB_LOCKING, "0");
     }
 }
