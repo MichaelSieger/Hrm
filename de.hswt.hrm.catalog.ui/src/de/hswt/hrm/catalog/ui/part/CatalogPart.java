9ad4db899e8b35a7a9f32e2bf89d281a37ffde71
Tobias Placht <tobias.placht@student.hswt.de>
2013-05-13 21:26:11 +0200

    add catalog ui plugin

---
+++ de.hswt.hrm.catalog.ui/src/de/hswt/hrm/catalog/ui/part/CatalogPart.java
@@ -0,0 +1,19 @@
+package de.hswt.hrm.catalog.ui.part;
+
+import javax.annotation.PostConstruct;
+
+import org.eclipse.swt.widgets.Composite;
+
+/**
+ * This class will contain a simple TV to Display all available "Soll/Ist/Maßnahmen"
+ * @author knacht
+ *
+ */
+public class CatalogPart {
+
+    @PostConstruct
+    public void postConstruct(Composite parent) {
+
+    }
+
+}
