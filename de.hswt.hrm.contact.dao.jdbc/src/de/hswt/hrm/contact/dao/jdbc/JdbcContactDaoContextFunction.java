367162ed4a94a8bb614d241a26eb516ce46dd684
Tobias Placht <tobias.placht@student.hswt.de>
2013-05-13 19:46:26 +0200

    add core manifest.mf entries

--- de.hswt.hrm.contact.dao.jdbc/src/de/hswt/hrm/contact/dao/jdbc/JdbcContactDaoContextFunction.java
+++ de.hswt.hrm.contact.dao.jdbc/src/de/hswt/hrm/contact/dao/jdbc/JdbcContactDaoContextFunction.java
@@ -1,5 +1,14 @@
 package de.hswt.hrm.contact.dao.jdbc;
 
+import org.eclipse.e4.core.contexts.ContextFunction;
+import org.eclipse.e4.core.contexts.IEclipseContext;
+
 public class JdbcContactDaoContextFunction extends ContextFunction {
+
+    @Override
+    public Object compute(IEclipseContext context) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
 }
