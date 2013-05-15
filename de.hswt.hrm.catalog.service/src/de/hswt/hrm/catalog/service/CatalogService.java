113a94e6f559f8962ff2dfe63d91b872f214249f
Ben <benjamin.pabst85@gmail.com>
2013-05-15 13:10:26 +0200

    Make CatalogService an addon for the application model.

--- de.hswt.hrm.catalog.service/src/de/hswt/hrm/catalog/service/CatalogService.java
+++ de.hswt.hrm.catalog.service/src/de/hswt/hrm/catalog/service/CatalogService.java
@@ -1,5 +1,11 @@
 package de.hswt.hrm.catalog.service;
 
+import org.eclipse.e4.core.di.annotations.Creatable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Creatable
 public class CatalogService {
+	private final static Logger LOG = LoggerFactory.getLogger(CatalogService.class);
 
 }
