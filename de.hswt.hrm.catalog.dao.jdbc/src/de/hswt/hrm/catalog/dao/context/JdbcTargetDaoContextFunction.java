96a5edf4f2259f54ba8da56c14bb6169a94a36c8
Ben <benjamin.pabst85@gmail.com>
2013-05-15 15:02:42 +0200

    Make TargetDao injectable in catalog.dao.jdbc.

--- de.hswt.hrm.catalog.dao.jdbc/src/de/hswt/hrm/catalog/dao/context/JdbcTargetDaoContextFunction.java
+++ de.hswt.hrm.catalog.dao.jdbc/src/de/hswt/hrm/catalog/dao/context/JdbcTargetDaoContextFunction.java
@@ -3,12 +3,11 @@ package de.hswt.hrm.catalog.dao.context;
 import org.eclipse.e4.core.contexts.ContextFunction;
 import org.eclipse.e4.core.contexts.IEclipseContext;
 
-public class JdbcActivityDaoContextFunction extends ContextFunction {
+public class JdbcTargetDaoContextFunction extends ContextFunction {
 
 	@Override
 	public Object compute(IEclipseContext context) {
-		System.out.println("### ActivityDao");
-		// TODO Auto-generated method stub
+		System.out.println("### TargetDao");
 		return null;
 	}
 
