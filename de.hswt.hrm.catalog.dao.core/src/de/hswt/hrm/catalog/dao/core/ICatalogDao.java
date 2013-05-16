5261d22fe0357bfc56470021b9e7187c9b789dee
haeringl <luke-haering@web.de>
2013-05-16 17:24:51 +0200

    implement methods in ICatalogDao.java

--- de.hswt.hrm.catalog.dao.core/src/de/hswt/hrm/catalog/dao/core/ICatalogDao.java
+++ de.hswt.hrm.catalog.dao.core/src/de/hswt/hrm/catalog/dao/core/ICatalogDao.java
@@ -1,5 +1,44 @@
 package de.hswt.hrm.catalog.dao.core;
 
-public class ICatalogDao {
+import java.util.Collection;
 
+import de.hswt.hrm.common.database.exception.DatabaseException;
+import de.hswt.hrm.common.database.exception.ElementNotFoundException;
+import de.hswt.hrm.common.database.exception.SaveException;
+import de.hswt.hrm.catalog.model.Catalog;
+
+/**
+ * Defines all the public methods to interact with the storage system for activitys.
+ */
+public interface ICatalogDao {
+    
+    /**
+     * @return All catalogs from storage.
+     */
+    Collection<Catalog> findAll() throws DatabaseException;
+    
+    /**
+     * @param id of the target catalog.
+     * @return Catalog with the given id.
+     * @throws ElementNotFoundException If the given id is not present in the database.
+     */
+    Catalog findById(int id) throws DatabaseException, ElementNotFoundException;
+    
+    /**
+     * Add a new catalog to storage.
+     * 
+     * @param catalog Catalog that should be stored.
+     * @return Newly generated catalog (also holding the correct id).
+     * @throws SaveException If the catalog could not be inserted.
+     */
+    Catalog insert(Catalog catalog) throws SaveException;
+    
+    /**
+     * Update an existing catalog in storage.
+     * 
+     * @param catalog Catalog that should be updated.
+     * @throws ElementNotFoundException If the given catalog is not present in the database.
+     * @throws SaveException If the catalog could not be updated.
+     */
+    void update(Catalog catalog) throws ElementNotFoundException, SaveException;
 }
