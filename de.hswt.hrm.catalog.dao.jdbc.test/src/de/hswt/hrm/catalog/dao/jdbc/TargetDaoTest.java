62b08cbca27830959daeef52ba7a84e93daebfff
haeringl <luke-haering@web.de>
2013-05-16 16:46:57 +0200

    implement testInsertTarget method

--- de.hswt.hrm.catalog.dao.jdbc.test/src/de/hswt/hrm/catalog/dao/jdbc/TargetDaoTest.java
+++ de.hswt.hrm.catalog.dao.jdbc.test/src/de/hswt/hrm/catalog/dao/jdbc/TargetDaoTest.java
@@ -1,17 +1,40 @@
 package de.hswt.hrm.catalog.dao.jdbc;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 
+import de.hswt.hrm.catalog.model.Target;
 import de.hswt.hrm.common.database.exception.DatabaseException;
 import de.hswt.hrm.common.database.exception.ElementNotFoundException;
 import de.hswt.hrm.test.database.AbstractDatabaseTest;
 
 public class TargetDaoTest extends AbstractDatabaseTest {
 
+    private void compareTargetFields(final Target expected, final Target actual) {
+        assertEquals("Name not set correctly.", expected.getName(), actual.getName());
+        assertEquals("Text not set correctly.", expected.getText(), actual.getText());
+
+    }
 
     @Test
-    public void testInsertActivity() throws ElementNotFoundException, DatabaseException {
+    public void testInsertTarget() throws ElementNotFoundException, DatabaseException {
+        final String name = "TargetName";
+        final String text = "Get outta here ...";
 
+        Target expected = new Target(name, text);
+
+        // Check return value from insert
+        TargetDao dao = new TargetDao();
+        Target parsed = dao.insert(expected);
+        compareTargetFields(expected, parsed);
+        assertTrue("ID not set correctly.", parsed.getId() >= 0);
+
+        // Request from database
+        Target requested = dao.findById(parsed.getId());
+        compareTargetFields(expected, requested);
+        assertEquals("Requested object does not equal parsed one.", parsed, requested);
     }
 
     @Test
