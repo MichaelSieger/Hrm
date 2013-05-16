5c5e128077b772fc88aad0e6021eb3140ba6753c
haeringl <luke-haering@web.de>
2013-05-16 16:46:44 +0200

    implement testInsertCurrent method

--- de.hswt.hrm.catalog.dao.jdbc.test/src/de/hswt/hrm/catalog/dao/jdbc/CurrentDaoTest.java
+++ de.hswt.hrm.catalog.dao.jdbc.test/src/de/hswt/hrm/catalog/dao/jdbc/CurrentDaoTest.java
@@ -1,22 +1,44 @@
 package de.hswt.hrm.catalog.dao.jdbc;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 
+import de.hswt.hrm.catalog.model.Current;
 import de.hswt.hrm.common.database.exception.DatabaseException;
 import de.hswt.hrm.common.database.exception.ElementNotFoundException;
 import de.hswt.hrm.test.database.AbstractDatabaseTest;
 
 public class CurrentDaoTest extends AbstractDatabaseTest {
 
-
-
-    @Test
-    public void testInsertActivity() throws ElementNotFoundException, DatabaseException {
+    private void compareCurrentFields(final Current expected, final Current actual) {
+        assertEquals("Name not set correctly.", expected.getName(), actual.getName());
+        assertEquals("Text not set correctly.", expected.getText(), actual.getText());
 
     }
 
     @Test
-    public void testUpdateActivity() throws ElementNotFoundException, DatabaseException {
+    public void testInsertCurrent() throws ElementNotFoundException, DatabaseException {
+        final String name = "CurrentName";
+        final String text = "Get outta here ...";
+
+        Current expected = new Current(name, text);
+
+        // Check return value from insert
+        CurrentDao dao = new CurrentDao();
+        Current parsed = dao.insert(expected);
+        compareCurrentFields(expected, parsed);
+        assertTrue("ID not set correctly.", parsed.getId() >= 0);
+
+        // Request from database
+        Current requested = dao.findById(parsed.getId());
+        compareCurrentFields(expected, requested);
+        assertEquals("Requested object does not equal parsed one.", parsed, requested);
+    }
+
+    @Test
+    public void testUpdateCurrent() throws ElementNotFoundException, DatabaseException {
 
     }
 
