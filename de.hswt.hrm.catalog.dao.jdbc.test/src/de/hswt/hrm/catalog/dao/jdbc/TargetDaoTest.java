1b014a5e3492fc2d5e1c8eb289ca32188f260871
haeringl <luke-haering@web.de>
2013-05-16 16:38:24 +0200

    add basic test methods in TargetDaoTest

--- de.hswt.hrm.catalog.dao.jdbc.test/src/de/hswt/hrm/catalog/dao/jdbc/TargetDaoTest.java
+++ de.hswt.hrm.catalog.dao.jdbc.test/src/de/hswt/hrm/catalog/dao/jdbc/TargetDaoTest.java
@@ -1,5 +1,32 @@
 package de.hswt.hrm.catalog.dao.jdbc;
 
-public class TargetDaoTest {
+import org.junit.Test;
+
+import de.hswt.hrm.common.database.exception.DatabaseException;
+import de.hswt.hrm.common.database.exception.ElementNotFoundException;
+import de.hswt.hrm.test.database.AbstractDatabaseTest;
+
+public class TargetDaoTest extends AbstractDatabaseTest {
+
+
+    @Test
+    public void testInsertActivity() throws ElementNotFoundException, DatabaseException {
+
+    }
+
+    @Test
+    public void testUpdateActivity() throws ElementNotFoundException, DatabaseException {
+
+    }
+
+    @Test
+    public void testFindAllActivity() throws ElementNotFoundException, DatabaseException {
+
+    }
+
+    @Test
+    public void testFindByIdActivity() throws ElementNotFoundException, DatabaseException {
+
+    }
 
 }
