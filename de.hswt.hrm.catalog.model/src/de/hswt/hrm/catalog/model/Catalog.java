20f2959b01698bf67d5bf321c5cd2c37b4ff3c5f
haeringl <luke-haering@web.de>
2013-05-16 17:48:28 +0200

    implement catalog model

--- de.hswt.hrm.catalog.model/src/de/hswt/hrm/catalog/model/Catalog.java
+++ de.hswt.hrm.catalog.model/src/de/hswt/hrm/catalog/model/Catalog.java
@@ -1,5 +1,70 @@
 package de.hswt.hrm.catalog.model;
 
-public class Catalog {
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Strings.isNullOrEmpty;
+
+/**
+ * Represents a contact.
+ */
+public final class Catalog {
+    private int id;
+    private String name;
+
+    private static final String IS_MANDATORY = "Field is a mandatory.";
+
+    public Catalog(final String name) {
+
+        this(-1, name);
+    }
+
+    public Catalog(int id, final String name) {
+
+        this.id = id;
+
+        setName(name);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        checkArgument(!isNullOrEmpty(name), IS_MANDATORY);
+        this.name = name;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + id;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Catalog other = (Catalog) obj;
+        if (id != other.id)
+            return false;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        }
+        else if (!name.equals(other.name))
+            return false;
+        return true;
+    }
 
 }
