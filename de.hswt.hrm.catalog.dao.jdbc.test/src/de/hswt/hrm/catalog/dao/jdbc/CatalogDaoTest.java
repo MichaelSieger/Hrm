876b19808ed22bf6b022c1c81e04c89d04fedfd2
haeringl <luke-haering@web.de>
2013-05-16 21:57:56 +0200

    add test methods

--- de.hswt.hrm.catalog.dao.jdbc.test/src/de/hswt/hrm/catalog/dao/jdbc/CatalogDaoTest.java
+++ de.hswt.hrm.catalog.dao.jdbc.test/src/de/hswt/hrm/catalog/dao/jdbc/CatalogDaoTest.java
@@ -1,5 +1,31 @@
 package de.hswt.hrm.catalog.dao.jdbc;
 
-public class CatalogDaoTest {
+import org.junit.Test;
+
+import de.hswt.hrm.common.database.exception.DatabaseException;
+import de.hswt.hrm.common.database.exception.ElementNotFoundException;
+import de.hswt.hrm.test.database.AbstractDatabaseTest;
+
+public class CatalogDaoTest extends AbstractDatabaseTest {
+
+    @Test
+    public void testInsertCatalog() throws ElementNotFoundException, DatabaseException {
+
+    }
+
+    @Test
+    public void testUpdateCatalog() throws ElementNotFoundException, DatabaseException {
+
+    }
+
+    @Test
+    public void testFindAllCatalog() throws ElementNotFoundException, DatabaseException {
+
+    }
+
+    @Test
+    public void testFindByIdCatalog() throws ElementNotFoundException, DatabaseException {
+
+    }
 
 }
