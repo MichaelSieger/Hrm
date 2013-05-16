f9af41f107b77a691a1ac51c0401161251193d49
haeringl <luke-haering@web.de>
2013-05-16 22:03:47 +0200

    implement testInsertCatalog

--- de.hswt.hrm.catalog.dao.jdbc.test/src/de/hswt/hrm/catalog/dao/jdbc/CatalogDaoTest.java
+++ de.hswt.hrm.catalog.dao.jdbc.test/src/de/hswt/hrm/catalog/dao/jdbc/CatalogDaoTest.java
@@ -1,16 +1,38 @@
 package de.hswt.hrm.catalog.dao.jdbc;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 
+import de.hswt.hrm.catalog.model.Catalog;
 import de.hswt.hrm.common.database.exception.DatabaseException;
 import de.hswt.hrm.common.database.exception.ElementNotFoundException;
 import de.hswt.hrm.test.database.AbstractDatabaseTest;
 
 public class CatalogDaoTest extends AbstractDatabaseTest {
 
+    private void compareCatalogFields(final Catalog expected, final Catalog actual) {
+        assertEquals("Name not set correctly.", expected.getName(), actual.getName());
+
+    }
+
     @Test
     public void testInsertCatalog() throws ElementNotFoundException, DatabaseException {
+        final String name = "CatalogName";
 
+        Catalog expected = new Catalog(name);
+
+        // Check return value from insert
+        CatalogDao dao = new CatalogDao();
+        Catalog parsed = dao.insert(expected);
+        compareCatalogFields(expected, parsed);
+        assertTrue("ID not set correctly.", parsed.getId() >= 0);
+
+        // Request from database
+        Catalog requested = dao.findById(parsed.getId());
+        compareCatalogFields(expected, requested);
+        assertEquals("Requested object does not equal parsed one.", parsed, requested);
     }
 
     @Test
