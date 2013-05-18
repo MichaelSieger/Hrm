1d1b09b2782e6d96aa5874139cab42a385b93e2b
Michi <michael.sieger@student.hswt.de>
2013-05-18 11:05:17 +0200

    Remove Drop code and change to DragData

--- de.hswt.hrm.scheme.ui/src/de/hswt/hrm/scheme/ui/GridDNDManager.java
+++ de.hswt.hrm.scheme.ui/src/de/hswt/hrm/scheme/ui/GridDNDManager.java
@@ -17,16 +17,13 @@ import de.hswt.hrm.scheme.model.RenderedComponent;
  * @author Michael Sieger
  *
  */
-public class GridDNDManager implements DragSourceListener, DropTargetListener{
+public class GridDNDManager implements DragSourceListener{
 	
 	private final SchemeGrid grid;
 	
-	/**
-	 * The item that is dragged at the moment. Null if no drag is running.
-	 */
-	private DragData dragging;
-	
 	private List<RenderedComponent> comps;
+	
+	private DragData data;
 	
 	public GridDNDManager(SchemeGrid grid, List<RenderedComponent> comps){
 		this.grid = grid;
@@ -38,7 +35,7 @@ public class GridDNDManager implements DragSourceListener, DropTargetListener{
 	    SchemeGridItem item = grid.removeImagePixel(ev.x, ev.y);
 	    if(item != null){
 		    RenderedComponent c = item.getRenderedComponent();
-	        dragging = new DragData(comps.indexOf(c), 
+	        data = new DragData(comps.indexOf(c), 
 	                item.getX(), item.getY(), item.getDirection());
 	    }else{
 	    	ev.doit = false;
@@ -47,71 +44,15 @@ public class GridDNDManager implements DragSourceListener, DropTargetListener{
 	
 	@Override
 	public void dragSetData(DragSourceEvent ev) {
-		if(dragging != null){
-			/*
-			 * SWT wants the data field to be filled
-			 */
-			ev.data = " ";
-		}
+		ev.data = data;
 	}
 	
 	@Override
 	public void dragFinished(DragSourceEvent ev) {
-		/*
-		 * dragging is not null, if it was dropped outside of the grid.
-		 * Thow it away in this case.
-		 */
-		dragging = null;
+
 	}
 
-    @Override
-    public void dropAccept(DropTargetEvent arg0) {}
-    
-    @Override
-    public void drop(DropTargetEvent ev) {
-        if(dragging != null){
-            Point loc = grid.toDisplay(0, 0);
-            final int x = ev.x - loc.x;
-            final int y = ev.y - loc.y;
-            try {
-                grid.setImageAtPixel(
-                        comps.get(dragging.getId()), 
-                        dragging.getDirection(), 
-                		x, y);
-            } catch (PlaceOccupiedException | IllegalArgumentException e) {
-                //try {
-				//	grid.setImage(dragging);
-				//} catch (PlaceOccupiedException | IllegalArgumentException e1) {
-					/*
-					 * Das kann eigentlich nicht passieren, weil der Startpunkt
-					 * vor dem Drag nicht belegt war.
-					 */
-					//e1.printStackTrace();
-				//}
-            }
-            dragging = null;
-        }
-    }
-    
-    @Override
-    public void dragOver(DropTargetEvent arg0) {}
-    
-    @Override
-    public void dragOperationChanged(DropTargetEvent arg0) {}
-    
-    @Override
-    public void dragLeave(DropTargetEvent ev) {
-    	if(dragging != null){
-    		ev.detail = DND.DROP_NONE;
-    	}
-    }
-    
-    @Override
-    public void dragEnter(DropTargetEvent ev) {
-    	if(dragging != null){
-    		ev.detail = DND.DROP_COPY;
-    	}
-    }
+ 
 	
 
 }
