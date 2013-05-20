833f8bc749f224bf2e01767f2b73def6f547b39b
Ben <benjamin.pabst85@gmail.com>
2013-05-20 17:19:48 +0200

    Fix: put DAOs into context in context functions in catalog.dao.

--- de.hswt.hrm.catalog.dao.jdbc/src/de/hswt/hrm/catalog/dao/context/JdbcActivityDaoContextFunction.java
+++ de.hswt.hrm.catalog.dao.jdbc/src/de/hswt/hrm/catalog/dao/context/JdbcActivityDaoContextFunction.java
@@ -1,15 +1,25 @@
 package de.hswt.hrm.catalog.dao.context;
 
 import org.eclipse.e4.core.contexts.ContextFunction;
+import org.eclipse.e4.core.contexts.ContextInjectionFactory;
 import org.eclipse.e4.core.contexts.IEclipseContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import de.hswt.hrm.catalog.dao.core.IActivityDao;
+import de.hswt.hrm.catalog.dao.jdbc.ActivityDao;
 
 public class JdbcActivityDaoContextFunction extends ContextFunction {
+    private final static Logger LOG = LoggerFactory.getLogger(JdbcActivityDaoContextFunction.class);
 
 	@Override
 	public Object compute(IEclipseContext context) {
-		System.out.println("### ActivityDao");
-		// TODO Auto-generated method stub
-		return null;
+	    // Create activity dao and inject context as it might need it for further injection
+        IActivityDao activityDao = ContextInjectionFactory.make(ActivityDao.class, context);
+        context.set(IActivityDao.class, activityDao);
+        
+        LOG.debug("Made ActivityDao available in Eclipse context.");
+        return activityDao;
 	}
 
 }
