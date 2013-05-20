4f110b413490557706844f208228f884fd8833ad
Ben <benjamin.pabst85@gmail.com>
2013-05-20 18:19:49 +0200

    Add context functions in scheme.dao.jdbc.

---
+++ de.hswt.hrm.scheme.dao.jdbc/src/de/hswt/hrm/scheme/dao/jdbc/SchemeComponentDao.java
@@ -0,0 +1,24 @@
+package de.hswt.hrm.scheme.dao.jdbc;
+
+import java.util.Collection;
+
+import de.hswt.hrm.scheme.dao.core.ISchemeComponentDao;
+import de.hswt.hrm.scheme.model.Component;
+import de.hswt.hrm.scheme.model.Scheme;
+import de.hswt.hrm.scheme.model.SchemeComponent;
+
+public class SchemeComponentDao implements ISchemeComponentDao {
+
+    @Override
+    public Collection<Component> findAllComponentByScheme(Scheme scheme) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void insertComponent(Scheme scheme, SchemeComponent component) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
