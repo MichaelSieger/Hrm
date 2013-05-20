c74d7731a5f490580332040ba11dad7e9dd4e242
Michi <michael.sieger@student.hswt.de>
2013-05-20 09:36:39 +0200

    ItemClickListener

---
+++ de.hswt.hrm.scheme.ui/src/de/hswt/hrm/scheme/ui/ItemClickListener.java
@@ -0,0 +1,13 @@
+package de.hswt.hrm.scheme.ui;
+
+/**
+ * Listens for a item click.
+ * 
+ * @author Michael Sieger
+ *
+ */
+public interface ItemClickListener {
+	
+	void itemClicked(SchemeGridItem item);
+
+}
