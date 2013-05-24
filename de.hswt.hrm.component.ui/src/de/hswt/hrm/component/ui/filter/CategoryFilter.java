f6400e8b9fa0d8f8f8f2a801433c9e0af70880bb
Anton Schreck <anton.schreck@student.hswt.de>
2013-05-24 13:37:39 +0200

    Implemented CategoryFilter

--- de.hswt.hrm.component.ui/src/de/hswt/hrm/component/ui/filter/CategoryFilter.java
+++ de.hswt.hrm.component.ui/src/de/hswt/hrm/component/ui/filter/CategoryFilter.java
@@ -3,16 +3,44 @@ package de.hswt.hrm.component.ui.filter;
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.jface.viewers.ViewerFilter;
 
+import de.hswt.hrm.component.model.Category;
+
 
 public class CategoryFilter extends ViewerFilter {
+    
+    private String searchString;
 
-    public CategoryFilter() {
-        // TODO Auto-generated constructor stub
+    public void setSearchString(String substring) {
+        searchString = (".*" + substring + ".*").toLowerCase();
     }
 
     @Override
     public boolean select(Viewer viewer, Object parentElement, Object element) {
-        // TODO Auto-generated method stub
+        
+        if (searchString == null || searchString.length() == 0) {
+            return true;
+        }
+        Category cat = (Category) element;
+        // match name
+        if (cat.getName().toLowerCase().matches(searchString)) {
+            return true;
+        }
+        // match defaultQuantifier
+        String s1 = Integer.toString(cat.getDefaultQuantifier());
+        if (s1.matches(searchString)) {
+            return true;
+        }
+        // match width
+        s1 = Integer.toString(cat.getWidth());
+        if (s1.matches(searchString)) {
+            return true;
+        }
+        // match height
+        s1 = Integer.toString(cat.getHeight());
+        if (s1.matches(searchString)) {
+            return true;
+        }
+        
         return false;
     }
 
