89da3444b2f7d35d405d453c37646083ab426885
Anton Schreck <anton.schreck@student.hswt.de>
2013-05-24 13:28:56 +0200

    Add dummy class CategoryFilter

---
+++ de.hswt.hrm.component.ui/src/de/hswt/hrm/component/ui/filter/CategoryFilter.java
@@ -0,0 +1,19 @@
+package de.hswt.hrm.component.ui.filter;
+
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.jface.viewers.ViewerFilter;
+
+
+public class CategoryFilter extends ViewerFilter {
+
+    public CategoryFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public boolean select(Viewer viewer, Object parentElement, Object element) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+}
