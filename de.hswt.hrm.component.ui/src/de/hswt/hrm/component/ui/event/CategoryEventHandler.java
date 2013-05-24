dcef08db7e499d459d5948e8bd9ae39eacbe6759
Anton Schreck <anton.schreck@student.hswt.de>
2013-05-24 14:42:16 +0200

    Added methods for TableViewer

--- de.hswt.hrm.component.ui/src/de/hswt/hrm/component/ui/event/CategoryEventHandler.java
+++ de.hswt.hrm.component.ui/src/de/hswt/hrm/component/ui/event/CategoryEventHandler.java
@@ -1,14 +1,20 @@
 package de.hswt.hrm.component.ui.event;
 
 import org.eclipse.e4.xwt.XWT;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Text;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.common.base.Optional;
+
+import de.hswt.hrm.common.database.exception.DatabaseException;
 import de.hswt.hrm.component.model.Category;
 import de.hswt.hrm.component.ui.filter.CategoryFilter;
+import de.hswt.hrm.component.ui.part.CategoryPartUtil;
 import de.hswt.hrm.scheme.service.CategoryService;
 
 public class CategoryEventHandler {
@@ -57,6 +63,34 @@ public class CategoryEventHandler {
         }
     }
     
+    /**
+     * Called whenever a doubleClick into the TableViewer occurs. It obtains
+     * the category from the selected line if the TableViewer. The category
+     * is passed to the CategoryWizard. When the Wizard has finished, the
+     * category will be updated in the database.
+     * @param event
+     */
+    public void tableEntrySelected(Event event) {
+        TableViewer tViewer = (TableViewer) XWT.findElementByName(event.widget, "categoryTable");
+        
+        // Obtain the category in the line where the doubleClick happened
+        Category selectedCat = (Category) tViewer.getElementAt(tViewer.getTable().getSelectionIndex());
+        if (selectedCat == null) {
+            return;
+        }
+        try {
+            categoryService.refresh(selectedCat);
+            Optional<Category> updatedCat = CategoryPartUtil.showWizard(event.display.getActiveShell(), Optional.of(selectedCat));
+            if (updatedCat.isPresent()) {
+                tViewer.refresh();
+            }
+        } catch (DatabaseException e) {
+            LOG.error("Could not retrieve the category from database.", e);
+            //TODO: Übersetzung ok?
+            MessageDialog.openError(event.display.getActiveShell(), "Connection Error",
+                    "Gewählte Kategorie kann nicht aus der Datenbank aktualisiert werden.");
+        }
+    }
     
 
 }
