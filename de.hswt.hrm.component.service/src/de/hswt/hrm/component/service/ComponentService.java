5d1c7308fa55fd594bb5dc2cc7513f6bc073ed00
Ben <benjamin.pabst85@gmail.com>
2013-05-25 17:53:32 +0200

    Make component service injectable.

--- de.hswt.hrm.component.service/src/de/hswt/hrm/component/service/ComponentService.java
+++ de.hswt.hrm.component.service/src/de/hswt/hrm/component/service/ComponentService.java
@@ -2,18 +2,19 @@ package de.hswt.hrm.component.service;
 
 import java.util.Collection;
 
+import org.eclipse.e4.core.di.annotations.Creatable;
+
 import de.hswt.hrm.common.database.exception.DatabaseException;
 import de.hswt.hrm.component.dao.core.IComponentDao;
 import de.hswt.hrm.component.dao.jdbc.ComponentDao;
 import de.hswt.hrm.component.model.Component;
 
+/**
+ * Service class which should be used to interact with
+ * the storage system for components.
+ */
+@Creatable
 public class ComponentService {
-	
-    /**
-     * Service class which schould be used to interact with
-     * the storage system for components
-     */
-	
 	private static IComponentDao dao = new ComponentDao();
 	
 	public ComponentService(){}
