8d5725ee2b52570d5d0892adfbfe3808d60dd284
Tobias Placht <tobias.placht@student.hswt.de>
2013-05-25 16:55:13 +0200

    add searchFilter to CatalogView

--- de.hswt.hrm.catalog.ui/src/de/hswt/hrm/catalog/ui/event/CatalogEventHandler.java
+++ de.hswt.hrm.catalog.ui/src/de/hswt/hrm/catalog/ui/event/CatalogEventHandler.java
@@ -4,10 +4,17 @@ import org.eclipse.e4.xwt.XWT;
 import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Text;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import de.hswt.hrm.catalog.ui.filter.CatalogFilter;
 
 public class CatalogEventHandler {
+
+    private final static Logger LOG = LoggerFactory.getLogger(CatalogEventHandler.class);
+    private static final String DEFAULT_SEARCH_STRING = "Search";
+    private static final String EMPTY = "";
 
     public void onSelection(Event event) {
 
@@ -30,4 +37,34 @@ public class CatalogEventHandler {
         tf.refresh();
     }
 
+    /**
+     * This event is called whenever the Search text field is entered
+     * 
+     * @param event
+     */
+    public void enterText(Event event) {
+        Text text = (Text) event.widget;
+        if (text.getText().equals(DEFAULT_SEARCH_STRING)) {
+            text.setText(EMPTY);
+        }
+    }
+
+    public void leaveText(Event event) {
+
+        Text text = (Text) event.widget;
+        if (text.getText().isEmpty()) {
+            text.setText(DEFAULT_SEARCH_STRING);
+        }
+        TableViewer tf = (TableViewer) XWT.findElementByName(text, "catalogTable");
+        tf.refresh();
+
+    }
+
+    public void onKeyUp(Event event) {
+        Text searchText = (Text) event.widget;
+        TableViewer tf = (TableViewer) XWT.findElementByName(searchText, "catalogTable");
+        CatalogFilter f = (CatalogFilter) tf.getFilters()[0];
+        f.setSearchString(searchText.getText());
+        tf.refresh();
+    }
 }
