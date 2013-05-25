fc63834bbb4890a536077828f0c9adf410b1a188
michi <sieger-michael@web.de>
2013-05-25 11:17:00 +0200

    DirectedRenderedComponent inherits RenderedComponent

--- de.hswt.hrm.scheme.ui/src/de/hswt/hrm/scheme/ui/DirectedRenderedComponent.java
+++ de.hswt.hrm.scheme.ui/src/de/hswt/hrm/scheme/ui/DirectedRenderedComponent.java
@@ -1,7 +1,9 @@
 package de.hswt.hrm.scheme.ui;
 
+import de.hswt.hrm.component.model.Component;
 import de.hswt.hrm.scheme.model.Direction;
 import de.hswt.hrm.scheme.model.RenderedComponent;
+import de.hswt.hrm.scheme.model.ThumbnailImage;
 
 /**
  * This class represents a RenderedComponent with a Direction
@@ -9,16 +11,20 @@ import de.hswt.hrm.scheme.model.RenderedComponent;
  * @author Michael Sieger
  *
  */
-public class DirectedRenderedComponent {
-	
-	private final RenderedComponent renderedComponent;
+public class DirectedRenderedComponent extends RenderedComponent{
+
 	private Direction direction;
 	
-	public DirectedRenderedComponent(RenderedComponent renderedComponent,
+	public DirectedRenderedComponent(Component component, ThumbnailImage leftRight,
+			ThumbnailImage rightLeft, ThumbnailImage topDown, ThumbnailImage downUp,
 			Direction direction) {
-		super();
-		this.renderedComponent = renderedComponent;
+		super(component, leftRight, rightLeft, topDown, downUp);
 		this.direction = direction;
+	}
+	
+	public DirectedRenderedComponent(RenderedComponent comp, Direction direction){
+		this(comp.getComponent(), comp.getLeftRight(), comp.getRightLeft(),
+				comp.getTopDown(), comp.getDownUp(), direction);
 	}
 
 	public Direction getDirection() {
@@ -29,10 +35,4 @@ public class DirectedRenderedComponent {
 		this.direction = direction;
 	}
 
-	public RenderedComponent getRenderedComponent() {
-		return renderedComponent;
-	}
-	
-	
-	
 }
