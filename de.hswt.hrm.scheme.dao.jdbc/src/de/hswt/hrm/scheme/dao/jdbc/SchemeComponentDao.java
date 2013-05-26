9865958aa632637c5f560582312fce9612d76967
haeringl <luke-haering@web.de>
2013-05-26 14:05:38 +0200

    implement findAll-Method - not finished ->TODO

--- de.hswt.hrm.scheme.dao.jdbc/src/de/hswt/hrm/scheme/dao/jdbc/SchemeComponentDao.java
+++ de.hswt.hrm.scheme.dao.jdbc/src/de/hswt/hrm/scheme/dao/jdbc/SchemeComponentDao.java
@@ -1,7 +1,18 @@
 package de.hswt.hrm.scheme.dao.jdbc;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Collection;
 
+import org.apache.commons.dbutils.DbUtils;
+
+import de.hswt.hrm.common.database.DatabaseFactory;
+import de.hswt.hrm.common.database.NamedParameterStatement;
+import de.hswt.hrm.common.database.SqlQueryBuilder;
 import de.hswt.hrm.common.database.exception.DatabaseException;
 import de.hswt.hrm.common.database.exception.ElementNotFoundException;
 import de.hswt.hrm.common.database.exception.SaveException;
@@ -26,8 +37,25 @@ public class SchemeComponentDao implements ISchemeComponentDao {
 
     @Override
     public Collection<SchemeComponent> findAll() throws DatabaseException {
-        // TODO Auto-generated method stub
-        return null;
+        SqlQueryBuilder builder = new SqlQueryBuilder();
+        builder.select(TABLE_NAME, Fields.ID, Fields.SCHEME, Fields.COMPONENT, Fields.X_POS,
+                Fields.Y_POS, Fields.DIRECTION);
+
+        final String query = builder.toString();
+
+        try (Connection con = DatabaseFactory.getConnection()) {
+            try (NamedParameterStatement stmt = NamedParameterStatement.fromConnection(con, query)) {
+                ResultSet result = stmt.executeQuery();
+
+                Collection<SchemeComponent> schemeComponents = fromResultSet(result);
+                DbUtils.closeQuietly(result);
+
+                return schemeComponents;
+            }
+        }
+        catch (SQLException e) {
+            throw new DatabaseException(e);
+        }
     }
 
     @Override
@@ -46,7 +74,39 @@ public class SchemeComponentDao implements ISchemeComponentDao {
     public void update(SchemeComponent schemeComponent) throws ElementNotFoundException,
             SaveException {
         // TODO Auto-generated method stub
-        
+
     }
+
+    private Collection<SchemeComponent> fromResultSet(ResultSet rs) throws SQLException {
+        checkNotNull(rs, "Result must not be null.");
+        Collection<SchemeComponent> schemeComponentList = new ArrayList<>();
+
+        while (rs.next()) {
+            int id = rs.getInt(Fields.ID);
+            int xPos = rs.getInt(Fields.X_POS);
+            int yPos = rs.getInt(Fields.Y_POS);
+            //TODO fixIT 
+            //Direction direction = ??
+            //TODO null durch direction ersetzen
+            SchemeComponent schemeComponent = new SchemeComponent(id, xPos, yPos, null);
+
+            schemeComponentList.add(schemeComponent);
+        }
+
+        return schemeComponentList;
+    }
+
+    private static final String TABLE_NAME = "Scheme_Component";
+
+    private static class Fields {
+        public static final String ID = "Scheme_Component_ID";
+        public static final String SCHEME = "Scheme_Component_Scheme_FK";
+        public static final String COMPONENT = "Scheme_Component_Component_FK";
+        public static final String X_POS = "Scheme_Component_X_Position";
+        public static final String Y_POS = "Scheme_Component_Y_Position";
+        public static final String DIRECTION = "Scheme_Component_Direction";
+
+    }
+
 
 }
