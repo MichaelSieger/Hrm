085169083463a8a173891edf0ee43b728f92e54a
Tobias Placht <tobias.placht@student.hswt.de>
2013-05-26 12:36:45 +0200

    add simple methode to interchange matched/available targets

--- de.hswt.hrm.catalog.ui/src/de/hswt/hrm/catalog/ui/event/CatalogMatchingEventHandler.java
+++ de.hswt.hrm.catalog.ui/src/de/hswt/hrm/catalog/ui/event/CatalogMatchingEventHandler.java
@@ -1,13 +1,31 @@
 package de.hswt.hrm.catalog.ui.event;
+
+import org.eclipse.e4.xwt.XWT;
 import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.List;
 
 public class CatalogMatchingEventHandler {
 
     public void onAvailableTargetMouseDoubleClick(Event event) {
-        
-        List l = (List)event.widget;
-        System.out.println(l.getItem(l.getSelectionIndex()));
-        
+
+        List l = (List) event.widget;
+        String s = l.getItem(l.getSelectionIndex());
+        l.remove(s);
+        List l2 = (List) XWT.findElementByName(l, "matchedTarget");
+        l2.add(s);
+
+    }
+
+    public void onMatchedTargetMouseDoubleClick(Event event) {
+
+        List l = (List) event.widget;
+        String s = l.getItem(l.getSelectionIndex());
+        l.remove(s);
+        List l2 = (List) XWT.findElementByName(l, "availableTarget");
+        l2.add(s);
+        String[] items = l2.getItems();
+        java.util.Arrays.sort(items);
+        l2.setItems(items);
+
     }
 }
