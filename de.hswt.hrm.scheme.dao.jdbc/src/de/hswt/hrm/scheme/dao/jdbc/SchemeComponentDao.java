ef307f1641366b5565ed451d6b4196cd9dd8500f
haeringl <luke-haering@web.de>
2013-05-26 13:13:50 +0200

    add basic methods

--- de.hswt.hrm.scheme.dao.jdbc/src/de/hswt/hrm/scheme/dao/jdbc/SchemeComponentDao.java
+++ de.hswt.hrm.scheme.dao.jdbc/src/de/hswt/hrm/scheme/dao/jdbc/SchemeComponentDao.java
@@ -2,6 +2,9 @@ package de.hswt.hrm.scheme.dao.jdbc;
 
 import java.util.Collection;
 
+import de.hswt.hrm.common.database.exception.DatabaseException;
+import de.hswt.hrm.common.database.exception.ElementNotFoundException;
+import de.hswt.hrm.common.database.exception.SaveException;
 import de.hswt.hrm.component.model.Component;
 import de.hswt.hrm.scheme.dao.core.ISchemeComponentDao;
 import de.hswt.hrm.scheme.model.Scheme;
@@ -19,6 +22,31 @@ public class SchemeComponentDao implements ISchemeComponentDao {
     public void insertComponent(Scheme scheme, SchemeComponent component) {
         // TODO Auto-generated method stub
 
+    }
+
+    @Override
+    public Collection<SchemeComponent> findAll() throws DatabaseException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public SchemeComponent findById(int id) throws DatabaseException, ElementNotFoundException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public SchemeComponent insert(SchemeComponent schemeComponent) throws SaveException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void update(SchemeComponent schemeComponent) throws ElementNotFoundException,
+            SaveException {
+        // TODO Auto-generated method stub
+        
     }
 
 }
