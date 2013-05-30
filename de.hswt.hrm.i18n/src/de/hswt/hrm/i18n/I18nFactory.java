3c8f135409bd4551c3070d3c849cf3a67cd002b5
Ben <benjamin.pabst85@gmail.com>
2013-05-30 10:36:05 +0200

    Add factory for i18n.

---
+++ de.hswt.hrm.i18n/src/de/hswt/hrm/i18n/I18nFactory.java
@@ -0,0 +1,13 @@
+package de.hswt.hrm.i18n;
+
+public class I18nFactory {
+	
+	/**
+	 * Create a new i18n object for the given class.
+	 * 
+	 * @param clazz
+	 */
+	public static <T> void createI18n(final Class<T> clazz) {
+		
+	}
+}
