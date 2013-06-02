46b9f3d6d899b17aef67b792c6621f7f4308c9f4
Kleeberger <stefan.kleeberger@student.hswt.de>
2013-06-03 00:44:59 +0200

    Add FileWrite Add SystemOverviewParser Change OverallEvaluationParser

---
+++ de.hswt.hrm.report.latex/src/de/hswt/hrm/report/latex/FileWriter.java
@@ -0,0 +1,23 @@
+package de.hswt.hrm.report.latex;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+/*
+ * writes parsed LaTeX files
+ */
+
+public class FileWriter {
+
+    public void writeToFile(String dirPath, String fileName, String content) throws IOException {
+        Path filePath = Paths.get(dirPath + fileName);
+        Path path = Paths.get(dirPath);
+        Files.createDirectories(path);
+        Files.write(filePath, content.getBytes(), StandardOpenOption.CREATE);
+        Files.write(filePath, content.getBytes(), StandardOpenOption.TRUNCATE_EXISTING);
+    }
+
+}
