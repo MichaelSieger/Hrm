de902e5d98b623c520e7dac2851826fee4d80730
Tobias Placht <tobias.placht@student.hswt.de>
2013-06-02 12:45:38 +0200

    remove not needed class

--- de.hswt.hrm.catalog.ui/src/de/hswt/hrm/catalog/ui/part/CatalogMatchingPart.java
+++ de.hswt.hrm.catalog.ui/src/de/hswt/hrm/catalog/ui/part/CatalogMatchingPart.java
@@ -28,125 +28,116 @@ import de.hswt.hrm.catalog.model.Current;
 import de.hswt.hrm.catalog.model.ICatalogItem;
 import de.hswt.hrm.catalog.model.Target;
 import de.hswt.hrm.catalog.service.CatalogService;
-import de.hswt.hrm.common.ui.swt.list.ListUtil;
 
 public class CatalogMatchingPart {
 
-	@Inject
-	CatalogService catalogService;
-	@Inject
-	EPartService service;
+    @Inject
+    CatalogService catalogService;
+    @Inject
+    EPartService service;
 
-	ListViewer targets;
-	ListViewer currents;
-	ListViewer activities;
-	ListViewer catalogs;
-	ListViewer matchedTargets;
-	ListViewer matchedCurrents;
-	ListViewer matchedActivities;
+    ListViewer targets;
+    ListViewer currents;
+    ListViewer activities;
+    ListViewer catalogs;
+    ListViewer matchedTargets;
+    ListViewer matchedCurrents;
+    ListViewer matchedActivities;
 
-	@PostConstruct
-	public void postConstruct(Composite parent, IEclipseContext context) {
+    @PostConstruct
+    public void postConstruct(Composite parent, IEclipseContext context) {
 
-		URL url = CatalogPart.class.getClassLoader().getResource(
-				"de/hswt/hrm/catalog/ui/xwt/CatalogMatchingView"
-						+ IConstants.XWT_EXTENSION_SUFFIX);
+        URL url = CatalogPart.class.getClassLoader().getResource(
+                "de/hswt/hrm/catalog/ui/xwt/CatalogMatchingView" + IConstants.XWT_EXTENSION_SUFFIX);
 
-		try {
-			final Composite composite = (Composite) XWTForms.load(parent, url);
+        try {
+            final Composite composite = (Composite) XWTForms.load(parent, url);
 
-			((Button) XWT.findElementByName(composite, "back2Main"))
-					.addListener(SWT.Selection, new Listener() {
-						@Override
-						public void handleEvent(Event event) {
-							service.findPart("Clients").setVisible(false);
-							service.findPart("Places").setVisible(false);
-							service.findPart("Plants").setVisible(false);
-							service.findPart("Scheme").setVisible(false);
-							service.findPart("Catalog").setVisible(false);
-							service.findPart("Category").setVisible(false);
-							service.findPart("Matched").setVisible(false);
-							service.findPart("Main").setVisible(true);
-							service.showPart("Main", PartState.VISIBLE);
-						}
-					});
+            ((Button) XWT.findElementByName(composite, "back2Main")).addListener(SWT.Selection,
+                    new Listener() {
+                        @Override
+                        public void handleEvent(Event event) {
+                            service.findPart("Clients").setVisible(false);
+                            service.findPart("Places").setVisible(false);
+                            service.findPart("Plants").setVisible(false);
+                            service.findPart("Scheme").setVisible(false);
+                            service.findPart("Catalog").setVisible(false);
+                            service.findPart("Category").setVisible(false);
+                            service.findPart("Matched").setVisible(false);
+                            service.findPart("Main").setVisible(true);
+                            service.showPart("Main", PartState.VISIBLE);
+                        }
+                    });
 
-			targets = (ListViewer) XWT.findElementByName(composite,
-					"availableTarget");
-			currents = (ListViewer) XWT.findElementByName(composite,
-					"availableCurrent");
-			activities = (ListViewer) XWT.findElementByName(composite,
-					"availableActivity");
-			matchedActivities = (ListViewer) XWT.findElementByName(composite,
-					"matchedActivity");
-			matchedTargets = (ListViewer) XWT.findElementByName(composite,
-					"matchedTarget");
-			matchedCurrents = (ListViewer) XWT.findElementByName(composite,
-					"matchedCurrent");
-			catalogs = (ListViewer) XWT
-					.findElementByName(composite, "catalogs");
+            targets = (ListViewer) XWT.findElementByName(composite, "availableTarget");
+            currents = (ListViewer) XWT.findElementByName(composite, "availableCurrent");
+            activities = (ListViewer) XWT.findElementByName(composite, "availableActivity");
+            matchedActivities = (ListViewer) XWT.findElementByName(composite, "matchedActivity");
+            matchedTargets = (ListViewer) XWT.findElementByName(composite, "matchedTarget");
+            matchedCurrents = (ListViewer) XWT.findElementByName(composite, "matchedCurrent");
+            catalogs = (ListViewer) XWT.findElementByName(composite, "catalogs");
 
-			catalogs.setContentProvider(ArrayContentProvider.getInstance());
-			catalogs.setLabelProvider(new LabelProvider() {
-				@Override
-				public String getText(Object element) {
-					Catalog c = (Catalog) element;
-					return c.getName();
-				}
-			});
+            catalogs.setContentProvider(ArrayContentProvider.getInstance());
+            catalogs.setLabelProvider(new LabelProvider() {
+                @Override
+                public String getText(Object element) {
+                    Catalog c = (Catalog) element;
+                    return c.getName();
+                }
+            });
 
-			createLabelProvider(activities);
-			createLabelProvider(matchedTargets);
-			createLabelProvider(matchedActivities);
-			createLabelProvider(matchedCurrents);
+            createLabelProvider(activities);
+            createLabelProvider(matchedTargets);
+            createLabelProvider(matchedActivities);
+            createLabelProvider(matchedCurrents);
 
-			Collection<Catalog> catalogsFromDB = catalogService
-					.findAllCatalog();
-			Collection<ICatalogItem> items = catalogService
-					.findAllCatalogItem();
-			fillAvailableViewer(items);
+            Collection<Catalog> catalogsFromDB = catalogService.findAllCatalog();
+            Collection<ICatalogItem> items = catalogService.findAllCatalogItem();
+            fillAvailableViewer(items);
 
-			catalogs.setInput(catalogsFromDB);
-			ListUtil.sortList(catalogs);
+            catalogs.setInput(catalogsFromDB);
 
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-	private void createLabelProvider(ListViewer lv) {
+    private void createLabelProvider(ListViewer lv) {
 
-		lv.setContentProvider(ArrayContentProvider.getInstance());
+        lv.setContentProvider(ArrayContentProvider.getInstance());
 
-		lv.setLabelProvider(new LabelProvider() {
-			@Override
-			public String getText(Object element) {
-				ICatalogItem c = (ICatalogItem) element;
-				return c.getName();
-			}
-		});
-	}
+        lv.setLabelProvider(new LabelProvider() {
+            @Override
+            public String getText(Object element) {
+                ICatalogItem c = (ICatalogItem) element;
+                return c.getName();
+            }
+        });
+    }
 
-	private void fillAvailableViewer(Collection<ICatalogItem> items) {
+    private void fillAvailableViewer(Collection<ICatalogItem> items) {
 
-		Collection<Activity> a = new ArrayList<>();
-		Collection<Current> c = new ArrayList<>();
-		Collection<Target> t = new ArrayList<>();
+        Collection<Activity> a = new ArrayList<>();
+        Collection<Current> c = new ArrayList<>();
+        Collection<Target> t = new ArrayList<>();
 
-		for (ICatalogItem i : items) {
-			if (i instanceof Activity) {
-				a.add((Activity) i);
-			} else if (i instanceof Current) {
-				c.add((Current) i);
-			} else {
-				t.add((Target) i);
-			}
-		}
+        for (ICatalogItem i : items) {
+            if (i instanceof Activity) {
+                a.add((Activity) i);
+            }
+            else if (i instanceof Current) {
+                c.add((Current) i);
+            }
+            else {
+                t.add((Target) i);
+            }
+        }
 
-		targets.setInput(t);
-		currents.setInput(c);
-		activities.setInput(a);
+        targets.setInput(t);
+        currents.setInput(c);
+        activities.setInput(a);
 
-	}
+    }
 
 }
