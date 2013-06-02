e75e28d695341aff3856ac459433a41d5e09cde3
Ben <benjamin.pabst85@gmail.com>
2013-06-02 21:49:26 +0200

    Implement first example for transaction handling.

--- de.hswt.hrm.common/src/de/hswt/hrm/common/database/DatabaseFactory.java
+++ de.hswt.hrm.common/src/de/hswt/hrm/common/database/DatabaseFactory.java
@@ -4,6 +4,9 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import static com.google.common.base.Strings.*;
 
 import de.hswt.hrm.common.Config;
@@ -14,7 +17,13 @@ import de.hswt.hrm.common.database.exception.DatabaseException;
  * Class that is used to get a database connection. This may be changed to injection later on.
  */
 public final class DatabaseFactory {
-	
+    private final static Logger LOG = LoggerFactory.getLogger(DatabaseFactory.class);
+    
+    /**
+     * The current connection if running in a transaction.
+     */
+    private static Connection currentConnection;
+    
 	private DatabaseFactory() { }
 	
     /**
@@ -45,6 +54,10 @@ public final class DatabaseFactory {
         }
         
         try {
+            if (currentConnection != null) {
+                return currentConnection;
+            }
+            
             return DriverManager.getConnection(conStr, username, password);
         }
         catch (SQLException e) {
@@ -52,5 +65,76 @@ public final class DatabaseFactory {
             throw new DatabaseException(e);
         }
 
+    }
+    
+    /**
+     * Starts a new transaction. All following calls to {@link #getConnection()} will retrieve
+     * the same connection with auto commit disabled. This is will also work if the transaction
+     * should be wrapped around a couple of classes that retrieve connections independently.
+     * 
+     * <p>Example usage:</p>
+     * <code><pre>
+     * {@code
+     * // First we start a new transaction
+     * DatabaseFactory.startTransaction();
+     * // Then you are able to retrieve the connection within the running transaction
+     * Connection con = DatabaseFactory.getConnection();
+     * // know do something with the connection ...
+     * // and then commit the transaction
+     * DatabaseFactory.commitTransaction();
+     * // at least close connection that is still opened
+     * con.close();
+     * }
+     * </pre></code>
+     * 
+     * @throws DatabaseException
+     * @throws SQLException
+     * @throws IllegalStateException If there is already a transaction running.
+     */
+    public static void startTransaction() throws DatabaseException, SQLException {
+        if (currentConnection != null) {
+            throw new IllegalStateException("There is already a transaction running.");
+        }
+        
+        currentConnection = getConnection();
+        currentConnection.setAutoCommit(false);
+    }
+    
+    /**
+     * Commit the currently running transaction.
+     * <p><b>You should then close the connection you have retrieved before!</b></p>
+     * 
+     * @throws SQLException
+     * @throws IllegalStateException If there is no transaction running, or the connection of the
+     *                               current transaction is already closed.
+     */
+    public static void commitTransaction() throws SQLException {
+        if (currentConnection == null || currentConnection.isClosed()) {
+            throw new IllegalArgumentException("There was no transaction started, or the"
+                    + " connection is already closed.");
+        }
+        
+        currentConnection.commit();
+        currentConnection.setAutoCommit(true);
+        currentConnection = null;
+    }
+    
+    /**
+     * Rollback the currently running transaction.
+     * <p><b>You should then close the connection you have retrieved before!</b></p>
+     * 
+     * @throws SQLException
+     * @throws IllegalStateException If there is no transaction running, or the connection of the
+     *                               current transaction is already closed.
+     */
+    public static void rollbackTransaction() throws SQLException {
+        if (currentConnection == null || currentConnection.isClosed()) {
+            throw new IllegalArgumentException("There was no transaction started, or the"
+                    + " connection is already closed.");
+        }
+        
+        currentConnection.rollback();
+        currentConnection.setAutoCommit(true);
+        currentConnection = null;
     }
 }
