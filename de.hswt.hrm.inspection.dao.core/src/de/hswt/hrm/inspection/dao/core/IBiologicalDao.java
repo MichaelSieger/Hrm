dd0d94e4c2218b09c36fc377e118b1dd3e5a5220
haeringl <luke-haering@web.de>
2013-06-06 16:50:48 +0200

    add methods

--- de.hswt.hrm.inspection.dao.core/src/de/hswt/hrm/inspection/dao/core/IBiologicalDao.java
+++ de.hswt.hrm.inspection.dao.core/src/de/hswt/hrm/inspection/dao/core/IBiologicalDao.java
@@ -5,7 +5,7 @@ import java.util.Collection;
 import de.hswt.hrm.common.database.exception.DatabaseException;
 import de.hswt.hrm.common.database.exception.ElementNotFoundException;
 import de.hswt.hrm.common.database.exception.SaveException;
-import de.hswt.hrm.inspection.model.Inspection;
+import de.hswt.hrm.inspection.model.Biological;
 
 /**
  * Defines all the public methods to interact with the storage system for activitys.
@@ -15,31 +15,30 @@ public interface IBiologicalDao {
     /**
      * @return All inspections from storage.
      */
-    Collection<Inspection> findAll() throws DatabaseException;
+    Collection<Biological> findAll() throws DatabaseException;
 
     /**
-     * @param id of the target inspection.
-     * @return inspection with the given id.
+     * @param id of the target biological.
+     * @return biological with the given id.
      * @throws ElementNotFoundException If the given id is not present in the database.
      */
-    Inspection findById(int id) throws DatabaseException, ElementNotFoundException;
+    Biological findById(int id) throws DatabaseException, ElementNotFoundException;
 
     /**
-     * Add a new inspection to storage.
-     * 
-     * @param inspection Inspection that should be stored.
-     * @return Newly generated inspection (also holding the correct id).
-     * @throws SaveException If the inspection could not be inserted.
+     * Add a new biological to storage.
+     * @param biological Biological that should be stored.
+     * @return Newly generated biological (also holding the correct id).
+     * @throws SaveException If the biological could not be inserted.
      */
-    Inspection insert(Inspection inspection) throws SaveException;
+    Biological insert(Biological biological) throws SaveException;
 
     /**
-     * Update an existing inspection in storage.
+     * Update an existing biological in storage.
      * 
-     * @param inspection Inspection that should be updated.
-     * @throws ElementNotFoundException If the given Inspection is not present in the database.
-     * @throws SaveException If the inspection could not be updated.
+     * @param biological Biological that should be updated.
+     * @throws ElementNotFoundException If the given Biological is not present in the database.
+     * @throws SaveException If the biological could not be updated.
      */
-    void update(Inspection inspection) throws ElementNotFoundException, SaveException;
+    void update(Biological biological) throws ElementNotFoundException, SaveException;
 }
 
