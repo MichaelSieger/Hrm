8569a39e1c73736719dcea28bf1bd00be171bd6f
haeringl <luke-haering@web.de>
2013-06-06 16:07:49 +0200

    add methods

--- de.hswt.hrm.photo.dao.core/src/de/hswt/hrm/photo/dao/core/IPhotoDao.java
+++ de.hswt.hrm.photo.dao.core/src/de/hswt/hrm/photo/dao/core/IPhotoDao.java
@@ -1,5 +1,44 @@
 package de.hswt.hrm.photo.dao.core;
 
+import java.util.Collection;
+
+import de.hswt.hrm.common.database.exception.DatabaseException;
+import de.hswt.hrm.common.database.exception.ElementNotFoundException;
+import de.hswt.hrm.common.database.exception.SaveException;
+import de.hswt.hrm.photo.model.Photo;
+
+/**
+ * Defines all the public methods to interact with the storage system for activitys.
+ */
 public interface IPhotoDao {
 
+    /**
+     * @return All photos from storage.
+     */
+    Collection<Photo> findAll() throws DatabaseException;
+
+    /**
+     * @param id of the target photo.
+     * @return Photo with the given id.
+     * @throws ElementNotFoundException If the given id is not present in the database.
+     */
+    Photo findById(int id) throws DatabaseException, ElementNotFoundException;
+
+    /**
+     * Add a new photo to storage.
+     * 
+     * @param photo Photo that should be stored.
+     * @return Newly generated photo (also holding the correct id).
+     * @throws SaveException If the photo could not be inserted.
+     */
+    Photo insert(Photo photo) throws SaveException;
+
+    /**
+     * Update an existing photo in storage.
+     * 
+     * @param photo Photo that should be updated.
+     * @throws ElementNotFoundException If the given Photo is not present in the database.
+     * @throws SaveException If the photo could not be updated.
+     */
+    void update(Photo photo) throws ElementNotFoundException, SaveException;
 }
