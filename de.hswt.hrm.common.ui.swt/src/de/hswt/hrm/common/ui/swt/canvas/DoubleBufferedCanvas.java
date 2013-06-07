1805ab4d86a56f9a0bc4cd33df3b08f40d7c3063
michi <sieger-michael@web.de>
2013-06-07 08:50:32 +0200

    DoubleBufferedCanvas

---
+++ de.hswt.hrm.common.ui.swt/src/de/hswt/hrm/common/ui/swt/canvas/DoubleBufferedCanvas.java
@@ -0,0 +1,49 @@
+package de.hswt.hrm.common.ui.swt.canvas;
+
+import org.eclipse.swt.events.PaintEvent;
+import org.eclipse.swt.events.PaintListener;
+import org.eclipse.swt.graphics.GC;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.Rectangle;
+import org.eclipse.swt.widgets.Canvas;
+import org.eclipse.swt.widgets.Composite;
+
+/**
+ * This is a Canvas whose content is first drawn into a image, and then
+ * this image is drawn to the display.
+ * 
+ * @author Michael Sieger
+ *
+ */
+public class DoubleBufferedCanvas extends Canvas{
+	
+	private Image backbuffer;
+
+	public DoubleBufferedCanvas(Composite parent, int style) {
+		super(parent, style);
+		this.addPaintListener(new PaintListener() {
+			
+			@Override
+			public void paintControl(PaintEvent e) {
+				Image img = getBackbuffer();
+				GC gc = new GC(img);
+				drawBuffered(gc);
+				e.gc.drawImage(img, 0, 0);	//Copy backbuffer to frontbuffer
+			}
+		});
+	}
+
+	protected void drawBuffered(GC gc){
+		
+	}
+	
+	private Image getBackbuffer(){
+		Rectangle bufRec = backbuffer.getBounds();
+		Rectangle widRec = getBounds();
+		if(backbuffer == null || bufRec.width != widRec.width || bufRec.height != widRec.height){
+			backbuffer = new Image(getDisplay(), widRec.width, widRec.height);
+		}
+		return backbuffer;
+	}
+	
+}
