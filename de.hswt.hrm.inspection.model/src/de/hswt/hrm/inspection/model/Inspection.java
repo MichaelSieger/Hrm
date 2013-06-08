6f299364a04fa3b927eb0ee0fa6c7224aa09df59
Ben <benjamin.pabst85@gmail.com>
2013-06-08 17:22:04 +0200

    Add id field to inspection model.

--- de.hswt.hrm.inspection.model/src/de/hswt/hrm/inspection/model/Inspection.java
+++ de.hswt.hrm.inspection.model/src/de/hswt/hrm/inspection/model/Inspection.java
@@ -1,5 +1,17 @@
 package de.hswt.hrm.inspection.model;
 
 public class Inspection {
+	private final int id;
+	
+	public Inspection() {
+		this(-1);
+	}
+	
+	public Inspection(final int id) {
+		this.id = id;
+	}
 
+	public int getId() {
+		return id;
+	}
 }
