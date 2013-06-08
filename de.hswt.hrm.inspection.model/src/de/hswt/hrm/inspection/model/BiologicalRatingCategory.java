dd61ff9219222b1babc7b85b49cfa8a00f5797c4
Ben <benjamin.pabst85@gmail.com>
2013-06-08 17:51:28 +0200

    Add fields for biological rating flag model.

--- de.hswt.hrm.inspection.model/src/de/hswt/hrm/inspection/model/BiologicalRatingCategory.java
+++ de.hswt.hrm.inspection.model/src/de/hswt/hrm/inspection/model/BiologicalRatingCategory.java
@@ -1,5 +1,27 @@
 package de.hswt.hrm.inspection.model;
 
 public class BiologicalRatingCategory {
-
+	private final int id;
+	private String name;
+	
+	public BiologicalRatingCategory(final String name) {
+		this(-1, name);
+	}
+	
+	public BiologicalRatingCategory(final int id, final String name) {
+		this.id = id;
+		this.name = name;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(final String name) {
+		this.name = name;
+	}
 }
