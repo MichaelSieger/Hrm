4e3f41153b5ebefe9b6217b27fc6bfe48b2c8999
Ben <benjamin.pabst85@gmail.com>
2013-06-08 16:58:56 +0200

    Add empty dummy for biological rating DAO.

--- de.hswt.hrm.inspection.dao.jdbc/src/de/hswt/hrm/inspection/dao/jdbc/BiologicalRatingDao.java
+++ de.hswt.hrm.inspection.dao.jdbc/src/de/hswt/hrm/inspection/dao/jdbc/BiologicalRatingDao.java
@@ -1,5 +1,37 @@
 package de.hswt.hrm.inspection.dao.jdbc;
 
-public class BiologicalRatingDao {
+import java.util.Collection;
+
+import de.hswt.hrm.common.database.exception.DatabaseException;
+import de.hswt.hrm.common.database.exception.ElementNotFoundException;
+import de.hswt.hrm.common.database.exception.SaveException;
+import de.hswt.hrm.common.exception.NotImplementedException;
+import de.hswt.hrm.inspection.dao.core.IBiologicalRatingDao;
+import de.hswt.hrm.inspection.model.BiologicalRating;
+
+public class BiologicalRatingDao implements IBiologicalRatingDao {
+
+	@Override
+	public Collection<BiologicalRating> findAll() throws DatabaseException {
+		throw new NotImplementedException();
+	}
+
+	@Override
+	public BiologicalRating findById(int id) throws DatabaseException,
+			ElementNotFoundException {
+		throw new NotImplementedException();
+	}
+
+	@Override
+	public BiologicalRating insert(BiologicalRating biological)
+			throws SaveException {
+		throw new NotImplementedException();
+	}
+
+	@Override
+	public void update(BiologicalRating biological)
+			throws ElementNotFoundException, SaveException {
+		throw new NotImplementedException();
+	}
 
 }
