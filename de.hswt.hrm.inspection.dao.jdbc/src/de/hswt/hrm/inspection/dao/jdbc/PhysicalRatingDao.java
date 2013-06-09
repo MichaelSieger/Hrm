3cb36bae8675a599a6195a160dba54fcb84ec253
Ben <benjamin.pabst85@gmail.com>
2013-06-09 15:04:46 +0200

    Add dummy for physical rating DAO.

---
+++ de.hswt.hrm.inspection.dao.jdbc/src/de/hswt/hrm/inspection/dao/jdbc/PhysicalRatingDao.java
@@ -0,0 +1,39 @@
+package de.hswt.hrm.inspection.dao.jdbc;
+
+import java.util.Collection;
+
+import de.hswt.hrm.common.database.exception.DatabaseException;
+import de.hswt.hrm.common.database.exception.ElementNotFoundException;
+import de.hswt.hrm.common.database.exception.SaveException;
+import de.hswt.hrm.inspection.dao.core.IPhysicalRatingDao;
+import de.hswt.hrm.inspection.model.PhysicalRating;
+
+public class PhysicalRatingDao implements IPhysicalRatingDao {
+
+	@Override
+	public Collection<PhysicalRating> findAll() throws DatabaseException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public PhysicalRating findById(int id) throws DatabaseException,
+			ElementNotFoundException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public PhysicalRating insert(PhysicalRating physical) throws SaveException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void update(PhysicalRating physical)
+			throws ElementNotFoundException, SaveException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
