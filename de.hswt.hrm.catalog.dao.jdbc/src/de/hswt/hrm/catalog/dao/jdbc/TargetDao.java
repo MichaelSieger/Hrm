241766728950963de075a69cabd9ad7467c11331
Ben <benjamin.pabst85@gmail.com>
2013-06-09 13:39:09 +0200

    Implement method to connect targets with catalogs.

--- de.hswt.hrm.catalog.dao.jdbc/src/de/hswt/hrm/catalog/dao/jdbc/TargetDao.java
+++ de.hswt.hrm.catalog.dao.jdbc/src/de/hswt/hrm/catalog/dao/jdbc/TargetDao.java
@@ -1,26 +1,39 @@
 package de.hswt.hrm.catalog.dao.jdbc;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collection;
 
+import javax.inject.Inject;
+
 import org.apache.commons.dbutils.DbUtils;
 
 import static com.google.common.base.Preconditions.checkArgument;
-import static com.google.common.base.Preconditions.checkNotNull;
-
 import de.hswt.hrm.common.database.DatabaseFactory;
 import de.hswt.hrm.common.database.NamedParameterStatement;
 import de.hswt.hrm.common.database.SqlQueryBuilder;
 import de.hswt.hrm.common.database.exception.DatabaseException;
 import de.hswt.hrm.common.database.exception.ElementNotFoundException;
 import de.hswt.hrm.common.database.exception.SaveException;
+import de.hswt.hrm.catalog.model.Catalog;
 import de.hswt.hrm.catalog.model.Target;
+import de.hswt.hrm.catalog.dao.core.ICatalogDao;
 import de.hswt.hrm.catalog.dao.core.ITargetDao;
 
 public class TargetDao implements ITargetDao {
+	private final ICatalogDao catalogDao;
+	
+	// TODO: Add LOG output
+	@Inject
+	public TargetDao(final ICatalogDao catalogDao) {
+		checkNotNull(catalogDao, "CatalogDao not properly injected to TargetDao.");
+		
+		this.catalogDao = catalogDao;
+	}
 
     @Override
     public Collection<Target> findAll() throws DatabaseException {
@@ -117,6 +130,39 @@ public class TargetDao implements ITargetDao {
             throw new SaveException(e);
         }
     }
+    
+    @Override
+    public void addToCatalog(Catalog catalog, Target target) throws SaveException {
+        SqlQueryBuilder builder = new SqlQueryBuilder();
+        builder.insert(CROSS_TABLE_NAME, Fields.CROSS_TARGET_FK, Fields.CROSS_CATALOG_FK);
+
+        // Insert target and current if not already in the database
+        if (catalog.getId() < 0) {
+            catalog = catalogDao.insert(catalog);
+        }
+        
+        if (target.getId() < 0) {
+            target = insert(target);
+        }
+        
+        final String query = builder.toString();
+
+        try (Connection con = DatabaseFactory.getConnection()) {
+            try (NamedParameterStatement stmt = NamedParameterStatement.fromConnection(con, query)) {
+                stmt.setParameter(Fields.CROSS_TARGET_FK, target.getId());
+                stmt.setParameter(Fields.CROSS_CATALOG_FK, catalog.getId());
+
+                int affectedRows = stmt.executeUpdate();
+                if (affectedRows != 1) {
+                    throw new SaveException();
+                }
+            }
+
+        }
+        catch (SQLException | DatabaseException e) {
+            throw new SaveException(e);
+        }
+    }
 
     @Override
     public void update(Target target) throws ElementNotFoundException, SaveException {
@@ -167,11 +213,13 @@ public class TargetDao implements ITargetDao {
     }
 
     private static final String TABLE_NAME = "State_Target";
+    private static final String CROSS_TABLE_NAME = "Catalog_Target";
 
     private static class Fields {
         public static final String ID = "State_Target_ID";
         public static final String NAME = "State_Target_Name";
         public static final String TEXT = "State_Target_Text";
-
+        public static final String CROSS_CATALOG_FK = "Category_Target_Catalog_FK";
+        public static final String CROSS_TARGET_FK = "Category_Target_State_Target_FK";
     }
 }
