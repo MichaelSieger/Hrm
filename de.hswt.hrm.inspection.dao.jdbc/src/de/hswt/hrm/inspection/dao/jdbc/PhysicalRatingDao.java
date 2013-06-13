38de2b63be7c3fb6c34494b0bd498ca76ed16cc0
haeringl <luke-haering@web.de>
2013-06-13 18:26:27 +0200

    add insert method

--- de.hswt.hrm.inspection.dao.jdbc/src/de/hswt/hrm/inspection/dao/jdbc/PhysicalRatingDao.java
+++ de.hswt.hrm.inspection.dao.jdbc/src/de/hswt/hrm/inspection/dao/jdbc/PhysicalRatingDao.java
@@ -22,17 +22,17 @@ import de.hswt.hrm.scheme.model.Scheme;
 
 public class PhysicalRatingDao implements IPhysicalRatingDao {
 
-	private final IComponentDao componentDao;
-	
-	// TODO: add LOG messages
-	public PhysicalRatingDao(final IComponentDao componentDao) {
-		checkNotNull(componentDao, "ComponentDao not properly injected to PhysicalRatingDao.");
-		
-		this.componentDao = componentDao;
-	}
-	
-	@Override
-	public Collection<PhysicalRating> findAll() throws DatabaseException {
+    private final IComponentDao componentDao;
+
+    // TODO: add LOG messages
+    public PhysicalRatingDao(final IComponentDao componentDao) {
+        checkNotNull(componentDao, "ComponentDao not properly injected to PhysicalRatingDao.");
+
+        this.componentDao = componentDao;
+    }
+
+    @Override
+    public Collection<PhysicalRating> findAll() throws DatabaseException {
         SqlQueryBuilder builder = new SqlQueryBuilder();
         builder.select(TABLE_NAME, Fields.ID, Fields.RATING, Fields.NOTE, Fields.COMPONENT_FK,
                 Fields.REPORT_FK);
@@ -54,73 +54,110 @@ public class PhysicalRatingDao implements IPhysicalRatingDao {
         }
     }
 
-	@Override
-	public PhysicalRating findById(int id) throws DatabaseException,
-			ElementNotFoundException {
-		
-		checkArgument(id >= 0, "ID must be non negative.");
-		
-		SqlQueryBuilder builder = new SqlQueryBuilder();
-		builder.select(TABLE_NAME, Fields.ID, Fields.RATING, Fields.NOTE, Fields.COMPONENT_FK,
-				Fields.REPORT_FK);
-		builder.where(Fields.ID);
-		
-		String query = builder.toString();
-		
-		try (Connection con = DatabaseFactory.getConnection()) {
-			try (NamedParameterStatement stmt = NamedParameterStatement.fromConnection(con, query)) {
-				stmt.setParameter(Fields.ID, id);
-				
-				ResultSet rs = stmt.executeQuery();
-				Collection<PhysicalRating> ratings = fromResultSet(rs);
-				rs.close();
-				
-				if (ratings.isEmpty()) {
-					throw new ElementNotFoundException();
-				}
-				else if (ratings.size() > 1) {
-					throw new DatabaseException("ID is not unique.");
-				}
-				
-				return ratings.iterator().next();
-			}
-		}
-		catch (SQLException e) {
-			throw new DatabaseException("Unknown error.", e);
-		}
-	}
+    @Override
+    public PhysicalRating findById(int id) throws DatabaseException, ElementNotFoundException {
 
-	@Override
-	public PhysicalRating insert(PhysicalRating physical) throws SaveException {
-		// TODO Auto-generated method stub
-		return null;
-	}
+        checkArgument(id >= 0, "ID must be non negative.");
 
-	@Override
-	public void update(PhysicalRating physical)
-			throws ElementNotFoundException, SaveException {
-		// TODO Auto-generated method stub
-		
-	}
+        SqlQueryBuilder builder = new SqlQueryBuilder();
+        builder.select(TABLE_NAME, Fields.ID, Fields.RATING, Fields.NOTE, Fields.COMPONENT_FK,
+                Fields.REPORT_FK);
+        builder.where(Fields.ID);
 
-	@Override
-	public Collection<PhysicalRating> findByScheme(Scheme scheme) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
-	private Collection<PhysicalRating> fromResultSet(final ResultSet rs) {
-		// Add component here
-		throw new NotImplementedException();
-	}
-	
-	private static final String TABLE_NAME = "Component_Physical_Rating";
-	
-	private static final class Fields {
-		private static final String ID = "Component_Physical_Rating_ID";
-		private static final String RATING = "Component_Physical_Rating_Rating";
-		private static final String NOTE = "Component_Physical_Rating_Note";
-		private static final String COMPONENT_FK = "Component_Physical_Rating_Component_FK";
-		private static final String REPORT_FK = "Component_Physical_Rating_Report_FK";
-	}
+        String query = builder.toString();
+
+        try (Connection con = DatabaseFactory.getConnection()) {
+            try (NamedParameterStatement stmt = NamedParameterStatement.fromConnection(con, query)) {
+                stmt.setParameter(Fields.ID, id);
+
+                ResultSet rs = stmt.executeQuery();
+                Collection<PhysicalRating> ratings = fromResultSet(rs);
+                rs.close();
+
+                if (ratings.isEmpty()) {
+                    throw new ElementNotFoundException();
+                }
+                else if (ratings.size() > 1) {
+                    throw new DatabaseException("ID is not unique.");
+                }
+
+                return ratings.iterator().next();
+            }
+        }
+        catch (SQLException e) {
+            throw new DatabaseException("Unknown error.", e);
+        }
+    }
+
+    @Override
+    public PhysicalRating insert(PhysicalRating physicalRating) throws SaveException {
+        SqlQueryBuilder builder = new SqlQueryBuilder();
+        builder.insert(TABLE_NAME, Fields.RATING, Fields.NOTE, Fields.COMPONENT_FK,
+                Fields.REPORT_FK);
+
+        final String query = builder.toString();
+
+        try (Connection con = DatabaseFactory.getConnection()) {
+            try (NamedParameterStatement stmt = NamedParameterStatement.fromConnection(con, query)) {
+                stmt.setParameter(Fields.RATING, physicalRating.getRating());
+                stmt.setParameter(Fields.NOTE, physicalRating.getNote());
+                stmt.setParameter(Fields.COMPONENT_FK, physicalRating.getComponent().get().getId());
+                stmt.setParameter(Fields.REPORT_FK, physicalRating.getReport.get().getId());
+
+                int affectedRows = stmt.executeUpdate();
+                if (affectedRows != 1) {
+                    throw new SaveException();
+                }
+
+                try (ResultSet generatedKeys = stmt.getGeneratedKeys()) {
+                    if (generatedKeys.next()) {
+                        int id = generatedKeys.getInt(1);
+
+                        // Create new Place with id
+                        PhysicalRating inserted = new PhysicalRating(id,
+                                physicalRating.getRating(), physicalRating.getNote());
+
+                        inserted.setComponent(physicalRating.getComponent().orNull());
+                        inserted.setReport(physicalRating.getReport().orNull());
+
+                        return inserted;
+                    }
+                    else {
+                        throw new SaveException("Could not retrieve generated ID.");
+                    }
+                }
+            }
+
+        }
+        catch (SQLException | DatabaseException e) {
+            throw new SaveException(e);
+        }
+    }
+
+    @Override
+    public void update(PhysicalRating physical) throws ElementNotFoundException, SaveException {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public Collection<PhysicalRating> findByScheme(Scheme scheme) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    private Collection<PhysicalRating> fromResultSet(final ResultSet rs) {
+        // Add component here
+        throw new NotImplementedException();
+    }
+
+    private static final String TABLE_NAME = "Component_Physical_Rating";
+
+    private static final class Fields {
+        private static final String ID = "Component_Physical_Rating_ID";
+        private static final String RATING = "Component_Physical_Rating_Rating";
+        private static final String NOTE = "Component_Physical_Rating_Note";
+        private static final String COMPONENT_FK = "Component_Physical_Rating_Component_FK";
+        private static final String REPORT_FK = "Component_Physical_Rating_Report_FK";
+    }
 }
