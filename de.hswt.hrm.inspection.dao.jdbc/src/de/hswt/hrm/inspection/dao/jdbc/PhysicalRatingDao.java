bf18bcf7cd147b03c4c4fc30fa1ba1cad15d2e76
haeringl <luke-haering@web.de>
2013-06-13 19:22:30 +0200

    implement findByInspection

--- de.hswt.hrm.inspection.dao.jdbc/src/de/hswt/hrm/inspection/dao/jdbc/PhysicalRatingDao.java
+++ de.hswt.hrm.inspection.dao.jdbc/src/de/hswt/hrm/inspection/dao/jdbc/PhysicalRatingDao.java
@@ -6,6 +6,7 @@ import static com.google.common.base.Preconditions.checkNotNull;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Collection;
 
 import de.hswt.hrm.common.database.DatabaseFactory;
@@ -16,7 +17,10 @@ import de.hswt.hrm.common.database.exception.ElementNotFoundException;
 import de.hswt.hrm.common.database.exception.SaveException;
 import de.hswt.hrm.common.exception.NotImplementedException;
 import de.hswt.hrm.component.dao.core.IComponentDao;
+import de.hswt.hrm.component.model.Component;
 import de.hswt.hrm.inspection.dao.core.IPhysicalRatingDao;
+import de.hswt.hrm.inspection.model.BiologicalRating;
+import de.hswt.hrm.inspection.model.Inspection;
 import de.hswt.hrm.inspection.model.PhysicalRating;
 import de.hswt.hrm.scheme.model.Scheme;
 
@@ -168,14 +172,52 @@ public class PhysicalRatingDao implements IPhysicalRatingDao {
     }
 
     @Override
-    public Collection<PhysicalRating> findByScheme(Scheme scheme) {
-        // TODO Auto-generated method stub
-        return null;
+    public Collection<PhysicalRating> findByInspection(Inspection inspection) {
+
+        checkNotNull(inspection, "Inspection is mandatory.");
+        checkArgument(inspection.getId() >= 0, "Inspection must have a valid ID.");
+
+
+        SqlQueryBuilder builder = new SqlQueryBuilder();
+        builder.select(TABLE_NAME, Fields.ID, Fields.RATING, Fields.NOTE, Fields.COMPONENT_FK,
+                Fields.REPORT_FK);
+        builder.where(Fields.REPORT_FK);
+
+        String query = builder.toString();
+
+        try (Connection con = DatabaseFactory.getConnection()) {
+            try (NamedParameterStatement stmt = NamedParameterStatement.fromConnection(con, query)) {
+                stmt.setParameter(Fields.REPORT_FK, inspection.getId());
+
+                ResultSet rs = stmt.executeQuery();
+                Collection<PhysicalRating> ratings = fromResultSet(rs);
+                rs.close();
+                return ratings;
+            }
+        }
+        catch (SQLException e) {
+            throw new DatabaseException("Unexpected error.", e);
+        }
     }
 
     private Collection<PhysicalRating> fromResultSet(final ResultSet rs) {
-        // Add component here
-        throw new NotImplementedException();
+        // FIXME Add component here
+        
+            checkNotNull(rs, "Result must not be null.");
+            Collection<PhysicalRating> physicalRatingList = new ArrayList<>();
+
+            while (rs.next()) {
+                int id = rs.getInt(Fields.ID);
+                String rating = rs.getString(Fields.RATING);
+                String note = rs.getString(Fields.NOTE);
+                PhysicalRating physicalRating = new PhysicalRating(id, rating, note);
+                physicalRating.setReport(InspectionDao.findById(rs.getInt(Fields.REPORT_FK)));
+                physicalRating.setComponent(ComponentDao.findById(rs.getInt(Fields.COMPONENT_FK)));
+                physicalRatingList.add(physicalRating);
+            }
+
+            return physicalRatingList;
+        
     }
 
     private static final String TABLE_NAME = "Component_Physical_Rating";
