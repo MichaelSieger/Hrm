466e755135c426db0487c18ddb55b2d3aad9cd85
Anton Schreck <anton.schreck@student.hswt.de>
2013-06-14 13:43:24 +0200

    Update design

--- de.hswt.hrm.contact.ui/src/de/hswt/hrm/contact/ui/wizard/ContactWizardPageTwo.java
+++ de.hswt.hrm.contact.ui/src/de/hswt/hrm/contact/ui/wizard/ContactWizardPageTwo.java
@@ -12,11 +12,14 @@ import org.eclipse.swt.events.KeyEvent;
 import org.eclipse.swt.events.KeyListener;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Text;
+import org.eclipse.ui.forms.widgets.Section;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.google.common.base.Optional;
 
+import de.hswt.hrm.common.ui.swt.forms.FormUtil;
+import de.hswt.hrm.common.ui.swt.layouts.PageContainerFillLayout;
 import de.hswt.hrm.contact.model.Contact;
 
 public class ContactWizardPageTwo extends WizardPage{
@@ -37,6 +40,7 @@ public class ContactWizardPageTwo extends WizardPage{
     
     @Override
     public void createControl(Composite parent) {
+        parent.setLayout(new PageContainerFillLayout());
         URL url = ContactWizardPageTwo.class.getClassLoader().getResource(
                 "de/hswt/hrm/contact/ui/xwt/ContactWizardWindowPageTwo"+IConstants.XWT_EXTENSION_SUFFIX);
         try {
@@ -47,6 +51,7 @@ public class ContactWizardPageTwo extends WizardPage{
         if (this.contact.isPresent()) {
             updateFields(container);
         }
+        FormUtil.initSectionColors((Section) XWT.findElementByName(container, "Optional"));
         setControl(container);
         setKeyListener();
     }
