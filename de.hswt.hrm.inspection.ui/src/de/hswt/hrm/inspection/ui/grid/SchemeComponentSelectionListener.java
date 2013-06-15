35ce4ff2b79bc35678d9d7a558e4b837142e96d8
Michi <sieger-michael@web.de>
2013-06-15 12:30:04 +0200

    comment

--- de.hswt.hrm.inspection.ui/src/de/hswt/hrm/inspection/ui/grid/SchemeComponentSelectionListener.java
+++ de.hswt.hrm.inspection.ui/src/de/hswt/hrm/inspection/ui/grid/SchemeComponentSelectionListener.java
@@ -2,8 +2,18 @@ package de.hswt.hrm.inspection.ui.grid;
 
 import de.hswt.hrm.scheme.model.SchemeComponent;
 
+/**
+ * A Listener that is called, if a component is selected.
+ * 
+ * @author Michael Sieger
+ *
+ */
 public interface SchemeComponentSelectionListener {
 	
+	/**
+	 * Called on component selection
+	 * @param selected The selected component
+	 */
 	void selected(SchemeComponent selected);
 
 }
