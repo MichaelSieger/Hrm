f166dc4c48b1fd33c5877fff48ebeb7271345227
Michi <sieger-michael@web.de>
2013-06-15 12:28:34 +0200

    SchemeComponentSelectionListener

---
+++ de.hswt.hrm.inspection.ui/src/de/hswt/hrm/inspection/ui/grid/SchemeComponentSelectionListener.java
@@ -0,0 +1,9 @@
+package de.hswt.hrm.inspection.ui.grid;
+
+import de.hswt.hrm.scheme.model.SchemeComponent;
+
+public interface SchemeComponentSelectionListener {
+	
+	void selected(SchemeComponent selected);
+
+}
