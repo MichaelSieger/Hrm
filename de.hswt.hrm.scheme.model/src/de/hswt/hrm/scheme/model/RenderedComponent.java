39be3c170ab3a0b559b71d349321753ceb036a35
Michi <michael.sieger@student.hswt.de>
2013-06-22 13:25:22 +0200

    hashCode, equals

--- de.hswt.hrm.scheme.model/src/de/hswt/hrm/scheme/model/RenderedComponent.java
+++ de.hswt.hrm.scheme.model/src/de/hswt/hrm/scheme/model/RenderedComponent.java
@@ -1,5 +1,7 @@
 package de.hswt.hrm.scheme.model;
 
+import com.google.common.base.Preconditions;
+
 import de.hswt.hrm.component.model.Component;
 
 /**
@@ -53,18 +55,53 @@ public class RenderedComponent {
 	 * @return
 	 */
 	public ThumbnailImage getByDirection(Direction d){
+		ThumbnailImage img;
 		switch(d){
 		case downUp:
-			return downUp;
+			img = downUp;
+			break;
 		case upDown:
-			return upDown;
+			img = upDown;
+			break;
 		case leftRight:
-			return leftRight;
+			img = leftRight;
+			break;
 		case rightLeft:
-			return rightLeft;
+			img = rightLeft;
+			break;
 		default:
 			throw new RuntimeException("There are more than 4 directions?");
 		}
+		//Preconditions.checkNotNull(img, "The direction doesnt exist");
+		return img;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((component == null) ? 0 : component.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		RenderedComponent other = (RenderedComponent) obj;
+		if (component == null) {
+			if (other.component != null)
+				return false;
+		} else if (!component.equals(other.component))
+			return false;
+		return true;
+	}
+	
+	
 
 }
