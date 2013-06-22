ee5e6d9e592d2f1dad70561414165e6fabbf9d00
haeringl <luke-haering@web.de>
2013-06-22 15:00:38 +0200

    add insert method to inspection dao

--- de.hswt.hrm.inspection.dao.jdbc/src/de/hswt/hrm/inspection/dao/jdbc/InspectionDao.java
+++ de.hswt.hrm.inspection.dao.jdbc/src/de/hswt/hrm/inspection/dao/jdbc/InspectionDao.java
@@ -26,9 +26,9 @@ public class InspectionDao implements IInspectionDao {
     public Collection<Inspection> findAll() throws DatabaseException {
         SqlQueryBuilder builder = new SqlQueryBuilder();
         builder.select(TABLE_NAME, Fields.ID, Fields.LAYOUT_FK, Fields.PLANT_FK,
-                Fields.REQUESTER_FK, Fields.CONTRACTOR_FK, Fields.CHECKER_FK, Fields.JOBDATE,
-                Fields.REPORTDATE, Fields.NEXTDATE, Fields.TEMPERATURE, Fields.HUMIDITY,
-                Fields.SUMMARY, Fields.TITEL, Fields.TEMPERATURERATING,
+                Fields.REQUESTER_FK, Fields.CONTRACTOR_FK, Fields.CHECKER_FK,
+                Fields.INSPECTIONDATE, Fields.REPORTDATE, Fields.NEXTDATE, Fields.TEMPERATURE,
+                Fields.HUMIDITY, Fields.SUMMARY, Fields.TITEL, Fields.TEMPERATURERATING,
                 Fields.TEMPERATUREQUANTIFIER, Fields.HUMIDITYRATING, Fields.HUMIDITYQUANTIFIER);
 
         String query = builder.toString();
@@ -55,9 +55,9 @@ public class InspectionDao implements IInspectionDao {
 
         SqlQueryBuilder builder = new SqlQueryBuilder();
         builder.select(TABLE_NAME, Fields.ID, Fields.LAYOUT_FK, Fields.PLANT_FK,
-                Fields.REQUESTER_FK, Fields.CONTRACTOR_FK, Fields.CHECKER_FK, Fields.JOBDATE,
-                Fields.REPORTDATE, Fields.NEXTDATE, Fields.TEMPERATURE, Fields.HUMIDITY,
-                Fields.SUMMARY, Fields.TITEL, Fields.TEMPERATURERATING,
+                Fields.REQUESTER_FK, Fields.CONTRACTOR_FK, Fields.CHECKER_FK,
+                Fields.INSPECTIONDATE, Fields.REPORTDATE, Fields.NEXTDATE, Fields.TEMPERATURE,
+                Fields.HUMIDITY, Fields.SUMMARY, Fields.TITEL, Fields.TEMPERATURERATING,
                 Fields.TEMPERATUREQUANTIFIER, Fields.HUMIDITYRATING, Fields.HUMIDITYQUANTIFIER);
         builder.where(Fields.ID);
 
@@ -88,8 +88,75 @@ public class InspectionDao implements IInspectionDao {
 
     @Override
     public Inspection insert(Inspection inspection) throws SaveException {
-        // TODO Auto-generated method stub
-        return null;
+        SqlQueryBuilder builder = new SqlQueryBuilder();
+        builder.insert(TABLE_NAME, Fields.LAYOUT_FK, Fields.PLANT_FK, Fields.REQUESTER_FK,
+                Fields.CONTRACTOR_FK, Fields.CHECKER_FK, Fields.INSPECTIONDATE, Fields.REPORTDATE,
+                Fields.NEXTDATE, Fields.TEMPERATURE, Fields.HUMIDITY, Fields.SUMMARY, Fields.TITEL,
+                Fields.TEMPERATURERATING, Fields.TEMPERATUREQUANTIFIER, Fields.HUMIDITYRATING,
+                Fields.HUMIDITYQUANTIFIER);
+
+        final String query = builder.toString();
+
+        try (Connection con = DatabaseFactory.getConnection()) {
+            try (NamedParameterStatement stmt = NamedParameterStatement.fromConnection(con, query)) {
+                stmt.setParameter(Fields.LAYOUT_FK, inspection.getLayout().getId());
+                stmt.setParameter(Fields.PLANT_FK, inspection.getPlant().getId());
+                stmt.setParameter(Fields.REQUESTER_FK, inspection.getRequester().get().getId());
+                stmt.setParameter(Fields.CONTRACTOR_FK, inspection.getContractor().get().getId());
+                stmt.setParameter(Fields.CHECKER_FK, inspection.getChecker().get().getId());
+                stmt.setParameter(Fields.INSPECTIONDATE, inspection.getInspectionDate());
+                stmt.setParameter(Fields.REPORTDATE, inspection.getReportDate());
+                stmt.setParameter(Fields.NEXTDATE, inspection.getNextInspectionDate());
+                stmt.setParameter(Fields.TEMPERATURE, inspection.getTemperature().orNull());
+                stmt.setParameter(Fields.HUMIDITY, inspection.getHumidity().orNull());
+                stmt.setParameter(Fields.SUMMARY, inspection.getSummary().orNull());
+                stmt.setParameter(Fields.TITEL, inspection.getTitle());
+                stmt.setParameter(Fields.TEMPERATURERATING, inspection.getTemperatureRating()
+                        .orNull());
+                stmt.setParameter(Fields.TEMPERATUREQUANTIFIER, inspection
+                        .getTemperatureQuantifier().orNull());
+                stmt.setParameter(Fields.HUMIDITYRATING, inspection.getHumidityRating().orNull());
+                stmt.setParameter(Fields.HUMIDITYQUANTIFIER, inspection.getHumidityQuantifier()
+                        .orNull());
+
+                int affectedRows = stmt.executeUpdate();
+                if (affectedRows != 1) {
+                    throw new SaveException();
+                }
+
+                try (ResultSet generatedKeys = stmt.getGeneratedKeys()) {
+                    if (generatedKeys.next()) {
+                        int id = generatedKeys.getInt(1);
+
+                        // Create new Inspection with id
+                        Inspection inserted = new Inspection(id, inspection.getReportDate(),
+                                inspection.getInspectionDate(), inspection.getNextInspectionDate(),
+                                inspection.getTitle(), inspection.getLayout(),
+                                inspection.getPlant());
+                        
+                        inserted.setRequester(inspection.getRequester().orNull());
+                        inserted.setContractor(inspection.getContractor().orNull());
+                        inserted.setChecker(inspection.getChecker().orNull());
+                        inserted.setTemperature(inspection.getTemperature().orNull());
+                        inserted.setHumidity(inspection.getHumidity().orNull());
+                        inserted.setSummary(inspection.getSummary().orNull());
+                        inserted.setTemperatureRating(inspection.getTemperatureRating().orNull());
+                        inserted.setTemperatureQuantifier(inspection.getTemperatureQuantifier()
+                                .orNull());
+                        inserted.setHumidityRating(inspection.getHumidityRating().orNull());
+                        inserted.setHumidityQuantifier(inspection.getHumidityQuantifier().orNull());
+                        return inserted;
+                    }
+                    else {
+                        throw new SaveException("Could not retrieve generated ID.");
+                    }
+                }
+            }
+
+        }
+        catch (SQLException | DatabaseException e) {
+            throw new SaveException(e);
+        }
     }
 
     @Override
@@ -118,7 +185,7 @@ public class InspectionDao implements IInspectionDao {
         public static final String REQUESTER_FK = "Report_Requester_FK";
         public static final String CONTRACTOR_FK = "Report_Contractor_FK";
         public static final String CHECKER_FK = "Report_Checker_FK";
-        public static final String JOBDATE = "Report_Jobdate";
+        public static final String INSPECTIONDATE = "Report_Jobdate";
         public static final String REPORTDATE = "Report_Reportdate";
         public static final String NEXTDATE = "Report_Nextdate";
         public static final String TEMPERATURE = "Report_Airtemperature";
