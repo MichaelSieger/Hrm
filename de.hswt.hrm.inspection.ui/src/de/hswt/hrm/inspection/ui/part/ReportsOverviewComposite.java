98bd253b5626d6ffbfd411041d920a670b08079c
Tobias Placht <tobias.placht@student.hswt.de>
2013-06-23 12:27:45 +0200

    add HIDE_SELECTION Flag

--- de.hswt.hrm.inspection.ui/src/de/hswt/hrm/inspection/ui/part/ReportsOverviewComposite.java
+++ de.hswt.hrm.inspection.ui/src/de/hswt/hrm/inspection/ui/part/ReportsOverviewComposite.java
@@ -115,7 +115,7 @@ public class ReportsOverviewComposite extends Composite {
         });
         toolkit.adapt(searchText, true, true);
 
-        tableViewer = new TableViewer(overviewComposite, SWT.BORDER | SWT.FULL_SELECTION);
+        tableViewer = new TableViewer(overviewComposite, SWT.BORDER | SWT.HIDE_SELECTION);
         table = tableViewer.getTable();
         table.setLinesVisible(true);
         table.setHeaderVisible(true);
