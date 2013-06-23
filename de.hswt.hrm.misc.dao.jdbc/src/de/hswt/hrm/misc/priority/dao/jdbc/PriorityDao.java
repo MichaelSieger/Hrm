5e3c38584e40666edb17773097058deee89c788e
haeringl <luke-haering@web.de>
2013-06-23 16:14:00 +0200

    add fromResultSet

--- de.hswt.hrm.misc.dao.jdbc/src/de/hswt/hrm/misc/priority/dao/jdbc/PriorityDao.java
+++ de.hswt.hrm.misc.dao.jdbc/src/de/hswt/hrm/misc/priority/dao/jdbc/PriorityDao.java
@@ -1,5 +1,10 @@
 package de.hswt.hrm.misc.priority.dao.jdbc;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Collection;
 
 import de.hswt.hrm.common.database.exception.DatabaseException;
@@ -31,9 +36,26 @@ public class PriorityDao implements IPriorityDao {
     @Override
     public void update(Priority priority) throws ElementNotFoundException, SaveException {
         // TODO Auto-generated method stub
-        
+
     }
-    
+
+    private Collection<Priority> fromResultSet(ResultSet rs) throws SQLException {
+        checkNotNull(rs, "Result must not be null.");
+        Collection<Priority> priorityList = new ArrayList<>();
+
+        while (rs.next()) {
+            int id = rs.getInt(Fields.ID);
+            String name = rs.getString(Fields.NAME);
+            String text = rs.getString(Fields.TEXT);
+            int priority = rs.getInt(Fields.PRIORITY);
+
+            Priority prio = new Priority(id, name, text, priority);
+
+            priorityList.add(prio);
+        }
+
+        return priorityList;
+    }
 
     private static final String TABLE_NAME = "Priority";
 
@@ -42,7 +64,7 @@ public class PriorityDao implements IPriorityDao {
         public static final String NAME = "Priority_Name";
         public static final String TEXT = "Priority_Text";
         public static final String PRIORITY = "Priority_Priority";
-        
+
     }
 
 }
