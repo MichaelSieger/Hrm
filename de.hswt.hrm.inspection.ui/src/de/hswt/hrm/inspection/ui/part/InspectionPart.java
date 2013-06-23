ec7b278470035ad0881a112a9f54d16b33bcee8c
Michi <michael.sieger@student.hswt.de>
2013-06-23 16:24:51 +0200

    PlantListener

--- de.hswt.hrm.inspection.ui/src/de/hswt/hrm/inspection/ui/part/InspectionPart.java
+++ de.hswt.hrm.inspection.ui/src/de/hswt/hrm/inspection/ui/part/InspectionPart.java
@@ -1,5 +1,8 @@
 package de.hswt.hrm.inspection.ui.part;
 
+import java.io.IOException;
+import java.util.Collection;
+
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.inject.Inject;
@@ -19,15 +22,37 @@ import org.eclipse.jface.action.Action;
 import org.eclipse.jface.action.ActionContributionItem;
 import org.eclipse.jface.action.Separator;
 
+import de.hswt.hrm.common.database.exception.DatabaseException;
+import de.hswt.hrm.common.database.exception.ElementNotFoundException;
 import de.hswt.hrm.common.ui.swt.forms.FormUtil;
+import de.hswt.hrm.plant.model.Plant;
+import de.hswt.hrm.scheme.model.Scheme;
+import de.hswt.hrm.scheme.model.SchemeComponent;
+import de.hswt.hrm.scheme.service.ComponentConverter;
+import de.hswt.hrm.scheme.service.SchemeService;
+import de.hswt.hrm.scheme.ui.SchemeGridItem;
 
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.base.Function;
+import com.google.common.base.Throwables;
+import com.google.common.collect.Collections2;
 
 public class InspectionPart {
+	
+	private static final String RENDER_ERROR = "Error on rendering image";
 
+	private final static Logger LOG = LoggerFactory
+			.getLogger(InspectionPart.class);
+	
     @Inject
     private IEclipseContext context;
+    
+    @Inject
+    private SchemeService schemeService;
 
     private FormToolkit formToolkit = new FormToolkit(Display.getDefault());
 
@@ -66,7 +91,7 @@ public class InspectionPart {
      * Create contents of the view part.
      */
     @PostConstruct
-    public void createControls(Composite parent) {
+    public void createControls(final Composite parent) {
         parent.setBackgroundMode(SWT.INHERIT_DEFAULT);
 
         Composite composite = new Composite(parent, SWT.NONE);
@@ -102,6 +127,35 @@ public class InspectionPart {
         generalTab.setText("General");
 
         reportGeneralComposite = new ReportGeneralComposite(tabFolder);
+        reportGeneralComposite.setPlantListener(new PlantSelectedListener() {
+			
+			@Override
+			public void selected(Plant plant) {
+				Scheme scheme;
+				try {
+					scheme = schemeService.findCurrentSchemeByPlant(plant);
+				} catch(ElementNotFoundException e){
+					//TODO what to do if there is no scheme?
+					throw Throwables.propagate(e);
+				}catch (DatabaseException e) {
+					throw Throwables.propagate(e);
+				}
+				Collection<SchemeGridItem> schemeGridItems = Collections2.transform(scheme.getSchemeComponents(), new Function<SchemeComponent, SchemeGridItem>() {
+					public SchemeGridItem apply(SchemeComponent c){
+						try {
+							return new SchemeGridItem(ComponentConverter.convert(parent.getDisplay(), c.getComponent()),
+														c.getDirection(), c.getX(), c.getY());
+						} catch (IOException e) {
+							LOG.error(RENDER_ERROR);
+							return null;
+						}
+					}
+				});
+				physicalComposite.setSchemeGridItems(schemeGridItems);
+				biologicalComposite.setSchemeGridItems(schemeGridItems);
+				performanceComposite.setSchemeGridItems(schemeGridItems);
+			}
+		});
         ContextInjectionFactory.inject(reportGeneralComposite, context);
         generalTab.setControl(reportGeneralComposite);
 
