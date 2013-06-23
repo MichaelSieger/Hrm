bd65c2896fd0d03d3312039ef1975746177bac43
haeringl <luke-haering@web.de>
2013-06-23 16:25:14 +0200

    add findAll method in priorityDao

--- de.hswt.hrm.misc.dao.jdbc/src/de/hswt/hrm/misc/priority/dao/jdbc/PriorityDao.java
+++ de.hswt.hrm.misc.dao.jdbc/src/de/hswt/hrm/misc/priority/dao/jdbc/PriorityDao.java
@@ -2,23 +2,46 @@ package de.hswt.hrm.misc.priority.dao.jdbc;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
+import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collection;
 
+import org.apache.commons.dbutils.DbUtils;
+
+import de.hswt.hrm.common.database.DatabaseFactory;
+import de.hswt.hrm.common.database.NamedParameterStatement;
+import de.hswt.hrm.common.database.SqlQueryBuilder;
 import de.hswt.hrm.common.database.exception.DatabaseException;
 import de.hswt.hrm.common.database.exception.ElementNotFoundException;
 import de.hswt.hrm.common.database.exception.SaveException;
 import de.hswt.hrm.misc.priority.dao.core.IPriorityDao;
 import de.hswt.hrm.misc.priority.model.Priority;
 
+
 public class PriorityDao implements IPriorityDao {
 
     @Override
     public Collection<Priority> findAll() throws DatabaseException {
-        // TODO Auto-generated method stub
-        return null;
+        SqlQueryBuilder builder = new SqlQueryBuilder();
+        builder.select(TABLE_NAME, Fields.ID, Fields.NAME, Fields.TEXT, Fields.PRIORITY);
+
+        final String query = builder.toString();
+
+        try (Connection con = DatabaseFactory.getConnection()) {
+            try (NamedParameterStatement stmt = NamedParameterStatement.fromConnection(con, query)) {
+                ResultSet result = stmt.executeQuery();
+
+                Collection<Priority> priorities = fromResultSet(result);
+                DbUtils.closeQuietly(result);
+
+                return priorities;
+            }
+        }
+        catch (SQLException e) {
+            throw new DatabaseException(e);
+        }
     }
 
     @Override
