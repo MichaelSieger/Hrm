c74048459643c5c69dcd8e8e6908056493ff4a74
Tobias Placht <tobias.placht@student.hswt.de>
2013-06-24 20:34:15 +0200

    begin implementation of editAtrributesDialog

--- de.hswt.hrm.scheme.ui/src/de/hswt/hrm/scheme/ui/dialog/EditAtrributesDialog.java
+++ de.hswt.hrm.scheme.ui/src/de/hswt/hrm/scheme/ui/dialog/EditAtrributesDialog.java
@@ -1,5 +1,55 @@
 package de.hswt.hrm.scheme.ui.dialog;
 
-public class EditAtrributesDialog {
+import org.eclipse.jface.dialogs.TitleAreaDialog;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.forms.widgets.Section;
+
+import de.hswt.hrm.common.ui.swt.forms.FormUtil;
+import de.hswt.hrm.common.ui.swt.layouts.LayoutUtil;
+import de.hswt.hrm.common.ui.swt.utils.SWTResourceManager;
+
+public class EditAtrributesDialog extends TitleAreaDialog {
+
+    public EditAtrributesDialog(Shell parentShell) {
+        super(parentShell);
+
+    }
+
+    @Override
+    protected Control createDialogArea(Composite parent) {
+        Composite composite = (Composite) super.createDialogArea(parent);
+
+        // Contents of Dialog
+        draw(composite);
+
+        setMessage("Please select a scheme to copy.");
+        setTitle("Scheme selection");
+
+        return composite;
+
+    }
+
+    private void draw(Composite parent) {
+        Section headerSection = new Section(parent, Section.TITLE_BAR);
+        headerSection.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
+        headerSection.setText("Scheme selection");
+        headerSection.setExpanded(true);
+        headerSection.setLayoutData(LayoutUtil.createFillData());
+        FormUtil.initSectionColors(headerSection);
+        headerSection.setLayout(new FillLayout());
+        headerSection.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
+
+        Composite headerComposite = new Composite(headerSection, SWT.NONE);
+        GridLayout gl = new GridLayout(2, false);
+        gl.marginWidth = 0;
+        gl.marginHeight = 0;
+        headerComposite.setLayout(gl);
+        headerComposite.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
+    }
 
 }
