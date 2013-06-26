6cd17214998631590110e4a547ee61f8b0913bf7
Michi <michael.sieger@student.hswt.de>
2013-06-26 02:22:17 +0200

    Uses ids

--- de.hswt.hrm.scheme.ui/src/de/hswt/hrm/scheme/ui/dnd/DragData.java
+++ de.hswt.hrm.scheme.ui/src/de/hswt/hrm/scheme/ui/dnd/DragData.java
@@ -3,9 +3,17 @@ package de.hswt.hrm.scheme.ui.dnd;
 import java.io.Serializable;
 import java.util.List;
 
+import com.google.common.base.Preconditions;
+import com.google.common.base.Throwables;
+
+import de.hswt.hrm.common.database.exception.DatabaseException;
+import de.hswt.hrm.common.database.exception.ElementNotFoundException;
+import de.hswt.hrm.component.service.ComponentService;
 import de.hswt.hrm.scheme.model.Direction;
 import de.hswt.hrm.scheme.model.RenderedComponent;
+import de.hswt.hrm.scheme.model.Scheme;
 import de.hswt.hrm.scheme.model.SchemeComponent;
+import de.hswt.hrm.scheme.service.SchemeService;
 import de.hswt.hrm.scheme.ui.SchemeGridItem;
 
 /**
@@ -26,16 +34,28 @@ public class DragData
      */
     private final int renderedComponentIndex;
     
-    private final SchemeComponent schemeComponent;
+    private final int id;
+    private int x = -1;
+    private int y = -1;
+    private final int schemeId;
+    private final int componentId;
+    private final Direction direction;
     
     public DragData(int renderedComponentIndex, SchemeComponent schemeComponent) {
         super();
         this.renderedComponentIndex = renderedComponentIndex;
-        this.schemeComponent = schemeComponent;
+        id = schemeComponent.getId();
+        if(schemeComponent.hasValidPosition()){
+            x = schemeComponent.getX();
+            y = schemeComponent.getY();
+        }
+        schemeId = (schemeComponent.getScheme() != null ? schemeComponent.getScheme().getId() : -1);
+        componentId = (schemeComponent.getComponent() != null ? schemeComponent.getComponent().getId() : -1);
+        direction = schemeComponent.getDirection();
     }
     
     public boolean hasPosition(){
-    	return !(schemeComponent.getX() == -1 && schemeComponent.getY() == -1);
+    	return !(x == -1 && y == -1);
     }
     
     /**
@@ -43,9 +63,28 @@ public class DragData
      * 
      * @param comps0
      * @return
+     * @throws DatabaseException 
+     * @throws ElementNotFoundException 
      */
-    public SchemeGridItem toSchemeGridItem(List<RenderedComponent> comps){
-    	return new SchemeGridItem(comps.get(renderedComponentIndex), schemeComponent);
+    public SchemeGridItem toSchemeGridItem(List<RenderedComponent> comps, SchemeService schemeService, ComponentService componentService){
+    	Preconditions.checkNotNull(comps);
+    	Preconditions.checkNotNull(schemeService);
+    	Preconditions.checkNotNull(componentService);
+    	try {
+    		Scheme scheme = null;
+    		if(schemeId != -1){
+    			scheme = schemeService.findById(schemeId);
+    		}
+			return new SchemeGridItem(comps.get(renderedComponentIndex), 
+												new SchemeComponent(scheme, 
+														x, 
+														y, 
+														direction, 
+														componentService.findById(componentId)));
+		} catch (DatabaseException e) {
+			//All these items were loaded before, so this should not happen
+			throw Throwables.propagate(e);
+		}
     }
 
 }
