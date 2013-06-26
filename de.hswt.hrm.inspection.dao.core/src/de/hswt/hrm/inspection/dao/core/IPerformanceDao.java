bd412ee3d34734d421d1d3621e659fc3b3cb3e27
Ben <benjamin.pabst85@gmail.com>
2013-06-26 15:22:27 +0200

    Add performance DAO and DAO interface.

---
+++ de.hswt.hrm.inspection.dao.core/src/de/hswt/hrm/inspection/dao/core/IPerformanceDao.java
@@ -0,0 +1,5 @@
+package de.hswt.hrm.inspection.dao.core;
+
+public interface IPerformanceDao {
+
+}
---
+++ de.hswt.hrm.inspection.dao.jdbc/src/de/hswt/hrm/inspection/dao/jdbc/PerformanceDao.java
@@ -0,0 +1,7 @@
+package de.hswt.hrm.inspection.dao.jdbc;
+
+import de.hswt.hrm.inspection.dao.core.IPerformanceDao;
+
+public class PerformanceDao implements IPerformanceDao {
+
+}
