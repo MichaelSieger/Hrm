d5626d6902d7c0a17cd03509f9a2aae2b1f6949b
Tobias Placht <tobias.placht@student.hswt.de>
2013-06-26 21:43:06 +0200

    made dialog tableviewer editable

--- de.hswt.hrm.scheme.ui/src/de/hswt/hrm/scheme/ui/dialog/EditAtrributesDialog.java
+++ de.hswt.hrm.scheme.ui/src/de/hswt/hrm/scheme/ui/dialog/EditAtrributesDialog.java
@@ -10,6 +10,11 @@ import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.jface.viewers.ViewerComparator;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.TableEditor;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
@@ -17,6 +22,8 @@ import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableItem;
+import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.forms.widgets.Section;
 
 import de.hswt.hrm.common.ui.swt.forms.FormUtil;
@@ -30,89 +37,123 @@ import de.hswt.hrm.scheme.ui.util.SchemeDialogUtil;
 
 public class EditAtrributesDialog extends TitleAreaDialog {
 
-	private Component component;
+    private Component component;
 
-	private Table table;
-	private TableViewer tableViewer;
-	private Collection<Attribute> attributes;
-	private Map<Attribute, String> assignedValues;
+    private Table table;
+    private TableViewer tableViewer;
 
-	public EditAtrributesDialog(Shell parentShell, Component component,
-			Collection<Attribute> attributes,
-			Map<Attribute, String> assignedValues) {
-		super(parentShell);
-		this.component = component;
-		this.attributes = attributes;
-		this.assignedValues = assignedValues;
+    private Text newEditor;
 
-	}
+    private Collection<Attribute> attributes;
+    private Map<Attribute, String> assignedValues;
 
-	@Override
-	protected Control createDialogArea(Composite parent) {
-		Composite composite = (Composite) super.createDialogArea(parent);
+    public EditAtrributesDialog(Shell parentShell, Component component,
+            Collection<Attribute> attributes, Map<Attribute, String> assignedValues) {
+        super(parentShell);
+        this.component = component;
+        this.attributes = attributes;
+        this.assignedValues = assignedValues;
 
-		// Contents of Dialog
-		draw(composite);
+    }
 
-		setMessage("Edit Attributes for Component: " + component.getName());
-		setTitle("Attributes ");
+    @Override
+    protected Control createDialogArea(Composite parent) {
+        Composite composite = (Composite) super.createDialogArea(parent);
 
-		return composite;
+        // Contents of Dialog
+        draw(composite);
 
-	}
+        setMessage("Edit Attributes for Component: " + component.getName());
+        setTitle("Attributes ");
 
-	private void draw(Composite parent) {
-		Section headerSection = new Section(parent, Section.TITLE_BAR);
-		headerSection.setBackground(SWTResourceManager
-				.getColor(SWT.COLOR_WHITE));
-		headerSection.setText("Available Attributes");
-		headerSection.setLayoutData(LayoutUtil.createFillData());
-		FormUtil.initSectionColors(headerSection);
-		headerSection.setLayout(new FillLayout());
-		headerSection.setBackground(SWTResourceManager
-				.getColor(SWT.COLOR_WHITE));
+        composite.forceFocus();
+        return composite;
 
-		Composite headerComposite = new Composite(headerSection, SWT.NONE);
-		GridLayout gl = new GridLayout(2, false);
-		gl.marginWidth = 0;
-		gl.marginHeight = 0;
-		headerComposite.setLayout(gl);
-		headerComposite.setBackground(SWTResourceManager
-				.getColor(SWT.COLOR_WHITE));
+    }
 
-		tableViewer = new TableViewer(headerSection, SWT.BORDER
-				| SWT.FULL_SELECTION);
-		table = tableViewer.getTable();
-		table.setLinesVisible(true);
-		table.setHeaderVisible(true);
-		headerSection.setClient(table);
-		GridData gd = LayoutUtil.createFillData();
-		gd.widthHint = 800;
-		gd.heightHint = 300;
-		table.setLayoutData(gd);
-		initTable();
-	}
+    private void draw(Composite parent) {
+        Section headerSection = new Section(parent, Section.TITLE_BAR);
+        headerSection.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
+        headerSection.setText("Available Attributes");
+        headerSection.setLayoutData(LayoutUtil.createFillData());
+        FormUtil.initSectionColors(headerSection);
+        headerSection.setLayout(new FillLayout());
+        headerSection.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
 
-	private void initTable() {
+        Composite headerComposite = new Composite(headerSection, SWT.NONE);
+        GridLayout gl = new GridLayout(2, false);
+        gl.marginWidth = 0;
+        gl.marginHeight = 0;
+        headerComposite.setLayout(gl);
+        headerComposite.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
 
-		List<ColumnDescription<Attribute>> columns = SchemeDialogUtil
-				.getColumns(assignedValues);
+        tableViewer = new TableViewer(headerSection, SWT.BORDER | SWT.FULL_SELECTION);
+        table = tableViewer.getTable();
+        table.setLinesVisible(true);
+        table.setHeaderVisible(true);
+        headerSection.setClient(table);
+        GridData gd = LayoutUtil.createFillData();
+        gd.widthHint = 800;
+        gd.heightHint = 300;
+        table.setLayoutData(gd);
+        initTable();
+    }
 
-		TableViewerController<Attribute> filler = new TableViewerController<>(
-				tableViewer);
-		filler.createColumns(columns);
-		filler.createColumnSelectionMenu();
+    private void initTable() {
 
-		tableViewer.setContentProvider(ArrayContentProvider.getInstance());
-		tableViewer.setInput(attributes);
-		tableViewer.setComparator(new ViewerComparator() {
-			@Override
-			public int compare(Viewer viewer, Object o1, Object o2) {
-				Attribute a1 = (Attribute) o1;
-				Attribute a2 = (Attribute) o2;
-				return a1.getName().compareToIgnoreCase(a2.getName());
-			}
-		});
+        List<ColumnDescription<Attribute>> columns = SchemeDialogUtil.getColumns(assignedValues);
 
-	}
+        TableViewerController<Attribute> filler = new TableViewerController<>(tableViewer);
+        filler.createColumns(columns);
+        filler.createColumnSelectionMenu();
+
+        tableViewer.setContentProvider(ArrayContentProvider.getInstance());
+        tableViewer.setInput(attributes);
+        tableViewer.setComparator(new ViewerComparator() {
+            @Override
+            public int compare(Viewer viewer, Object o1, Object o2) {
+                Attribute a1 = (Attribute) o1;
+                Attribute a2 = (Attribute) o2;
+                return a1.getName().compareToIgnoreCase(a2.getName());
+            }
+        });
+
+        final TableEditor editor = new TableEditor(table);
+        // The editor must have the same size as the cell and must
+        // not be any smaller than 50 pixels.
+        editor.horizontalAlignment = SWT.LEFT;
+        editor.grabHorizontal = true;
+        editor.minimumWidth = 50;
+        // editing the second column
+        final int EDITABLECOLUMN = 1;
+
+        table.addSelectionListener(new SelectionAdapter() {
+
+            public void widgetSelected(SelectionEvent e) {
+                // Clean up any previous editor control
+                Control oldEditor = editor.getEditor();
+                if (oldEditor != null)
+                    oldEditor.dispose();
+
+                // Identify the selected row
+                TableItem item = (TableItem) e.item;
+                if (item == null)
+                    return;
+
+                // The control that will be the editor must be a child of the Table
+                newEditor = new Text(table, SWT.NONE);
+                newEditor.setText(item.getText(EDITABLECOLUMN));
+                newEditor.addModifyListener(new ModifyListener() {
+                    public void modifyText(ModifyEvent me) {
+                        Text text = (Text) editor.getEditor();
+                        editor.getItem().setText(EDITABLECOLUMN, text.getText());
+                    }
+                });
+                newEditor.selectAll();
+                newEditor.setFocus();
+                editor.setEditor(newEditor, item, EDITABLECOLUMN);
+            }
+        });
+
+    }
 }
