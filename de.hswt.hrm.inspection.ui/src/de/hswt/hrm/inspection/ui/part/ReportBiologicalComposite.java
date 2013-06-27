316ca714e5b03168d88062173ba444fab9965390
Michi <michael.sieger@student.hswt.de>
2013-06-27 14:52:02 +0200

    addGradeSelectionObserver

--- de.hswt.hrm.inspection.ui/src/de/hswt/hrm/inspection/ui/part/ReportBiologicalComposite.java
+++ de.hswt.hrm.inspection.ui/src/de/hswt/hrm/inspection/ui/part/ReportBiologicalComposite.java
@@ -7,6 +7,8 @@ import org.eclipse.e4.core.contexts.IEclipseContext;
 import org.eclipse.jface.window.IShellProvider;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.ScrolledComposite;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
@@ -19,6 +21,8 @@ import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.forms.widgets.FormToolkit;
 import org.eclipse.ui.forms.widgets.Section;
 
+import de.hswt.hrm.common.observer.Observable;
+import de.hswt.hrm.common.observer.Observer;
 import de.hswt.hrm.common.ui.swt.forms.FormUtil;
 import de.hswt.hrm.common.ui.swt.layouts.LayoutUtil;
 import de.hswt.hrm.common.ui.swt.utils.ContentProposalUtil;
@@ -54,6 +58,8 @@ public class ReportBiologicalComposite extends AbstractComponentRatingComposite 
 	private Text contactCultureConcentrationText;
 
 	private Text airGermsConcentrationText;
+	
+	private Observable<Integer> selectedGrade = new Observable<>();
 
     // @TODO remove when example is no longer needed
     private static final String[] items = new String[] { "Alpha", "Beta", "gaama", "pie", "alge",
@@ -158,6 +164,19 @@ public class ReportBiologicalComposite extends AbstractComponentRatingComposite 
         }
         // TODO set 0 selected or grade from db
         contactGgradeList.select(0);
+        contactGgradeList.addSelectionListener(new SelectionListener() {
+			
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				int sel = contactGgradeList.getSelectionIndex();
+				if(sel != -1){
+					selectedGrade.set(sel);
+				}
+			}
+			
+			@Override
+			public void widgetDefaultSelected(SelectionEvent e) {}
+		});
 
         contactWeightList = new List(contactCultureComposite, SWT.BORDER);
         contactWeightList.setLayoutData(LayoutUtil.createHorzFillData(2));
@@ -302,6 +321,10 @@ public class ReportBiologicalComposite extends AbstractComponentRatingComposite 
         contactCultureRadioButton.setText("Contact culture");
 
         sc.setMinSize(composite.computeSize(SWT.DEFAULT, SWT.DEFAULT));
+    }
+    
+    public void addGradeSelectionObserver(Observer<Integer> o){
+    	selectedGrade.addObserver(o);
     }
 
     @Override
