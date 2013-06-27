8ff8c8e8df0bc30ff39625d470e986836e74ae74
Michi <michael.sieger@student.hswt.de>
2013-06-27 14:43:39 +0200

    BiologicalDisplay

---
+++ de.hswt.hrm.inspection.ui/src/de/hswt/hrm/inspection/ui/grid/BiologicalDisplay.java
@@ -0,0 +1,44 @@
+package de.hswt.hrm.inspection.ui.grid;
+
+import java.util.Collection;
+
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.Device;
+
+import com.google.common.base.Preconditions;
+
+import de.hswt.hrm.inspection.model.BiologicalRating;
+import de.hswt.hrm.scheme.model.SchemeComponent;
+
+public class BiologicalDisplay {
+	
+	private final InspectionSchemeGrid schemeGrid;
+	private final Color[] colors;
+	
+	public BiologicalDisplay(InspectionSchemeGrid schemeGrid){
+		this.schemeGrid = schemeGrid;
+		Device device = schemeGrid.getControl().getDisplay();
+		colors = new Color[]
+				{
+					new Color(device, 0, 0, 0),
+					new Color(device, 255, 0, 0),
+					new Color(device, 255, 0, 0),
+					new Color(device, 255, 255, 0),
+					new Color(device, 255, 255, 0),
+					new Color(device, 0, 255, 0),
+					new Color(device, 0, 255, 0)
+				};
+	}
+
+	public void update(Collection<BiologicalRating> ratings){
+		schemeGrid.clearColors();
+		for(BiologicalRating rating : ratings){
+			//TODO
+			SchemeComponent c = null;
+			int r = rating.getRating();
+			Preconditions.checkArgument(r >= 0 && r < colors.length);
+			schemeGrid.setColor(c, colors[r]);
+		}
+	}
+	
+}
