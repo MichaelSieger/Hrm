4f65eb4c8760aa0bceef6087e57e040add73929a
Michael Sieger <michael.sieger@student.hswt.de>
2013-06-28 16:55:09 +0200

    SamplingPoint Implementation

--- de.hswt.hrm.inspection.ui/src/de/hswt/hrm/inspection/ui/grid/BiologicalDisplay.java
+++ de.hswt.hrm.inspection.ui/src/de/hswt/hrm/inspection/ui/grid/BiologicalDisplay.java
@@ -1,27 +1,41 @@
 package de.hswt.hrm.inspection.ui.grid;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 import org.eclipse.swt.graphics.Color;
 
+import com.google.common.base.Optional;
 import com.google.common.base.Preconditions;
 
 import de.hswt.hrm.inspection.model.BiologicalRating;
+import de.hswt.hrm.inspection.model.SamplingPointType;
+import de.hswt.hrm.scheme.ui.SchemeGridItem;
 
 public class BiologicalDisplay extends RatingDisplay{
 	
-	public BiologicalDisplay(InspectionSchemeGrid grid){
-		super(grid);
+	private List<SchemeGridItem> samplePoints = new ArrayList<>();
+	
+	public BiologicalDisplay(InspectionSchemeGrid grid, SamplingPoints samplingPoints){
+		super(grid, samplingPoints);
 	}
 
 	public void update(Collection<BiologicalRating> ratings){
 		InspectionSchemeGrid schemeGrid = getSchemeGrid();
+		schemeGrid.removeAll(samplePoints);
+		samplePoints.clear();
 		schemeGrid.clearColors();
 		Color[] colors = getColors();
 		for(BiologicalRating rating : ratings){
 			int r = rating.getRating();
 			Preconditions.checkArgument(r >= 0 && r < colors.length);
 			schemeGrid.setColor(rating.getComponent(), colors[r]);
+			Optional<SamplingPointType> samplingPoint = rating.getSamplingPointType();
+			if(samplingPoint.isPresent()){
+				samplePoints.add(SamplingPointPosition.getSamplingPoint(
+									getSamplingPoints(), samplingPoint.get(), null, rating.getComponent()));
+			}
 		}
 	}
 	
