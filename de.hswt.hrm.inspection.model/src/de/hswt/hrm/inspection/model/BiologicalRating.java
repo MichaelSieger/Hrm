fdc347488b9ef46f37544a3d2b63cfcf625f627e
Michael Sieger <michael.sieger@student.hswt.de>
2013-06-28 17:42:39 +0200

    default is not rated

--- de.hswt.hrm.inspection.model/src/de/hswt/hrm/inspection/model/BiologicalRating.java
+++ de.hswt.hrm.inspection.model/src/de/hswt/hrm/inspection/model/BiologicalRating.java
@@ -40,7 +40,7 @@ public class BiologicalRating {
     }
 
     public BiologicalRating(Inspection inspection, SchemeComponent component) {
-        this(inspection, component, -1, -1, -1, null, null);
+        this(inspection, component, -1, 0, -1, null, null);
     }
 
     public BiologicalRating(Inspection inspection, SchemeComponent component, int bacteriaCount,
