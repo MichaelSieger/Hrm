28a83e53a73f1d74e42a6769a50befb1d768d525
Anton Schreck <anton.schreck@student.hswt.de>
2013-06-28 10:54:00 +0200

    Update translation

--- de.hswt.hrm.plant.ui.shared/src/de/hswt/hrm/plant/ui/shared/ui/wizzard/PlantWizardPageOne.java
+++ de.hswt.hrm.plant.ui.shared/src/de/hswt/hrm/plant/ui/shared/ui/wizzard/PlantWizardPageOne.java
@@ -217,6 +217,8 @@ public class PlantWizardPageOne extends WizardPage {
         // Labels
         setLabelText(container, "lblDescription", I18N.tr("Description") + ":");
         setLabelText(container, "lblScheme", I18N.tr("Scheme") + ":");
+        setLabelText(container, "lblArea", I18N.tr("Area") + ":");
+        setLabelText(container, "lblLocation", I18N.tr("Location") + ":");
         setLabelText(container, "lblManufactor", I18N.tr("Manufactor") + ":");
         setLabelText(container, "lblConstructionYear", I18N.tr("Construction Year") + ":");
         setLabelText(container, "lblType", I18N.tr("Type") + ":");
