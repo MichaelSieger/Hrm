fdc3f6430ed1117f83f481b797b60a27f4d2fea8
Michi <michael.sieger@student.hswt.de>
2013-06-29 21:12:09 +0200

    move code to RatingDisplay

--- de.hswt.hrm.inspection.ui/src/de/hswt/hrm/inspection/ui/grid/BiologicalDisplay.java
+++ de.hswt.hrm.inspection.ui/src/de/hswt/hrm/inspection/ui/grid/BiologicalDisplay.java
@@ -19,60 +19,19 @@ import de.hswt.hrm.scheme.ui.SchemeGridItem;
 
 public class BiologicalDisplay extends RatingDisplay{
 	
-	private List<SchemeGridItem> samplePoints = new ArrayList<>();
-	
 	public BiologicalDisplay(InspectionSchemeGrid grid, SamplingPoints samplingPoints){
 		super(grid, samplingPoints);
 	}
 
 	public void update(Collection<BiologicalRating> ratings, Scheme scheme){
+		updateSamplingPoints(ratings, scheme);
 		InspectionSchemeGrid schemeGrid = getSchemeGrid();
-		schemeGrid.removeAll(samplePoints);
-		samplePoints.clear();
 		schemeGrid.clearColors();
 		Color[] colors = getColors();
 		for(BiologicalRating rating : ratings){
 			int r = rating.getRating();
 			Preconditions.checkArgument(r >= 0 && r < colors.length);
 			schemeGrid.setColor(rating.getComponent(), colors[r]);
-			Optional<SamplingPointType> samplingPoint = rating.getSamplingPointType();
-			if(samplingPoint.isPresent() && samplingPoint.get() != SamplingPointType.none){
-				Collection<SchemeGridItem> items = schemeGrid.getItems();
-				Placement place = SamplingPointPosition.getPlacement(items, findById(items, rating.getComponent()));
-				if(place != null){
-					if(place.x < 0){
-						int moveX = -place.x;
-						schemeGrid.move(moveX, 0);
-						place.x += moveX;
-						moveSamplePoints(moveX, 0);
-					}
-					if(place.y < 0){
-						int moveY = -place.y;
-						schemeGrid.move(0, moveY);
-						place.y += moveY;
-						moveSamplePoints(0, moveY);
-					}
-					samplePoints.add(SamplingPointPosition.getSamplingPoint(
-										getSamplingPoints(), samplingPoint.get(), scheme, rating.getComponent(), place));
-				}
-			}
-		}
-		schemeGrid.addAll(samplePoints);
-	}
-	
-	private SchemeComponent findById(Collection<SchemeGridItem> items, SchemeComponent c){
-		for(SchemeGridItem i : items){
-			if(i.asSchemeComponent().getId() == c.getId()){
-				return i.asSchemeComponent();
-			}
-		}
-		throw new RuntimeException("Internal Error");
-	}
-	
-	private void moveSamplePoints(int x, int y){
-		for(SchemeGridItem item : samplePoints){
-			item.setX(item.getX() + x);
-			item.setY(item.getY() + y);
 		}
 	}
 	
--- de.hswt.hrm.inspection.ui/src/de/hswt/hrm/inspection/ui/grid/RatingDisplay.java
+++ de.hswt.hrm.inspection.ui/src/de/hswt/hrm/inspection/ui/grid/RatingDisplay.java
@@ -1,12 +1,21 @@
 package de.hswt.hrm.inspection.ui.grid;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
+import java.util.List;
 
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.widgets.Display;
 
+import com.google.common.base.Optional;
+
+import de.hswt.hrm.inspection.model.Rating;
+import de.hswt.hrm.inspection.model.SamplingPointType;
+import de.hswt.hrm.inspection.ui.grid.SamplingPointPosition.Placement;
+import de.hswt.hrm.scheme.model.Scheme;
+import de.hswt.hrm.scheme.model.SchemeComponent;
 import de.hswt.hrm.scheme.ui.SchemeGridItem;
 
 public class RatingDisplay {
@@ -14,6 +23,7 @@ public class RatingDisplay {
 	private final Color[] colors;
 	private final InspectionSchemeGrid grid;
 	private final SamplingPoints samplingPoints;
+	private List<SchemeGridItem> samplePointItems = new ArrayList<>();
 	
 	public RatingDisplay(InspectionSchemeGrid grid, SamplingPoints samplingPoints){
 		this.grid = grid;
@@ -28,6 +38,51 @@ public class RatingDisplay {
 					new Color(device, 244, 146, 238),
 					new Color(device, 248, 84, 57)
 				};
+	}
+	
+	public void updateSamplingPoints(Collection<? extends Rating> ratings, Scheme scheme){
+		grid.removeAll(samplePointItems);
+		samplePointItems.clear();
+		for(Rating rating : ratings){
+			Optional<SamplingPointType> samplingPoint = rating.getSamplingPointType();
+			if(samplingPoint.isPresent() && samplingPoint.get() != SamplingPointType.none){
+				Collection<SchemeGridItem> items = grid.getItems();
+				Placement place = SamplingPointPosition.getPlacement(items, findById(items, rating.getComponent()));
+				if(place != null){
+					if(place.x < 0){
+						int moveX = -place.x;
+						grid.move(moveX, 0);
+						place.x += moveX;
+						moveSamplePoints(moveX, 0);
+					}
+					if(place.y < 0){
+						int moveY = -place.y;
+						grid.move(0, moveY);
+						place.y += moveY;
+						moveSamplePoints(0, moveY);
+					}
+					samplePointItems.add(SamplingPointPosition.getSamplingPoint(
+										getSamplingPoints(), samplingPoint.get(), scheme, rating.getComponent(), place));
+				}
+			}
+		}
+		grid.addAll(samplePointItems);
+	}
+	
+	private SchemeComponent findById(Collection<SchemeGridItem> items, SchemeComponent c){
+		for(SchemeGridItem i : items){
+			if(i.asSchemeComponent().getId() == c.getId()){
+				return i.asSchemeComponent();
+			}
+		}
+		throw new RuntimeException("Internal Error");
+	}
+	
+	private void moveSamplePoints(int x, int y){
+		for(SchemeGridItem item : samplePointItems){
+			item.setX(item.getX() + x);
+			item.setY(item.getY() + y);
+		}
 	}
 	
 	public InspectionSchemeGrid getSchemeGrid(){
