3060a35ffb48a4bd288c55ab7d8dfa3b215a9ac0
haeringl <luke-haering@web.de>
2013-06-29 11:03:02 +0200

    add hash and equals

--- de.hswt.hrm.component.model/src/de/hswt/hrm/component/model/Attribute.java
+++ de.hswt.hrm.component.model/src/de/hswt/hrm/component/model/Attribute.java
@@ -4,38 +4,83 @@ import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
 
 public class Attribute {
-	private final int id;
-	private final Component component;
-	private String name;
-	
-	public Attribute(final String name, final Component component) {
-		this(-1, name, component);
-	}
-	
-	public Attribute(final int id, final String name, final Component component) {
-		checkNotNull(name, "Name is mandatory.");
-		checkArgument(name.length() > 0, "Name must not be of length 0.");
-		
-		this.id = id;
-		this.name = name;
-		this.component = component;
-	}
-	
-	public int getId() {
-		return id;
-	}
-	
-	public String getName() {
-		return name;
-	}
-	
-	public void setName(final String name) {
-		checkNotNull(name, "Name is mandatory.");
-		checkArgument(name.length() > 0, "Name must not be of length 0.");
-		this.name = name;
-	}
-	
-	public Component getComponent() {
-		return component;
-	}
+    private final int id;
+    private final Component component;
+    private String name;
+
+    public Attribute(final String name, final Component component) {
+        this(-1, name, component);
+    }
+
+    public Attribute(final int id, final String name, final Component component) {
+        checkNotNull(name, "Name is mandatory.");
+        checkArgument(name.length() > 0, "Name must not be of length 0.");
+
+        this.id = id;
+        this.name = name;
+        this.component = component;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(final String name) {
+        checkNotNull(name, "Name is mandatory.");
+        checkArgument(name.length() > 0, "Name must not be of length 0.");
+        this.name = name;
+    }
+
+    public Component getComponent() {
+        return component;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((component == null) ? 0 : component.hashCode());
+        result = prime * result + id;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        Attribute other = (Attribute) obj;
+        if (component == null) {
+            if (other.component != null) {
+                return false;
+            }
+        }
+        else if (!component.equals(other.component)) {
+            return false;
+        }
+        if (id != other.id) {
+            return false;
+        }
+        if (name == null) {
+            if (other.name != null) {
+                return false;
+            }
+        }
+        else if (!name.equals(other.name)) {
+            return false;
+        }
+        return true;
+    }
+
 }
